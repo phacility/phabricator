dbb4653be3c831d9d72b4686d6dab4f637818c94
epriestley <epriestley@phabricator.com>
2011-02-24 14:50:50 -0800

    Improve the DarkConsole "config" plugin to list libraries and source locations.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- src/aphront/console/plugin/config/__init__.php
+++ src/aphront/console/plugin/config/__init__.php
@@ -11,6 +11,7 @@ phutil_require_module('phabricator', 'infrastructure/env');
 phutil_require_module('phabricator', 'view/control/table');
 
 phutil_require_module('phutil', 'markup');
+phutil_require_module('phutil', 'moduleutils');
 
 
 phutil_require_source('DarkConsoleConfigPlugin.php');
