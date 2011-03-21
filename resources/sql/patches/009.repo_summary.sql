e1d68e852a56e41a1aea19f5e6338943fed5e34c
epriestley <epriestley@phabricator.com>
2011-03-20 17:55:31 -0700

    Sorta need this file.

---
+++ resources/sql/patches/009.repo_summary.sql
@@ -0,0 +1,7 @@
+CREATE TABLE phabricator_repository.`repository_summary` (
+  `repositoryID` int(10) unsigned NOT NULL,
+  `size` int(10) unsigned NOT NULL,
+  `lastCommitID` int(10) unsigned NOT NULL,
+  `epoch` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`repositoryID`)
+);
