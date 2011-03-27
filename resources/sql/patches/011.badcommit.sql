919421ead7e1ef2fb54e7aa64011377f53c7e425
epriestley <epriestley@phabricator.com>
2011-03-26 23:59:12 -0700

    Derp derp, apparentl neglected to save this file.

--- resources/sql/patches/011.badcommit.sql
+++ resources/sql/patches/011.badcommit.sql
@@ -1,44 +1,4 @@
-CREATE DATABASE phabricator_herald;
-
-CREATE TABLE phabricator_herald.herald_action (
-  id int unsigned not null auto_increment primary key,
-  ruleID int unsigned not null,
-  action varchar(255) not null,
-  target text not null
-);
-
-CREATE TABLE phabricator_herald.herald_rule (
-  id int unsigned not null auto_increment primary key,
-  name varchar(255) not null,
-  authorPHID varchar(64) binary not null,
-  contentType varchar(255) not null,
-  mustMatchAll bool not null,
-  configVersion int unsigned not null default '1',
-  dateCreated int unsigned not null,
-  dateModified int unsigned not null,
-  unique key (authorPHID, name)
-);
-
-CREATE TABLE phabricator_herald.herald_condition (
-  id int unsigned not null auto_increment primary key,
-  ruleID int unsigned not null,
-  fieldName varchar(255) not null,
-  fieldCondition varchar(255) not null,
-  value text not null
-);
-
-CREATE TABLE phabricator_herald.herald_transcript (
-  id int unsigned not null auto_increment primary key,
-  phid varchar(64) binary not null,
-  time int unsigned not null,
-  host varchar(255) not null,
-  psth varchar(255) not null,
-  duration float not null,
-  objectPHID varchar(64) binary not null,
-  dryRun bool not null,
-  objectTranscript longblob not null,
-  ruleTranscripts longblob not null,
-  conditionTranscripts longblob not null,
-  applyTranscripts longblob not null,
-  unique key (phid)
+CREATE TABLE phabricator_repository.repository_badcommit (
+  fullCommitName varchar(255) binary not null primary key,
+  description longblob not null
 );
