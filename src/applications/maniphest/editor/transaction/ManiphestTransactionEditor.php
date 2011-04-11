2b77cea45fb8d7994b71eebe95cc921ccfe9adaf
tuomaspelkonen <tuomas.pelkonen@fb.com>
2011-04-11 12:31:17 -0700

    Removed extra space from Maniphest email subject line.
    Summary:
    Like the title says.
    
    Test Plan:
    grep for ':  ' didn't reveal any other similar problems.
    
    Reviewed By: jungejason
    Reviewers: epriestley, jungejason
    CC: jungejason
    Differential Revision: 121

--- src/applications/maniphest/editor/transaction/ManiphestTransactionEditor.php
+++ src/applications/maniphest/editor/transaction/ManiphestTransactionEditor.php
@@ -180,7 +180,7 @@ class ManiphestTransactionEditor {
 
     id(new PhabricatorMetaMTAMail())
       ->setSubject(
-        '[Maniphest] T'.$task->getID().' '.$action.':  '.$task->getTitle())
+        '[Maniphest] T'.$task->getID().' '.$action.': '.$task->getTitle())
       ->setFrom($transaction->getAuthorPHID())
       ->addTos($email_to)
       ->addCCs($email_cc)
