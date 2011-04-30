3815668a6d067b403c45e63c491bd655583ba920
Ryan McElroy <ryanmce@gmail.com>
2011-04-30 13:48:21 -0700

    [phabricator] Allow missing dependency checks to run
    Test Plan:
    Run upgrade-schema.php, see error instead of silent failure.
    
    Task: T123
    
    Reviewed By: epriestley
    Reviewers: epriestley, jungejason
    CC: aran, rm, epriestley
    Differential Revision: 199

--- scripts/__init_script__.php
+++ scripts/__init_script__.php
@@ -18,7 +18,7 @@
 
 $include_path = ini_get('include_path');
 ini_set('include_path', $include_path.':'.dirname(__FILE__).'/../../');
-@require_once 'libphutil/src/__phutil_library_init__.php';
+@include_once 'libphutil/src/__phutil_library_init__.php';
 if (!@constant('__LIBPHUTIL__')) {
   echo "ERROR: Unable to load libphutil. Update your PHP 'include_path' to ".
        "include the parent directory of libphutil/.\n";
