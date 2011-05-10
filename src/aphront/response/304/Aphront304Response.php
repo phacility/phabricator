80698aba6fff5ffd6dab5617107a9947cf538a11
epriestley <git@epriestley.com>
2011-05-10 16:08:20 -0700

    Fix horrible broken mess in 304 handling
    Summary:
    Sending a response body in a 304 triggers some crazy broken behavior in Safari +
    Apache that I never hit during testing. Be spec-compliant.
    
    Test Plan:
    Mashed reload a bunch on a .php page with Safari + Apache against localhost.
    
    Reviewed By: tuomaspelkonen
    Reviewers: tuomaspelkonen, jungejason, aran, rm
    CC: aran, tuomaspelkonen
    Differential Revision: 261

--- src/aphront/response/304/Aphront304Response.php
+++ src/aphront/response/304/Aphront304Response.php
@@ -26,7 +26,10 @@ class Aphront304Response extends AphrontResponse {
   }
 
   public function buildResponseString() {
-    return '304 Not Modified';
+    // IMPORTANT! According to the HTTP/1.1 spec (RFC 2616) a 304 response
+    // "MUST NOT" have any content. Apache + Safari strongly agree, and
+    // completely flip out and you start getting 304s for no-cache pages.
+    return null;
   }
 
 }
