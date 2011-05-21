1efc66a0dde07cdc456b57a803f6d86d113cbe7b
epriestley <git@epriestley.com>
2011-05-21 09:53:50 -0700

    Add file.download to Conduit
    Summary:
    This is required to make "arc patch" and "arc export" support binary changes.
    
    Test Plan:
    Called from web console and "arc".
    
    Reviewed By: aran
    Reviewers: tuomaspelkonen, jungejason, aran
    CC: aran, epriestley
    Differential Revision: 326

---
+++ src/applications/conduit/method/file/download/ConduitAPI_file_download_Method.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Copyright 2011 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class ConduitAPI_file_download_Method extends ConduitAPIMethod {
+
+  public function getMethodDescription() {
+    return "Download a file from the server.";
+  }
+
+  public function defineParamTypes() {
+    return array(
+      'phid' => 'required phid',
+    );
+  }
+
+  public function defineReturnType() {
+    return 'nonempty base64-bytes';
+  }
+
+  public function defineErrorTypes() {
+    return array(
+      'ERR-BAD-PHID' => 'No such file exists.',
+    );
+  }
+
+  protected function execute(ConduitAPIRequest $request) {
+    $phid = $request->getValue('phid');
+
+    $file = id(new PhabricatorFile())->loadOneWhere(
+      'phid = %s',
+      $phid);
+    if (!$file) {
+      throw new ConduitException('ERR-BAD-PHID');
+    }
+
+    return base64_encode($file->loadFileData());
+  }
+
+}
