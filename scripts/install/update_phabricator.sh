6a3eb19876213c70689ff498e88d502c4db42015
epriestley <git@epriestley.com>
2011-06-29 19:45:12 -0700

    Provide a basic update script for Phabricator
    Summary:
    This isn't completely cross-system compatible but it's definitely better than
    nothing.
    
    Test Plan:
    Pushed secure.phabricator.com a bunch of times.
    
    Reviewed By: moskov
    Reviewers: moskov
    CC: aran, moskov
    Differential Revision: 558

---
+++ scripts/install/update_phabricator.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+set -e
+set -x
+
+# This is an example script for updating Phabricator, similar to the one used to
+# update <https://secure.phabricator.com/>. It might not work perfectly on your
+# system, but hopefully it should be easy to adapt.
+
+# NOTE: This script assumes you are running it from a directory which contains
+# arcanist/, libphutil/, phabricator/, and possibly diviner/.
+
+ROOT=`pwd` # You can hard-code the path here instead.
+
+
+### UPDATE WORKING COPIES ######################################################
+
+if [ -e $ROOT/diviner ]
+then
+  (cd $ROOT/diviner && git pull)
+fi
+
+(cd $ROOT/libphutil && git pull)
+(cd $ROOT/arcanist && git pull)
+(cd $ROOT/phabricator && git pull && git submodule update --init)
+
+
+### GENERATE DOCUMENTATION #####################################################
+
+# This generates documentation if you have diviner/ checked out. You generally
+# don't need to do this unless you're contributing to Phabricator and want to
+# preview some of the amazing documentation you've just written.
+if [ -e $ROOT/diviner ]
+then
+  (cd $ROOT/diviner && $ROOT/diviner/bin/diviner .)
+  (cd $ROOT/libphutil && $ROOT/diviner/bin/diviner .)
+  (cd $ROOT/arcanist && $ROOT/diviner/bin/diviner .)
+  (cd $ROOT/phabricator && $ROOT/diviner/bin/diviner .)
+fi
+
+### CYCLE APACHE AND DAEMONS ###################################################
+
+# Stop Apache. Depening on what system you're running, you may need to use
+# 'apachectl' or something else to cycle apache.
+sudo /etc/init.d/httpd stop
+
+# Stop daemons.
+$ROOT/phabricator/bin/phd stop
+
+# Upgrade the database schema.
+$ROOT/phabricator/scripts/sql/upgrade_schema.php -f
+
+# Restart apache.
+sudo /etc/init.d/httpd start
+
+# Restart daemons. Customize this to start whatever daemons you're running on
+# your system.
+
+# $ROOT/phabricator/bin/phd repository-launch-master
+# $ROOT/phabricator/bin/phd launch metamta
+# $ROOT/phabricator/bin/phd launch 4 taskmaster
+# $ROOT/phabricator/bin/phd launch ircbot /config/bot.json
