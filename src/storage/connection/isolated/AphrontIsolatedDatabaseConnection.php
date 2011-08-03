29444d1df364523702daf1b1144fde5858f1a5a0
epriestley <git@epriestley.com>
2011-08-03 08:17:53 -0700

    Add a little more unit test documentation, fail loudly when isolation prevents a query
    Summary:
      - Provide an example unit test, and document it.
      - Document database isolation better.
      - When we issue an unsimulated query to the isolated connection, throw a
    helpful message.
      - Pygments is complaining about my madeup "lang=demo", change it to
    "lang=text".
    
    Test Plan:
      - Ran the unit test (sanity check).
      - Ran all other unit tests (verify I didn't break isolation).
      - Added a queryfx(..., 'SELECT 1') to a test and verified it throws.
      - Read the documentation.
    
    Reviewed By: edward
    Reviewers: edward, jungejason, tuomaspelkonen, aran
    CC: aran, edward
    Differential Revision: 773

--- src/storage/connection/isolated/AphrontIsolatedDatabaseConnection.php
+++ src/storage/connection/isolated/AphrontIsolatedDatabaseConnection.php
@@ -73,6 +73,18 @@ class AphrontIsolatedDatabaseConnection extends AphrontDatabaseConnection {
 
   public function executeRawQuery($raw_query) {
 
+    // NOTE: "[\s<>K]*" allows any number of (properly escaped) comments to
+    // appear prior to the INSERT/UPDATE, since this connection escapes them
+    // as "<K>" (above).
+    if (!preg_match('/^[\s<>K]*(INSERT|UPDATE)\s*/i', $raw_query)) {
+      $doc_uri = PhabricatorEnv::getDoclink('article/Writing_Unit_Tests.html');
+      throw new Exception(
+        "Database isolation currently only supports INSERT and UPDATE ".
+        "queries. For more information, see <{$doc_uri}>. You are trying to ".
+        "issue a query which does not begin with INSERT or UPDATE: ".
+        "'".$raw_query."'");
+    }
+
     // NOTE: This method is intentionally simplified for now, since we're only
     // using it to stub out inserts/updates. In the future it will probably need
     // to grow more powerful.
--- src/storage/connection/isolated/__init__.php
+++ src/storage/connection/isolated/__init__.php
@@ -6,6 +6,7 @@
 
 
 
+phutil_require_module('phabricator', 'infrastructure/env');
 phutil_require_module('phabricator', 'storage/connection/base');
 
 phutil_require_module('phutil', 'utils');
