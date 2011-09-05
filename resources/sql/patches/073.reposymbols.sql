cd05c960ff50621df0c8fe26c555bb8499dd8001
epriestley <git@epriestley.com>
2011-09-04 17:31:16 -0700

    Add storage for repository symbol tracking
    Summary: See T315 for an extensive description of this feature. Adds the
    descibed storage table.
    
    Test Plan: Used phpsh to read/write symbol objects.
    
    Reviewers: jungejason, nh, tuomaspelkonen, aran
    
    Reviewed By: tuomaspelkonen
    
    CC: aran, epriestley, tuomaspelkonen
    
    Differential Revision: 897

---
+++ resources/sql/patches/073.reposymbols.sql
@@ -0,0 +1,9 @@
+CREATE TABLE phabricator_repository.repository_symbol (
+  arcanistProjectID INT UNSIGNED NOT NULL,
+  symbolName varchar(128) NOT NULL,
+  KEY (symbolName),
+  symbolType varchar(12) BINARY NOT NULL,
+  symbolLanguage varchar(32) BINARY NOT NULL,
+  pathID INT UNSIGNED NOT NULL,
+  lineNumber INT UNSIGNED NOT NULL
+) ENGINE=InnoDB;
