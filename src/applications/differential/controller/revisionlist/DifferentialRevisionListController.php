369079dd452258b30c1c019af8a9018353701bd9
Nick Harper <nharper@nharper.org>
2011-12-22 19:37:36 -0800

    Change default status to 'all' in DifferentialRevisionList
    Summary: Makes it easier to discover the list of all revisions for a user.
    
    Test Plan:
    Opened up /differential/filter/revisions/, and saw that it defaulted
    to status of all. Clicked between tabs, and it stayed on all. Selected
    open, it only displayed open revisions, including as I switched between
    tabs.
    
    Reviewers: epriestley, jungejason
    
    Reviewed By: epriestley
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1278

--- src/applications/differential/controller/revisionlist/DifferentialRevisionListController.php
+++ src/applications/differential/controller/revisionlist/DifferentialRevisionListController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,7 +61,7 @@ class DifferentialRevisionListController extends DifferentialController {
     // parameters are missing.
     $params += array(
       'phid' => $user->getPHID(),
-      'status' => 'open',
+      'status' => 'all',
       'order' => 'modified',
     );
 
@@ -314,8 +314,8 @@ class DifferentialRevisionListController extends DifferentialController {
       case 'status':
         $links = $this->renderToggleButtons(
           array(
-            'open' => 'Open',
             'all'  => 'All',
+            'open' => 'Open',
           ),
           $params['status'],
           $uri,
