beaee478d360d87a14c8707483c3b16cb327d578
kevin <kevin@tenxer.com>
2012-01-10 19:51:55 -0800

    Added conduit method to get maniphest transactions
    Summary:
    Added a Conduit API method to return all transactions for a
    given set of task_ids. This will be used to comments and other important
    information about the tasks.
    
    Test Plan:
    Use Conduit to execute ##maniphest.gettasktransactions## and
    visually verify that transaction information is returned.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1361

---
+++ src/applications/conduit/method/maniphest/gettasktransactions/ConduitAPI_maniphest_gettasktransactions_Method.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * Copyright 2012 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @group maniphest
+ */
+class ConduitAPI_maniphest_gettasktransactions_Method
+  extends ConduitAPI_maniphest_Method {
+
+  public function getMethodDescription() {
+    return "Retrieve Maniphest Task Transactions.";
+  }
+
+  public function defineParamTypes() {
+    return array(
+      'ids' => 'required list<int>',
+    );
+  }
+
+  public function defineReturnType() {
+    return 'nonempty list<dict<string, wild>>';
+  }
+
+  public function defineErrorTypes() {
+    return array(
+    );
+  }
+
+  protected function execute(ConduitAPIRequest $request) {
+    $results = array();
+    $task_ids = $request->getValue('ids');
+
+    if (!$task_ids) {
+      return $results;
+    }
+
+    $transactions = id(new ManiphestTransaction())->loadAllWhere(
+      'taskID IN (%Ld) ORDER BY id ASC',
+      $task_ids);
+
+    foreach ($transactions as $transaction) {
+      $task_id = $transaction->getTaskID();
+      if (!array_key_exists($task_id, $results)) {
+        $results[$task_id] = array();
+      }
+      $results[$task_id][] = array(
+        'taskID'  => $task_id,
+        'transactionType'  => $transaction->getTransactionType(),
+        'oldValue'  => $transaction->getOldValue(),
+        'newValue'  => $transaction->getNewValue(),
+        'comments'      => $transaction->getComments(),
+        'authorPHID'  => $transaction->getAuthorPHID(),
+        'dateCreated' => $transaction->getDateCreated(),
+      );
+    }
+
+    return $results;
+  }
+}
