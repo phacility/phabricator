13bf353f14251f0568ef824465c422946fdb1c30
epriestley <git@epriestley.com>
2012-01-13 12:07:50 -0800

    Shorten Phriction slug field to 256 to avoid key length issues in UTF-8 default databases
    Summary: This is a stopgap patch until we fix T345/T327 properly, but you can't
    add a 512-character key to a UTF-8 column because it creates an index on more
    than 767 bytes, which is the key length limit.
    
    Test Plan: Ran schmea upgrade script.
    
    Reviewers: indiefan, btrahan, jungejason
    
    Reviewed By: jungejason
    
    CC: aran, epriestley, xela, jungejason
    
    Maniphest Tasks: T327
    
    Differential Revision: https://secure.phabricator.com/D1391

--- resources/sql/patches/060.phriction.sql
+++ resources/sql/patches/060.phriction.sql
@@ -4,7 +4,7 @@ CREATE TABLE phabricator_phriction.phriction_document (
   id INT UNSIGNED NOT NULL,
   phid VARCHAR(64) BINARY NOT NULL,
   UNIQUE KEY (phid),
-  slug VARCHAR(512) NOT NULL,
+  slug VARCHAR(256) NOT NULL,
   UNIQUE KEY (slug),
   depth INT UNSIGNED NOT NULL,
   UNIQUE KEY (depth, slug),
---
+++ resources/sql/patches/094.phrictioncolumn.sql
@@ -0,0 +1,2 @@
+ALTER TABLE phabricator_phriction.phriction_document
+  CHANGE slug slug VARCHAR(256) NOT NULL;
