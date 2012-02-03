c0efecb561943b04f7973aeff987ededeedfc077
vrana <jakubv@fb.com>
2012-02-03 10:07:43 -0800

    Specify encoding in <meta>
    Summary: Phabricator sends information about encoding in Content-Type header but
    when I save the HTML page then this information is lost.
    
    Test Plan: /
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1561

--- src/view/page/base/AphrontPageView.php
+++ src/view/page/base/AphrontPageView.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -68,6 +68,7 @@ class AphrontPageView extends AphrontView {
 <!DOCTYPE html>
 <html>
   <head>
+    <meta charset="UTF-8" />
     <title>{$title}</title>
     {$head}
   </head>
