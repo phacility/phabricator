47631530a550aff0bf31334177a7522de813611a
epriestley <git@epriestley.com>
2012-02-06 19:31:20 -0800

    Remove admin requirement from MetaMTASendGridReceiveController
    Summary:
    This got caught in the crossfire when we admin-only'd the whole MetaMTA tool. It
    should not be admin only.
    
    (Generally, we should probably separate this out better at some point.)
    
    Test Plan: Hit /mail/sendgrid/ as a logged-out, non-admin user (like SendGrid
    does).
    
    Reviewers: s, btrahan
    
    Reviewed By: s
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1588

--- src/applications/metamta/controller/sendgridreceive/PhabricatorMetaMTASendGridReceiveController.php
+++ src/applications/metamta/controller/sendgridreceive/PhabricatorMetaMTASendGridReceiveController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,10 @@ class PhabricatorMetaMTASendGridReceiveController
   extends PhabricatorMetaMTAController {
 
   public function shouldRequireLogin() {
+    return false;
+  }
+
+  public function shouldRequireAdmin() {
     return false;
   }
 
