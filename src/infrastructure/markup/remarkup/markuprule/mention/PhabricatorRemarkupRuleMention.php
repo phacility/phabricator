9b318e4044c8ff27699d2294fbd780d04ad9c128
vrana <jakubv@fb.com>
2012-02-28 15:04:36 -0800

    Respect username letter case in Remarkup
    Test Plan:
    Type ##@makinde## to comment, verify that it is converted to ##@Makinde##.
    Verify that ##@NonExistent## stays ##@NonExistent##.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1725

--- src/infrastructure/markup/remarkup/markuprule/mention/PhabricatorRemarkupRuleMention.php
+++ src/infrastructure/markup/remarkup/markuprule/mention/PhabricatorRemarkupRuleMention.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -108,11 +108,11 @@ class PhabricatorRemarkupRuleMention
           'a',
           array(
             'class'   => $class,
-            'href'    => '/p/'.$username.'/',
+            'href'    => '/p/'.$actual_users[$username]['username'].'/',
             'target'  => '_blank',
             'title'   => $actual_users[$username]['realName'],
           ),
-          phutil_escape_html('@'.$username));
+          phutil_escape_html('@'.$actual_users[$username]['username']));
         foreach ($tokens as $token) {
           $engine->overwriteStoredText($token, $tag);
         }
