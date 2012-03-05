6ddb98f8045c8a2b09cdcb103d19e9cf9fed0a37
Nick Harper <nharper@fb.com>
2012-03-05 11:02:32 -0800

    Add retry count to meta mta view
    Summary:
    Show the retry count in the meta mta view (in addition to the list of
    messages) - I find this info useful when I'm trying to debug what's going on
    with mail failures.
    
    Task ID: #
    
    Blame Rev:
    
    Test Plan:
    loaded /mail/view/NNNNN/ and saw the retry count
    
    Revert Plan:
    
    Tags:
    
    Reviewers: epriestley, btrahan, jungejason
    
    Reviewed By: epriestley
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1782

--- src/applications/metamta/controller/view/PhabricatorMetaMTAViewController.php
+++ src/applications/metamta/controller/view/PhabricatorMetaMTAViewController.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -52,6 +52,10 @@ class PhabricatorMetaMTAViewController extends PhabricatorMetaMTAController {
         id(new AphrontFormStaticControl())
           ->setLabel('Status')
           ->setValue($status))
+      ->appendChild(
+        id(new AphrontFormStaticControl())
+          ->setLabel('Retry Count')
+          ->setValue($mail->getRetryCount()))
       ->appendChild(
         id(new AphrontFormStaticControl())
           ->setLabel('Message')
