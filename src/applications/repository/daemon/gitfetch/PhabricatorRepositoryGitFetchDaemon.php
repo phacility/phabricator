89dac1cf195a2f709030c7a03e8a2378b83e54a7
epriestley <git@epriestley.com>
2012-03-08 13:24:25 -0800

    When updating git repositories, use --prune to prune old branches
    Summary: We'll keep deleted branches around right now because Git's behavior is not to remove them without --prune.
    
    Test Plan: Ran "git fetch --all --prune" to make sure it at least ostensibly works.
    
    Reviewers: btrahan, 20after4
    
    Reviewed By: 20after4
    
    CC: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D1833

--- src/applications/repository/daemon/gitfetch/PhabricatorRepositoryGitFetchDaemon.php
+++ src/applications/repository/daemon/gitfetch/PhabricatorRepositoryGitFetchDaemon.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -38,7 +38,7 @@ final class PhabricatorRepositoryGitFetchDaemon
     $local_path) {
 
     // This is a local command, but needs credentials.
-    $future = $repository->getRemoteCommandFuture('fetch --all');
+    $future = $repository->getRemoteCommandFuture('fetch --all --prune');
     $future->setCWD($local_path);
     $future->resolvex();
   }
