ef74b9b88b87f5be5a4cad55872d60e3a6085e64
epriestley <git@epriestley.com>
2012-03-12 10:34:27 -0700

    Improve messaging around passworded SSH keys
    Summary: It would be better to test if a key is passworded, but I couldn't figure out how to do that.
    
    Test Plan: Ran "test_connection.php"
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran, epriestley
    
    Maniphest Tasks: T924
    
    Differential Revision: https://secure.phabricator.com/D1856

--- scripts/repository/test_connection.php
+++ scripts/repository/test_connection.php
@@ -2,7 +2,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -56,6 +56,21 @@ if (!$repository) {
 $vcs = $repository->getVersionControlSystem();
 
 PhutilServiceProfiler::installEchoListener();
+
+echo phutil_console_format(
+  "\n".
+  "**NOTE:** If you are prompted for an SSH password in the next step, the\n".
+  "daemon won't work because it doesn't have the password and can't respond\n".
+  "to an interactive prompt. Instead of typing the password, it will hang\n".
+  "forever when prompted. There are several ways to resolve this:\n\n".
+  "  - Run the daemon inside an ssh-agent session where you have unlocked\n".
+  "    the key (most secure, but most complicated).\n".
+  "  - Generate a new, passwordless certificate for the daemon to use\n".
+  "    (usually quite easy).\n".
+  "  - Remove the passphrase from the key with `ssh-keygen -p`\n".
+  "    (easy, but questionable).");
+
+phutil_console_confirm('Did you read all that?', $default_no = false);
 
 echo "Trying to connect to the remote...\n";
 switch ($vcs) {
