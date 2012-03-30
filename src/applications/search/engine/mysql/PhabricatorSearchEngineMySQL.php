99704ed4851388e8a1561fb08e5f97ed26211d56
vrana <jakubv@fb.com>
2012-03-30 16:49:06 -0700

    Support operators in Phabricator search
    Summary:
    Boolean search supports operators, such as phrase search.
    It can be further improved by setting [[http://dev.mysql.com/doc/mysql/en/server-system-variables.html#sysvar_ft_boolean_syntax | ft_boolean_syntax]] to `' |-><()~*:""&^'` (note the leading space):
    Default value uses no operator for "optional word" and `+` for "mandatory word".
    This value uses no operator for "mandatory word" and `|` for "optional word".
    
    Test Plan: Search for "Enter the name" (with quotes).
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D2064

--- src/applications/search/engine/mysql/PhabricatorSearchEngineMySQL.php
+++ src/applications/search/engine/mysql/PhabricatorSearchEngineMySQL.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright 2011 Facebook, Inc.
+ * Copyright 2012 Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -180,7 +180,7 @@ final class PhabricatorSearchEngineMySQL extends PhabricatorSearchEngine {
         "{$t_field} field ON field.phid = document.phid");
       $where[] = qsprintf(
         $conn_r,
-        'MATCH(corpus) AGAINST (%s)',
+        'MATCH(corpus) AGAINST (%s IN BOOLEAN MODE)',
         $q);
 
       // When searching for a string, promote user listings above other
