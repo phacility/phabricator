5885764728e9a643b8ca167cdd0f169c14ba5989
vrana <jakubv@fb.com>
2012-04-03 11:19:58 -0700

    Allow IPv6
    Test Plan:
    /people/logs/
    Search for `2620:0:1cfe:`.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, jungejason
    
    Differential Revision: https://secure.phabricator.com/D2089

---
+++ resources/sql/patches/125.ipv6.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `phabricator_user`.`user_log`
+  -- 45 is length of "0000:0000:0000:0000:0000:0000:255.255.255.255".
+  MODIFY `remoteAddr` varchar(45) COLLATE 'utf8_general_ci' NOT NULL;
