8f70d891fa8d78f5b2c020433b1ddb401c4a3ca0
Edward Speyer <edward@fb.com>
2012-04-09 17:15:02 -0700

    AphrontFormSelectControl: Add <optgroup> to <select>
    Summary:
    Add <optgroup> style selects, if the array of options is actually an
    array-of-arrays.
    
    Test Plan: Made one, it looked OK.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D2177

--- src/view/form/control/select/AphrontFormSelectControl.php
+++ src/view/form/control/select/AphrontFormSelectControl.php
@@ -49,21 +49,35 @@ final class AphrontFormSelectControl extends AphrontFormControl {
     array $options,
     array $attrs = array()) {
 
-    $option_tags = array();
-    foreach ($options as $value => $label) {
-      $option_tags[] = phutil_render_tag(
-        'option',
-        array(
-          'selected' => ($value == $selected) ? 'selected' : null,
-          'value'    => $value,
-        ),
-        phutil_escape_html($label));
-    }
+    $option_tags = self::renderOptions($selected, $options);
 
     return javelin_render_tag(
       'select',
       $attrs,
       implode("\n", $option_tags));
+  }
+
+  private static function renderOptions($selected, array $options) {
+    $tags = array();
+    foreach ($options as $value => $thing) {
+      if (is_array($thing)) {
+        $tags[] = phutil_render_tag(
+          'optgroup',
+          array(
+            'label' => $value,
+          ),
+          implode("\n", self::renderOptions($selected, $thing)));
+      } else {
+        $tags[] = phutil_render_tag(
+          'option',
+          array(
+            'selected' => ($value == $selected) ? 'selected' : null,
+            'value'    => $value,
+          ),
+          phutil_escape_html($thing));
+      }
+    }
+    return $tags;
   }
 
 }
