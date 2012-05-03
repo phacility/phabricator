21d278eaa740b37610d5367951538f52f3e981e4
Craig Silverstein <csilvers@khanacademy.org>
2012-05-02 19:29:45 -0700

    Allow google logins.  And get rid of a conf file that proved not-so-useful.
    I added a new file to hold secret information that shouldn't be in
    source control.

---
+++ conf/custom/khan-google.conf.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+
+  // To populate this file, go to https://code.google.com/apis/console/,
+  // click on 'api access' and then 'craete client id':
+  //   name='Phabricator login'
+  //   logo='http://phabricator.khanacademy.org/favicon.ico'
+  //   [click next]
+  //   your site='[https://]phabriactor.khanacademy.org'
+  // This will lead to a page with a client id, and client secret.
+
+  'google.application-id' => 'CLIENT ID GOES HERE',
+  'google.application-secret' => 'CLIENT SECRET GOES HERE',
+
+)
