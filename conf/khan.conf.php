53f5a46f623c13f8fc925b59009bf84d43a85bf5
Craig Silverstein <csilvers@khanacademy.org>
2012-05-03 17:08:31 -0700

    Finish up configuring phabricator for our local setup.

--- conf/khan.conf.php
+++ conf/khan.conf.php
@@ -9,26 +9,44 @@ return array(
   // Use "https://" if you have SSL. See below for some notes.
   'phabricator.base-uri' => 'http://phabricator.khanacademy.org/',
 
-  // Connection information for MySQL.
-  'mysql.host' => 'localhost',
-  'mysql.user' => 'phabricator',
-  'mysql.pass' => 'codereview',
-
-  // Basic email domain configuration.
-  'metamta.default-address' => 'noreply@phabricator.khanacademy.org',
-  'metamta.domain'          => 'phabricator.khanacademy.org',
-
-  // This apparently avoids some cookie-based attacks.
-  'security.alternate-file-domain'  => 'http://phabricator-files.khanacademy.org/',
-
-  // Timezone for khan academy.
-  'phabricator.timezone'    => 'America/Los_Angeles',
+  // Allow, but don't require, a user to say how they did testing.
+  'differential.require-test-plan-field' => true,
 
   // Allow logins via google.
   'google.auth-enabled' => true,
   'google.auth-permanent' => true,
   // Allow anyone with a google account to register.
   'google.registration-enabled' => true,
+
+  // Logging.
+  'log.access.path'             => '/home/ubuntu/logs/phabricator.log',
+
+  // Basic email domain configuration.
+  'metamta.default-address' => 'noreply@phabricator.khanacademy.org',
+  'metamta.domain'          => 'phabricator.khanacademy.org',
+  'metamta.can-send-as-user'    => true,
+  // gmail threading will break if the subject changes.
+  'metamta.vary-subjects' => false,
+
+  // Connection information for MySQL.
+  'mysql.host' => 'localhost',
+  'mysql.user' => 'phabricator',
+  'mysql.pass' => 'codereview',
+
+  // Timezone for khan academy.
+  'phabricator.timezone'    => 'America/Los_Angeles',
+  'phabricator.csrf-key'    => '0016ee009c31da52bc9044dd5a989ff1ec6da80',
+
+  // source-code highlighting is the bomb
+  'pygments.enabled'            => true,
+
+  // TODO(csilvers): enable recaptcha if brute-force attacks become a problem.
+
+  // Docs say this is "pretty silly (but sort of awesome)". Good enough for me.
+  'remarkup.enable-embedded-youtube' => true,
+
+  // This apparently avoids some cookie-based attacks.
+  'security.alternate-file-domain'  => 'http://phabricator-files.khanacademy.org/',
 
 ) + phabricator_read_config_file('custom/khan-google.conf.php')
   + phabricator_read_config_file('production');
