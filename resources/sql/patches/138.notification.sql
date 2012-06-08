207f101aee6a633f068548d9096c1df9a1fab2a4
Keebuhm Park <keebuhm@gmail.com>
2012-06-08 12:40:26 -0700

    SQL patch for notification
    Summary: Added `PhabricatorBuiltinPatchList` entry so that "storage upgrade" will update the database. Renamed and numbered the notification.sql patch.
    
    Test Plan: Drop phabricator_feed.feed_storynotification table if it exists and run bin/storage upgrade to check if the patch is correctly applied.
    
    Reviewers: epriestley, btrahan, allenjohnashton
    
    Reviewed By: epriestley
    
    CC: ddfisher, aran, Korvin
    
    Differential Revision: https://secure.phabricator.com/D2687

--- resources/sql/patches/138.notification.sql
+++ resources/sql/patches/138.notification.sql
@@ -1,8 +1,8 @@
-CREATE TABLE if not exists phabricator_feed.feed_storynotification (
+CREATE TABLE {$NAMESPACE}_feed.feed_storynotification (
   userPHID varchar(64) not null collate utf8_bin,
   primaryObjectPHID varchar(64) not null collate utf8_bin,
   chronologicalKey BIGINT UNSIGNED NOT NULL,
   hasViewed boolean not null,
   UNIQUE KEY (userPHID, chronologicalKey),
   KEY (userPHID, hasViewed, primaryObjectPHID)
-);
+) ENGINE=InnoDB, COLLATE utf8_general_ci;
