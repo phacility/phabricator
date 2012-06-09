1f3b058daaf1448f26670984203e6a1711337ce7
John-Ashton Allen <allenjohnashton@gmail.com>
2012-06-08 20:24:48 -0700

    Implemented fallback for unimplemented notifications with silly message.
    Summary:
    Most notifications are not implemented, a fallback message should
    be displayed for these cases.
    
    Test Plan: Commented out renderNotificationView in PhabricatorFeedStoryManiphest and watched the notifications render.
    
    Reviewers: epriestley, btrahan
    
    Reviewed By: epriestley
    
    CC: ddfisher, keebuhm, aran, Korvin
    
    Differential Revision: https://secure.phabricator.com/D2700

--- src/applications/feed/story/PhabricatorFeedStory.php
+++ src/applications/feed/story/PhabricatorFeedStory.php
@@ -29,8 +29,11 @@ abstract class PhabricatorFeedStory {
 
   abstract public function renderView();
 
-//  TODO: Restore this once subclasses actually implement it.
-//  abstract public function renderNotificationView();
+//  TODO: Make abstract once all subclasses implement it.
+  public function renderNotificationView() {
+    return id(new PhabricatorFeedStoryUnknown($this->data))
+      ->renderNotificationView();
+  }
 
   public function getRequiredHandlePHIDs() {
     return array();
