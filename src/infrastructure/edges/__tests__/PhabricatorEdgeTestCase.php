1089a48d4a66f01401049816ed88e3be034f379b
epriestley <git@epriestley.com>
2012-07-09 10:39:38 -0700

    Allow edges to be configured to prevent cycles
    Summary:
    Certain types of things we should be storing in edges (notably, Task X depends on Task Y) should always be acyclic. Allow `PhabricatorEdgeEditor` to enforce this, since we can't correctly enforce it outside of the editor without being vulnerable to races.
    
    Each edge type can be marked acyclic. If an edge type is acyclic, we perform additional steps when writing new edges of that type:
    
      - We acquire a global lock on the edge type before performing any reads or writes. This ensures we can't produce a cycle as a result of a race where two edits add edges which independently do not produce a cycle, but do produce a cycle when combined.
      - After performing writes but before committing transactions, we load the edge graph for each acyclic type and verify that it is, in fact, acyclic. If we detect cycles, we abort the edit.
      - When we're done, we release the edge type locks.
    
    This is a relatively high-complexity change, but gives us a simple way to flag an edge type as acyclic in a robust way.
    
    Test Plan: Ran unit tests.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T1162
    
    Differential Revision: https://secure.phabricator.com/D2940

---
+++ src/infrastructure/edges/__tests__/PhabricatorEdgeTestCase.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * Copyright 2012 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+final class PhabricatorEdgeTestCase extends PhabricatorTestCase {
+
+  protected function getPhabricatorTestCaseConfiguration() {
+    return array(
+      self::PHABRICATOR_TESTCONFIG_BUILD_STORAGE_FIXTURES => true,
+    );
+  }
+
+  public function testCycleDetection() {
+
+    // The editor should detect that this introduces a cycle and prevent the
+    // edit.
+
+    $user = new PhabricatorUser();
+
+    $obj1 = id(new HarbormasterObject())->save();
+    $obj2 = id(new HarbormasterObject())->save();
+    $phid1 = $obj1->getPHID();
+    $phid2 = $obj2->getPHID();
+
+    $editor = id(new PhabricatorEdgeEditor())
+      ->setUser($user)
+      ->addEdge($phid1, PhabricatorEdgeConfig::TYPE_TEST_NO_CYCLE, $phid2)
+      ->addEdge($phid2, PhabricatorEdgeConfig::TYPE_TEST_NO_CYCLE, $phid1);
+
+    $caught = null;
+    try {
+      $editor->save();
+    } catch (Exception $ex) {
+      $caught = $ex;
+    }
+
+    $this->assertEqual(
+      true,
+      $caught instanceof Exception);
+
+
+    // The first edit should go through (no cycle), bu the second one should
+    // fail (it introduces a cycle).
+
+    $editor = id(new PhabricatorEdgeEditor())
+      ->setUser($user)
+      ->addEdge($phid1, PhabricatorEdgeConfig::TYPE_TEST_NO_CYCLE, $phid2)
+      ->save();
+
+    $editor = id(new PhabricatorEdgeEditor())
+      ->setUser($user)
+      ->addEdge($phid2, PhabricatorEdgeConfig::TYPE_TEST_NO_CYCLE, $phid1);
+
+    $caught = null;
+    try {
+      $editor->save();
+    } catch (Exception $ex) {
+      $caught = $ex;
+    }
+
+    $this->assertEqual(
+      true,
+      $caught instanceof Exception);
+  }
+
+
+}
