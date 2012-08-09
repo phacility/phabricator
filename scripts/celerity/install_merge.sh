80d9a276f3217a5758d17976172ae3f4356b3e08
vrana <jakubv@fb.com>
2012-08-09 15:44:09 -0700

    Automatically resolve merge conflicts in Celerity map
    Summary:
    We can use `.gitattributes` instead but there's no way how to set repository config for all users in Git, right?
    So provide a script writting to `.git/info/attributes` instead so that we don't have to .gitignore `.gitattributes`.
    
    Test Plan:
      $ scripts/celerity/install_merge.sh
      $ git pull # with merge conflict in Celerity map
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, Korvin
    
    Differential Revision: https://secure.phabricator.com/D3228

---
+++ scripts/celerity/install_merge.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+echo "src/__celerity_resource_map__.php merge=celerity" \
+  >> `dirname "$0"`/../../.git/info/attributes
+
+git config merge.celerity.name "Celerity Mapper"
+
+git config merge.celerity.driver \
+  '$GIT_DIR/../scripts/celerity_mapper.php $GIT_DIR/../webroot'
