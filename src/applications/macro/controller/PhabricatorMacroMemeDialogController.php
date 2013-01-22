4f3fafd2e91abc88074560b5887396024fe2402b
epriestley <git@epriestley.com>
2013-01-21 18:46:04 -0800

    Create a basic meme dialog
    Summary: Simple dialog for creating memes. We can add more features (typeahead, selection thumbs, preview) later.
    
    Test Plan: {F29815}
    
    Reviewers: DeedyDas, chad
    
    Reviewed By: chad
    
    CC: aran
    
    Maniphest Tasks: T2353
    
    Differential Revision: https://secure.phabricator.com/D4557

---
+++ src/applications/macro/controller/PhabricatorMacroMemeDialogController.php
@@ -0,0 +1,66 @@
+<?php
+
+final class PhabricatorMacroMemeDialogController
+  extends PhabricatorMacroController {
+
+  public function processRequest() {
+    $request = $this->getRequest();
+    $user = $request->getUser();
+
+    $name = $request->getStr('macro');
+    $above = $request->getStr('above');
+    $below = $request->getStr('below');
+
+    $e_macro = true;
+    $errors = array();
+    if ($request->isDialogFormPost()) {
+      if (!$name) {
+        $e_macro = pht('Required');
+        $errors[] = pht('Macro name is required.');
+      } else {
+        $macro = id(new PhabricatorFileImageMacro())->loadOneWhere(
+          'name = %s',
+          $name);
+        if (!$macro) {
+          $e_macro = pht('Invalid');
+          $errors[] = pht('No such macro.');
+        }
+      }
+
+      if (!$errors) {
+        $result = array(
+          'text' => "{meme, src={$name}, above={$above}, below={$below}}",
+        );
+        return id(new AphrontAjaxResponse())->setContent($result);
+      }
+    }
+
+    $view = id(new AphrontFormLayoutView())
+      ->appendChild(
+        id(new AphrontFormTextControl())
+          ->setLabel(pht('Macro'))
+          ->setName('macro')
+          ->setValue($name)
+          ->setError($e_macro))
+      ->appendChild(
+        id(new AphrontFormTextControl())
+          ->setLabel(pht('Above'))
+          ->setName('above')
+          ->setValue($above))
+      ->appendChild(
+        id(new AphrontFormTextControl())
+          ->setLabel(pht('Below'))
+          ->setName('below')
+          ->setValue($below));
+
+    $dialog = id(new AphrontDialogView())
+      ->setUser($user)
+      ->setTitle(pht('Create Meme'))
+      ->appendChild($view)
+      ->addCancelButton('/')
+      ->addSubmitButton(pht('rofllolo!!~'));
+
+    return id(new AphrontDialogResponse())->setDialog($dialog);
+  }
+
+}
