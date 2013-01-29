fc4cb5735702324b502ceecd5825a93406596a3e
epriestley <git@epriestley.com>
2013-01-28 18:11:27 -0800

    Fix JSON encoding of PhutilSafeHTML for browser consumption
    Summary:
    If you run this code:
    
      json_encode(array('tag' => phutil_tag('div', array())));
    
    ...you get this result, because json_encode() does not call toString() on objects:
    
      {"tag":{}}
    
    Instead, convert such objects to their underlying strings. Javelin has support for JX.HTML and for implicit conversion (which is kind of sketchy for other reasons) but it's sort of complicated (only happens on Ajax, not behaviors) and messy (not metadata-based), so ignore it for now.
    
    We'll need to do something similar for serialization to the database. My plan there is just to throw on any objects. The only time we put HTML in the database is cache-related and those tiny number of callsites can manually handle it.
    
    Test Plan: Various ajax things now receive the correct data.
    
    Reviewers: vrana
    
    Reviewed By: vrana
    
    CC: aran
    
    Maniphest Tasks: T2432
    
    Differential Revision: https://secure.phabricator.com/D4684

--- src/infrastructure/celerity/CelerityStaticResourceResponse.php
+++ src/infrastructure/celerity/CelerityStaticResourceResponse.php
@@ -128,7 +128,8 @@ final class CelerityStaticResourceResponse {
   public function renderHTMLFooter() {
     $data = array();
     if ($this->metadata) {
-      $json_metadata = json_encode($this->metadata);
+      $json_metadata = AphrontResponse::encodeJSONForHTTPResponse(
+        $this->metadata);
       $this->metadata = array();
     } else {
       $json_metadata = '{}';
@@ -164,7 +165,9 @@ final class CelerityStaticResourceResponse {
         if (!$group) {
           continue;
         }
-        $onload[] = 'JX.initBehaviors('.json_encode($group).')';
+        $group_json = AphrontResponse::encodeJSONForHTTPResponse(
+          $group);
+        $onload[] = 'JX.initBehaviors('.$group_json.')';
       }
     }
 
