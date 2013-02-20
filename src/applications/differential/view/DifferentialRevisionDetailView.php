2191e99b49a2ade7ae723b5c2e100bde303008ea
vrana <jakubv@fb.com>
2013-02-19 16:08:04 -0800

    Delete unused variable

--- src/applications/differential/view/DifferentialRevisionDetailView.php
+++ src/applications/differential/view/DifferentialRevisionDetailView.php
@@ -67,7 +67,6 @@ final class DifferentialRevisionDetailView extends AphrontView {
 
     $next_step = null;
     if ($status == ArcanistDifferentialRevisionStatus::ACCEPTED) {
-      $command = null;
       switch ($local_vcs) {
         case PhabricatorRepositoryType::REPOSITORY_TYPE_MERCURIAL:
           $bookmark = $this->getDiff()->getBookmark();
