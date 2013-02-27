b5da7b37233824dd8c8540f37979f6085e274cba
epriestley <git@epriestley.com>
2013-02-27 07:18:30 -0800

    Missing change from D5120 which I incorrectly discarded in a merge.
    Auditors: vrana

--- src/applications/paste/remarkup/PhabricatorPasteRemarkupRule.php
+++ src/applications/paste/remarkup/PhabricatorPasteRemarkupRule.php
@@ -4,10 +4,24 @@
  * @group markup
  */
 final class PhabricatorPasteRemarkupRule
-  extends PhabricatorRemarkupRuleObjectName {
+  extends PhabricatorRemarkupRuleObject {
 
   protected function getObjectNamePrefix() {
     return 'P';
+  }
+
+  protected function loadObjects(array $ids) {
+    $viewer = $this->getEngine()->getConfig('viewer');
+
+    if (!$viewer) {
+      return array();
+    }
+
+    return id(new PhabricatorPasteQuery())
+      ->setViewer($viewer)
+      ->withIDs($ids)
+      ->execute();
+
   }
 
 }
--- src/applications/ponder/remarkup/PonderRemarkupRule.php
+++ src/applications/ponder/remarkup/PonderRemarkupRule.php
@@ -1,13 +1,36 @@
 <?php
 
-/**
- * @group markup
- */
 final class PonderRemarkupRule
-  extends PhabricatorRemarkupRuleObjectName {
+  extends PhabricatorRemarkupRuleObject {
 
   protected function getObjectNamePrefix() {
-    return 'Q(?![1-4]\b)';
+    return 'Q';
   }
+
+  protected function loadObjects(array $ids) {
+    $viewer = $this->getEngine()->getConfig('viewer');
+
+    if (!$viewer) {
+      return array();
+    }
+
+    return id(new PonderQuestionQuery())
+      ->setViewer($viewer)
+      ->withIDs($ids)
+      ->execute();
+  }
+
+  protected function shouldMarkupObject(array $params) {
+    // NOTE: Q1, Q2, Q3 and Q4 are often used to refer to quarters of the year;
+    // mark them up only in the {Q1} format.
+    if ($params['type'] == 'ref') {
+      if ($params['id'] <= 4) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
 }
 
