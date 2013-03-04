fd016b586485b518cccb52511bfc9948c12f190f
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-03-04 07:31:59 -0800

    Creating (or updating) a Phriction Document creates empty parent documents
    Summary:
    Fixes T2208
    
    Parent documents get created as stubs - while the action is "parent" - don't ask
    
    Test Plan: Created some documents with somewhat ancestry - verified their parents had pages and stub messages display ({F34456}).
    
    Reviewers: epriestley, btrahan
    
    Reviewed By: epriestley
    
    CC: aran, Korvin
    
    Maniphest Tasks: T2208
    
    Differential Revision: https://secure.phabricator.com/D5201

--- src/applications/phriction/constants/PhrictionDocumentStatus.php
+++ src/applications/phriction/constants/PhrictionDocumentStatus.php
@@ -7,11 +7,13 @@ final class PhrictionDocumentStatus extends PhrictionConstants {
 
   const STATUS_EXISTS     = 0;
   const STATUS_DELETED    = 1;
+  const STATUS_STUB       = 3;
 
   public static function getConduitConstant($const) {
     static $map = array(
       self::STATUS_EXISTS   => 'exists',
       self::STATUS_DELETED   => 'deleted',
+      self::STATUS_STUB      => 'stubbed',
     );
 
     return idx($map, $const, 'unknown');
--- src/applications/phriction/controller/PhrictionDocumentController.php
+++ src/applications/phriction/controller/PhrictionDocumentController.php
@@ -140,6 +140,14 @@ final class PhrictionDocumentController
           pht('This document has been deleted. You can edit it to put new '.
           'content here, or use history to revert to an earlier version.'));
         $core_content = $notice->render();
+      } else if ($doc_status == PhrictionDocumentStatus::STATUS_STUB) {
+        $notice = new AphrontErrorView();
+        $notice->setSeverity(AphrontErrorView::SEVERITY_NOTICE);
+        $notice->setTitle('Empty Document');
+        $notice->appendChild(
+          pht('This document is empty. You can edit it to put some proper '.
+          'content here.'));
+        $core_content = $notice->render();
       } else {
         throw new Exception("Unknown document status '{$doc_status}'!");
       }
@@ -244,14 +252,17 @@ final class PhrictionDocumentController
       'SELECT d.slug, d.depth, c.title FROM %T d JOIN %T c
         ON d.contentID = c.id
         WHERE d.slug LIKE %> AND d.depth IN (%d, %d)
-          AND d.status = %d
+          AND d.status IN (%Ld)
         ORDER BY d.depth, c.title LIMIT %d',
       $document_dao->getTableName(),
       $content_dao->getTableName(),
       ($slug == '/' ? '' : $slug),
       $d_child,
       $d_grandchild,
-      PhrictionDocumentStatus::STATUS_EXISTS,
+      array(
+        PhrictionDocumentStatus::STATUS_EXISTS,
+        PhrictionDocumentStatus::STATUS_STUB,
+      ),
       $limit);
 
     if (!$children) {
