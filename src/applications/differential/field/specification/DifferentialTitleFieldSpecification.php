05c5c43159dc1dae2a5b35fbe6960f86d8ead227
Afaque Hussain <Afaque.Hussain@outlook.com>
2013-03-13 09:34:23 -0700

    Yell at developers if they don't replace the default revision title with thier new revision title.
    Summary: Same as the title
    
    Test Plan: Will test locally by running arc diff
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, Korvin, AnhNhan, starruler
    
    Maniphest Tasks: T2663
    
    Differential Revision: https://secure.phabricator.com/D5297

--- src/applications/differential/field/specification/DifferentialTitleFieldSpecification.php
+++ src/applications/differential/field/specification/DifferentialTitleFieldSpecification.php
@@ -41,7 +41,16 @@ final class DifferentialTitleFieldSpecification
     if (!strlen($this->title)) {
       $this->error = 'Required';
       throw new DifferentialFieldValidationException(
-        "You must provide a title.");
+        "You must provide a revision title in the first line ".
+        "of your commit message.");
+    }
+
+    if (preg_match('/^<<.*>>$/', $this->title)) {
+      $default_title = self::getDefaultRevisionTitle();
+      $this->error = 'Required';
+      throw new DifferentialFieldValidationException(
+        "Replace the line '{$default_title}' with a revision title ".
+        "that describes the change.");
     }
   }
 
@@ -84,6 +93,10 @@ final class DifferentialTitleFieldSpecification
 
   public function getColumnClassForRevisionList() {
     return 'wide pri';
+  }
+
+  public static function getDefaultRevisionTitle() {
+    return '<<Replace this line with your Revision Title>>';
   }
 
   public function renderValueForRevisionList(DifferentialRevision $revision) {
