ecfb72071372b3abc9205e7b725c08a4d7ec42de
Anh Nhan Nguyen <anhnhan@outlook.com>
2013-03-22 09:46:21 -0700

    Adding some filters and queries to Macro application
    Summary:
    Fixes T2778
    
    Introduces `PhabricatorMacroQuery`, which should consolidate all queries regarding macros
    
    Adds `PolicyInterface` to `PhabricatorImageMacro`, as else the query would fail (we should consider adding it to the ApplicationTransaction instead, if that was ever planned)
    
    Adds `Active Macros` filter, making it the default
    
    Adds `My Macros` filter. You may ask why it overwrites `$authors`. Well, I did not want the page jump to the conclusion that it is a search result. It //is// one more or less, but the filter would jump to `seach` instead of `my`. If you want `My Macros` removed, tell me. It is useful only to heavy-macro-uploaders-and-users though. Five or six people in `http://secure.phabricator.(org|com)`, and an estimated dozen and a half at bigger installs.
    
    Test Plan: created multiple macros from multiple authors, disabled them at will. browsed around, verified that Macros only appeared in the right filters and that nothing else broke.
    
    Reviewers: epriestley, chad, btrahan
    
    CC: aran, Korvin
    
    Maniphest Tasks: T2778
    
    Differential Revision: https://secure.phabricator.com/D5409

--- src/applications/macro/storage/PhabricatorFileImageMacro.php
+++ src/applications/macro/storage/PhabricatorFileImageMacro.php
@@ -3,12 +3,28 @@
 final class PhabricatorFileImageMacro extends PhabricatorFileDAO
   implements
     PhabricatorSubscribableInterface,
-    PhabricatorApplicationTransactionInterface {
+    PhabricatorApplicationTransactionInterface,
+    PhabricatorPolicyInterface {
 
   protected $filePHID;
   protected $phid;
   protected $name;
   protected $isDisabled = 0;
+
+  private $file;
+
+  public function attachFile(PhabricatorFile $file) {
+    $this->file = $file;
+    return $this;
+  }
+
+  public function getFile() {
+    if (!$this->file) {
+      throw new Exception("Attach a file with attachFile() first!");
+    }
+
+    return $this->file;
+  }
 
   public function getConfiguration() {
     return array(
@@ -31,6 +47,21 @@ final class PhabricatorFileImageMacro extends PhabricatorFileDAO
 
   public function getApplicationTransactionObject() {
     return new PhabricatorMacroTransaction();
+  }
+
+  public function getCapabilities() {
+    return array(
+      PhabricatorPolicyCapability::CAN_VIEW,
+      PhabricatorPolicyCapability::CAN_EDIT,
+    );
+  }
+
+  public function getPolicy($capability) {
+    return PhabricatorPolicies::POLICY_USER;
+  }
+
+  public function hasAutomaticCapability($capability, PhabricatorUser $viewer) {
+    return false;
   }
 
 }
