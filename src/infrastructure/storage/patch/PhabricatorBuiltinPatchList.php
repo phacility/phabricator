3f7ae27b584b078e5911b5e85cd718df174386f7
Afaque Hussain <Afaque.Hussain@outlook.com>
2013-04-24 15:14:25 -0700

    Sql Patch to update the user_externalaccount table.
    Summary: Sql Patch to rename the externalaccount table to user_externalaccount and to add dateCreated, dateModified fields to the updated table.
    
    Test Plan: {F41442}
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: aran, Korvin, AnhNhan
    
    Differential Revision: https://secure.phabricator.com/D5770

--- src/infrastructure/storage/patch/PhabricatorBuiltinPatchList.php
+++ src/infrastructure/storage/patch/PhabricatorBuiltinPatchList.php
@@ -1242,6 +1242,10 @@ final class PhabricatorBuiltinPatchList extends PhabricatorSQLPatchList {
         'type' => 'sql',
         'name' => $this->getPatchPath('20130417.externalaccount.sql'),
       ),
+      '20130423.updateexternalaccount.sql' => array(
+        'type' => 'sql',
+        'name' => $this->getPatchPath('20130423.updateexternalaccount.sql'),
+      ),
     );
   }
 }
