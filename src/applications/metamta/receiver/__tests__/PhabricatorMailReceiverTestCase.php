b10b1fc65fe5d6d168a2e9bf1b4afdd5866fdc13
epriestley <git@epriestley.com>
2013-05-14 15:04:17 -0700

    Add unit tests to cover PhabricatorMailReceiver::matchAddresses()
    Summary: Ref T3183. We should accept addresses like `"New Bug" <bugs@example.com>` to match `bugs@example.com`.
    
    Test Plan: Ran unit tests.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T3183
    
    Differential Revision: https://secure.phabricator.com/D5923

---
+++ src/applications/metamta/receiver/__tests__/PhabricatorMailReceiverTestCase.php
@@ -0,0 +1,42 @@
+<?php
+
+final class PhabricatorMailReceiverTestCase extends PhabricatorTestCase {
+
+  public function testAddressSimilarity() {
+    $env = PhabricatorEnv::beginScopedEnv();
+    $env->overrideEnvConfig('metamta.single-reply-handler-prefix', 'prefix');
+
+    $base = 'alincoln@example.com';
+
+    $same = array(
+      'alincoln@example.com',
+      '"Abrahamn Lincoln" <alincoln@example.com>',
+      'ALincoln@example.com',
+      'prefix+alincoln@example.com',
+    );
+
+    foreach ($same as $address) {
+      $this->assertEqual(
+        true,
+        PhabricatorMailReceiver::matchAddresses($base, $address),
+        "Address {$address}");
+    }
+
+    $diff = array(
+      'a.lincoln@example.com',
+      'aluncoln@example.com',
+      'prefixalincoln@example.com',
+      'badprefix+alincoln@example.com',
+      'bad+prefix+alincoln@example.com',
+      'prefix+alincoln+sufffix@example.com',
+    );
+
+    foreach ($diff as $address) {
+      $this->assertEqual(
+        false,
+        PhabricatorMailReceiver::matchAddresses($base, $address),
+        "Address: {$address}");
+    }
+  }
+
+}
