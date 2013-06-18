9c29174bb4c8de7c4ab95d13ebbf810125636a4b
epriestley <git@epriestley.com>
2013-06-18 10:58:16 -0700

    Provide vertical margins around login buttons
    Summary: Ref T1536. If you only have button-based logins, the new login screen looks weird.
    
    Test Plan:
    Before
    
    {F46725}
    
    After
    
    {F46726}
    
    Reviewers: chad, jamesr
    
    Reviewed By: chad
    
    CC: aran
    
    Maniphest Tasks: T1536
    
    Differential Revision: https://secure.phabricator.com/D6225

--- src/__celerity_resource_map__.php
+++ src/__celerity_resource_map__.php
@@ -923,7 +923,7 @@ celerity_register_resource_map(array(
   ),
   'auth-css' =>
   array(
-    'uri' => '/res/911e97cb/rsrc/css/application/auth/auth.css',
+    'uri' => '/res/751cacaa/rsrc/css/application/auth/auth.css',
     'type' => 'css',
     'requires' =>
     array(
