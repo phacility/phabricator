d27e7c52b21865563bd56f37d5d422515aa40bb7
Levi Jackson <ljackson@unionstreetmedia.com>
2013-07-14 16:02:12 -0700

    Add explicit `mysql.port` configuration
    See: https://github.com/facebook/phabricator/pull/356
    
    Reviewed by: epriestley

--- src/applications/config/option/PhabricatorMySQLConfigOptions.php
+++ src/applications/config/option/PhabricatorMySQLConfigOptions.php
@@ -69,6 +69,10 @@ final class PhabricatorMySQLConfigOptions
             "this namespace if you want. Normally, you should not do this ".
             "unless you are developing Phabricator and using namespaces to ".
             "separate multiple sandbox datasets.")),
+        $this->newOption('mysql.port', 'string', null)
+        ->setLocked(true)
+        ->setDescription(
+          pht("MySQL port to use when connecting to the database.")),
     );
   }
 
