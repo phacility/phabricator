215da7728f724da91de3b83e6d37e913fb8cf1f8
epriestley <git@epriestley.com>
2013-07-21 08:44:53 -0700

    Use ApplicationSearch in Releeph project list
    Summary: Ref T3092. Convert Releeph project list to ApplicationSearch.
    
    Test Plan: {F50333}
    
    Reviewers: btrahan, edward
    
    Reviewed By: edward
    
    CC: aran
    
    Maniphest Tasks: T3092
    
    Differential Revision: https://secure.phabricator.com/D6474

--- src/applications/releeph/controller/ReleephController.php
+++ src/applications/releeph/controller/ReleephController.php
@@ -15,4 +15,27 @@ abstract class ReleephController extends PhabricatorController {
     return $response->setContent($page->render());
   }
 
+  public function buildSideNavView($for_app = false) {
+    $user = $this->getRequest()->getUser();
+
+    $nav = new AphrontSideNavFilterView();
+    $nav->setBaseURI(new PhutilURI($this->getApplicationURI()));
+
+    if ($for_app) {
+      $nav->addFilter('project/create/', pht('Create Project'));
+    }
+
+    id(new ReleephProjectSearchEngine())
+      ->setViewer($user)
+      ->addNavigationItems($nav->getMenu());
+
+    $nav->selectFilter(null);
+
+    return $nav;
+  }
+
+  public function buildApplicationMenu() {
+    return $this->buildSideNavView(true)->getMenu();
+  }
+
 }
