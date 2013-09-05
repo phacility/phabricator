4b061a766a9c6018a1638046a90530046a1ec64f
Chad Little <chad@chadsdomain.com>
2013-09-05 12:29:07 -0700

    More Diviner style updates
    Summary: This adds a number of new styles for Diviner documentation. Not sure I've covered all the bases or wrote this in the most efficient manner, but passing it along now for early review before tightening everything up.
    
    Test Plan: Review various class pages.
    
    Reviewers: epriestley, btrahan
    
    Reviewed By: epriestley
    
    CC: Korvin, aran
    
    Differential Revision: https://secure.phabricator.com/D6888

---
+++ src/applications/diviner/view/DivinerSectionView.php
@@ -0,0 +1,43 @@
+<?php
+
+final class DivinerSectionView extends AphrontTagView {
+
+  private $header;
+  private $content;
+
+  public function addContent($content) {
+    $this->content[] = $content;
+    return $this;
+  }
+
+  public function setHeader($text) {
+    $this->header = $text;
+    return $this;
+  }
+
+  public function getTagName() {
+    return 'div';
+  }
+
+  public function getTagAttributes() {
+    return array(
+      'class' => 'diviner-document-section',
+    );
+  }
+
+  public function getTagContent() {
+    require_celerity_resource('diviner-shared-css');
+
+    $header = id(new PhabricatorHeaderView())
+      ->setBleedHeader(true)
+      ->setHeader($this->header);
+
+    $content = id(new PHUIBoxView())
+      ->addPadding(PHUI::PADDING_LARGE_LEFT)
+      ->addPadding(PHUI::PADDING_LARGE_RIGHT)
+      ->appendChild($this->content);
+
+    return array($header, $content);
+  }
+
+}
