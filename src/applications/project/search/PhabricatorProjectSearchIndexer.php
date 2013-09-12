e96201773d4241ecfa5ff5f7950e0e0a34d0fa7a
epriestley <git@epriestley.com>
2013-09-12 13:05:19 -0700

    Index projects in the main search index
    Summary:
    Part one of a large and complicated plot:
    
      - The last filter for Maniphest "pro" queries is "Group By".
      - This is currently executed in a convoluted and ridiculous way, loading massive amounts of data.
      - The primary reason it works like it does is that we don't have a project name index available in Maniphest, so we can't sort in the DB.
      - So, I want to provide a name index to Maniphest and push this work to the DB.
    
    To do that, my plan is:
    
      - Index projects in Search.
      - Add a "did update index" event.
      - Have Maniphest listen for it.
      - When projects are updated, update their indexes in Maniphest.
      - Rewrite the giant mess of "group by: project" to be somewhat reasonable.
      - This may also extend to some future "group by: assignee".
    
    This is the first small step down this path, which just indexes projects in search.
    
    Test Plan: Ran `bin/search index --type project`, then searched for projects.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D6955

---
+++ src/applications/project/search/PhabricatorProjectSearchIndexer.php
@@ -0,0 +1,25 @@
+<?php
+
+final class PhabricatorProjectSearchIndexer
+  extends PhabricatorSearchDocumentIndexer {
+
+  public function getIndexableObject() {
+    return new PhabricatorProject();
+  }
+
+  protected function buildAbstractDocumentByPHID($phid) {
+    $project = $this->loadDocumentByPHID($phid);
+
+    $doc = new PhabricatorSearchAbstractDocument();
+    $doc->setPHID($project->getPHID());
+    $doc->setDocumentType(PhabricatorProjectPHIDTypeProject::TYPECONST);
+    $doc->setDocumentTitle($project->getName());
+    $doc->setDocumentCreated($project->getDateCreated());
+    $doc->setDocumentModified($project->getDateModified());
+
+    // NOTE: This could be more full-featured, but for now we're mostly
+    // interested in the side effects of indexing.
+
+    return $doc;
+  }
+}
