819bd7f03b2c5cfd1cfc217e49abb1afd691aeb4
Bob Trahan <bob.trahan@gmail.com>
2013-11-11 15:01:21 -0800

    Add arcanist project phid to differential.query
    Summary: Fixes T3535. Also, flip flop on that spacing thing and make the spaces purdy
    
    Test Plan: got an arcanist projected phid in the json dict
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: Korvin, epriestley, aran
    
    Maniphest Tasks: T3535
    
    Differential Revision: https://secure.phabricator.com/D7565

--- src/applications/differential/conduit/ConduitAPI_differential_query_Method.php
+++ src/applications/differential/conduit/ConduitAPI_differential_query_Method.php
@@ -202,27 +202,28 @@ final class ConduitAPI_differential_query_Method
       $auxiliary_fields = $this->loadAuxiliaryFields(
                                  $revision, $request->getUser());
       $result = array(
-        'id'            => $id,
-        'phid'          => $revision->getPHID(),
-        'title'         => $revision->getTitle(),
-        'uri'           => PhabricatorEnv::getProductionURI('/D'.$id),
-        'dateCreated'   => $revision->getDateCreated(),
-        'dateModified'  => $revision->getDateModified(),
-        'authorPHID'    => $revision->getAuthorPHID(),
-        'status'        => $revision->getStatus(),
-        'statusName'    =>
+        'id'                  => $id,
+        'phid'                => $revision->getPHID(),
+        'title'               => $revision->getTitle(),
+        'uri'                 => PhabricatorEnv::getProductionURI('/D'.$id),
+        'dateCreated'         => $revision->getDateCreated(),
+        'dateModified'        => $revision->getDateModified(),
+        'authorPHID'          => $revision->getAuthorPHID(),
+        'status'              => $revision->getStatus(),
+        'statusName'          =>
           ArcanistDifferentialRevisionStatus::getNameForRevisionStatus(
             $revision->getStatus()),
-        'branch'        => $diff->getBranch(),
-        'summary'       => $revision->getSummary(),
-        'testPlan'      => $revision->getTestPlan(),
-        'lineCount'     => $revision->getLineCount(),
-        'diffs'         => $revision->getDiffIDs(),
-        'commits'       => $revision->getCommitPHIDs(),
-        'reviewers'     => array_values($revision->getReviewers()),
-        'ccs'           => array_values($revision->getCCPHIDs()),
-        'hashes'        => $revision->getHashes(),
-        'auxiliary'     => $auxiliary_fields,
+        'branch'              => $diff->getBranch(),
+        'summary'             => $revision->getSummary(),
+        'testPlan'            => $revision->getTestPlan(),
+        'lineCount'           => $revision->getLineCount(),
+        'diffs'               => $revision->getDiffIDs(),
+        'commits'             => $revision->getCommitPHIDs(),
+        'reviewers'           => array_values($revision->getReviewers()),
+        'ccs'                 => array_values($revision->getCCPHIDs()),
+        'hashes'              => $revision->getHashes(),
+        'auxiliary'           => $auxiliary_fields,
+        'arcanistProjectPHID' => $diff->getArcanistProjectPHID()
       );
 
       // TODO: This is a hacky way to put permissions on this field until we
