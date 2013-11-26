209861500f52319834f103e11bee2ad762acac50
epriestley <git@epriestley.com>
2013-11-26 08:50:36 -0800

    Use "en_US.UTF-8", not "C", as the LANG setting
    Summary: `LANG=C` is smooshing UTF-8 in some cases. See IRC.
    
    Test Plan: User confirmed this works.
    
    Reviewers: btrahan, asherkin
    
    Reviewed By: asherkin
    
    CC: aran
    
    Differential Revision: https://secure.phabricator.com/D7659

--- src/applications/repository/storage/PhabricatorRepository.php
+++ src/applications/repository/storage/PhabricatorRepository.php
@@ -294,10 +294,10 @@ final class PhabricatorRepository extends PhabricatorRepositoryDAO
 
   private function getCommonCommandEnvironment() {
     $env = array(
-      // NOTE: Force the language to "C", which overrides locale settings.
-      // This makes stuff print in English instead of, e.g., French, so we can
-      // parse the output of some commands, error messages, etc.
-      'LANG' => 'C',
+      // NOTE: Force the language to "en_US.UTF-8", which overrides locale
+      // settings. This makes stuff print in English instead of, e.g., French,
+      // so we can parse the output of some commands, error messages, etc.
+      'LANG' => 'en_US.UTF-8',
 
       // Propagate PHABRICATOR_ENV explicitly. For discussion, see T4155.
       'PHABRICATOR_ENV' => PhabricatorEnv::getSelectedEnvironmentName(),
