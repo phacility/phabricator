6b1ec35cf3775804b20cc19d4979eb1696b40b12
epriestley <git@epriestley.com>
2013-12-11 14:46:39 -0800

    Allow Herald rules to check for revisions with no reviewers
    Summary: Fixes T4225. Adds the NON_EXISTS condition to Herald for "Reviewers", and adds a few more conditions which have reasonable meanings.
    
    Test Plan: Used test console to check a revision with reviewers, and another without reviewers. Both produced the expected results.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    CC: aran
    
    Maniphest Tasks: T4225
    
    Differential Revision: https://secure.phabricator.com/D7757

--- src/applications/herald/storage/HeraldRule.php
+++ src/applications/herald/storage/HeraldRule.php
@@ -16,7 +16,7 @@ final class HeraldRule extends HeraldDAO
   protected $ruleType;
   protected $isDisabled = 0;
 
-  protected $configVersion = 14;
+  protected $configVersion = 15;
 
   private $ruleApplied = self::ATTACHABLE; // phids for which this rule has been applied
   private $validAuthor = self::ATTACHABLE;
