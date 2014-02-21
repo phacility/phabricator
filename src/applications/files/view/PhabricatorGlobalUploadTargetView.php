2a277bbc038cbe0f6a067f42023f193e96650801
Bob Trahan <bob.trahan@gmail.com>
2014-02-21 13:04:23 -0800

    Hide upload JS stuff if user isn't logged in
    Summary: ...cuz it won't work. I think adding a "login to upload" has no real value as this is a pretty unexpected / power user feature anyway. Fixes T4354.
    
    Test Plan: tried to upload as a logged out user to Phabricator home. my browser just loaded the file as expected.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    CC: Korvin, epriestley, aran
    
    Maniphest Tasks: T4354
    
    Differential Revision: https://secure.phabricator.com/D8298

--- src/applications/files/view/PhabricatorGlobalUploadTargetView.php
+++ src/applications/files/view/PhabricatorGlobalUploadTargetView.php
@@ -14,6 +14,11 @@ final class PhabricatorGlobalUploadTargetView extends AphrontView {
   }
 
   public function render() {
+    $viewer = $this->getUser();
+    if (!$viewer->isLoggedIn()) {
+      return null;
+    }
+
     $instructions_id = celerity_generate_unique_node_id();
 
     require_celerity_resource('global-drag-and-drop-css');
