8b6c86e27d141df489788a40f39c69dcc50eedd4
Michael Peters <mpeters@insidesales.com>
2014-03-11 13:07:45 -0700

    Fix the script that saves lint for a repo into the database and updates diffusion.
    Summary:
    It appears a change to the way the configuration was loaded into ArcanistRepositoryAPI in rARCa2285b2b broke the save_lint script.
    This updates the DiffusionLintSaveRunner to use the configuration correctly, allowing the linter to run
    
    Test Plan: cd /your/project; ../../../path/to/phabricator/scripts/repository/save_lint.php
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: aran, epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D8487

--- src/applications/diffusion/DiffusionLintSaveRunner.php
+++ src/applications/diffusion/DiffusionLintSaveRunner.php
@@ -44,7 +44,10 @@ final class DiffusionLintSaveRunner {
 
   public function run($dir) {
     $working_copy = ArcanistWorkingCopyIdentity::newFromPath($dir);
-    $api = ArcanistRepositoryAPI::newAPIFromWorkingCopyIdentity($working_copy);
+    $configuration_manager = new ArcanistConfigurationManager();
+    $configuration_manager->setWorkingCopyIdentity($working_copy);
+    $api = ArcanistRepositoryAPI::newAPIFromConfigurationManager(
+      $configuration_manager);
 
     $this->svnRoot = id(new PhutilURI($api->getSourceControlPath()))->getPath();
     if ($api instanceof ArcanistGitAPI) {
