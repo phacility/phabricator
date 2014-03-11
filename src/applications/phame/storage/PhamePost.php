a1faac0a21b88bacc242fe2083acc4b799b6737f
Bob Trahan <btrahan@phacility.com>
2014-03-11 15:51:53 -0700

    Phame - create conduit API to create posts
    Summary:
    nothing too crazy here. try to be smart about some defaults (i.e. phame title is optional and can be derived from title; post as not a draft by default; etc). Fixes T3695.
    
    also do a little re-factoring to centralizing initializing new posts and turning posts into dictionaries. also change blogs => posts in another conduit method so it makes sense and stuff.
    
    Test Plan: made some posts via conduit. testing trying to specify blogger, phame title, and isDraft, all worked nicely
    
    Reviewers: chad, epriestley
    
    Reviewed By: epriestley
    
    Subscribers: aran, epriestley, Korvin
    
    Maniphest Tasks: T3695
    
    Differential Revision: https://secure.phabricator.com/D8485

--- src/applications/phame/storage/PhamePost.php
+++ src/applications/phame/storage/PhamePost.php
@@ -26,6 +26,19 @@ final class PhamePost extends PhameDAO
 
   private $blog;
 
+  public static function initializePost(
+    PhabricatorUser $blogger,
+    PhameBlog $blog) {
+
+    $post = id(new PhamePost())
+      ->setBloggerPHID($blogger->getPHID())
+      ->setBlogPHID($blog->getPHID())
+      ->setBlog($blog)
+      ->setDatePublished(0)
+      ->setVisibility(self::VISIBILITY_DRAFT);
+    return $post;
+  }
+
   public function setBlog(PhameBlog $blog) {
     $this->blog = $blog;
     return $this;
@@ -80,6 +93,22 @@ final class PhamePost extends PhameDAO
   public function generatePHID() {
     return PhabricatorPHID::generateNewPHID(
       PhabricatorPhamePHIDTypePost::TYPECONST);
+  }
+
+  public function toDictionary() {
+    return array(
+      'id'            => $this->getID(),
+      'phid'          => $this->getPHID(),
+      'blogPHID'      => $this->getBlogPHID(),
+      'bloggerPHID'   => $this->getBloggerPHID(),
+      'viewURI'       => $this->getViewURI(),
+      'title'         => $this->getTitle(),
+      'phameTitle'    => $this->getPhameTitle(),
+      'body'          => $this->getBody(),
+      'summary'       => PhabricatorMarkupEngine::summarize($this->getBody()),
+      'datePublished' => $this->getDatePublished(),
+      'published'     => !$this->isDraft(),
+    );
   }
 
   public static function getVisibilityOptionsForSelect() {
