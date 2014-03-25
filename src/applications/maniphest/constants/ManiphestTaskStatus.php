7713fb5d99a3e27b2ec54430e84a6fe95d74d807
epriestley <git@epriestley.com>
2014-03-25 14:04:51 -0700

    Add config validation for task status config
    Summary: Ref T1812. This still doesn't expose configuration to the user, but adds validation for it.
    
    Test Plan: Added a pile of unit tests.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: epriestley
    
    Maniphest Tasks: T1812
    
    Differential Revision: https://secure.phabricator.com/D8584

--- src/applications/maniphest/constants/ManiphestTaskStatus.php
+++ src/applications/maniphest/constants/ManiphestTaskStatus.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * @task validate Configuration Validation
+ */
 final class ManiphestTaskStatus extends ManiphestConstants {
 
   const STATUS_OPEN               = 'open';
@@ -151,7 +154,7 @@ final class ManiphestTaskStatus extends ManiphestConstants {
   }
 
   private static function getSpecialStatus($special) {
-    foreach (self::getEnabledStatusMap() as $const => $status) {
+    foreach (self::getStatusConfig() as $const => $status) {
       if (idx($status, 'special') == $special) {
         return $const;
       }
@@ -249,6 +252,138 @@ final class ManiphestTaskStatus extends ManiphestConstants {
     }
 
     return $default;
+  }
+
+
+/* -(  Configuration Validation  )------------------------------------------- */
+
+
+  /**
+   * @task validate
+   */
+  public static function isValidStatusConstant($constant) {
+    if (strlen($constant) > 12) {
+      return false;
+    }
+    if (!preg_match('/^[a-z0-9]+\z/', $constant)) {
+      return false;
+    }
+    return true;
+  }
+
+
+  /**
+   * @task validate
+   */
+  public static function validateConfiguration(array $config) {
+    foreach ($config as $key => $value) {
+      if (!self::isValidStatusConstant($key)) {
+        throw new Exception(
+          pht(
+            'Key "%s" is not a valid status constant. Status constants must '.
+            'be 1-12 characters long and contain only lowercase letters (a-z) '.
+            'and digits (0-9). For example, "%s" or "%s" are reasonable '.
+            'choices.',
+            $key,
+            'open',
+            'closed'));
+      }
+      if (!is_array($value)) {
+        throw new Exception(
+          pht(
+            'Value for key "%s" should be a dictionary.',
+            $key));
+      }
+
+      PhutilTypeSpec::checkMap(
+        $value,
+        array(
+          'name' => 'string',
+          'name.full' => 'optional string',
+          'name.action' => 'optional string',
+          'closed' => 'optional bool',
+          'special' => 'optional string',
+          'transaction.icon' => 'optional string',
+          'transaction.color' => 'optional string',
+          'silly' => 'optional bool',
+          'prefixes' => 'optional list<string>',
+          'suffixes' => 'optional list<string>',
+        ));
+    }
+
+    $special_map = array();
+    foreach ($config as $key => $value) {
+      $special = idx($value, 'special');
+      if (!$special) {
+        continue;
+      }
+
+      if (isset($special_map[$special])) {
+        throw new Exception(
+          pht(
+            'Configuration has two statuses both marked with the special '.
+            'attribute "%s" ("%s" and "%s"). There should be only one.',
+            $special,
+            $special_map[$special],
+            $key));
+      }
+
+      switch ($special) {
+        case self::SPECIAL_DEFAULT:
+          if (!empty($value['closed'])) {
+            throw new Exception(
+              pht(
+                'Status "%s" is marked as default, but it is a closed '.
+                'status. The default status should be an open status.',
+                $key));
+          }
+          break;
+        case self::SPECIAL_CLOSED:
+          if (empty($value['closed'])) {
+            throw new Exception(
+              pht(
+                'Status "%s" is marked as the default status for closing '.
+                'tasks, but is not a closed status. It should be a closed '.
+                'status.',
+                $key));
+          }
+          break;
+        case self::SPECIAL_DUPLICATE:
+          if (empty($value['closed'])) {
+            throw new Exception(
+              pht(
+                'Status "%s" is marked as the status for closing tasks as '.
+                'duplicates, but it is not a closed status. It should '.
+                'be a closed status.',
+                $key));
+          }
+          break;
+      }
+
+      $special_map[$special] = $key;
+    }
+
+    // NOTE: We're not explicitly validating that we have at least one open
+    // and one closed status, because the DEFAULT and CLOSED specials imply
+    // that to be true. If those change in the future, that might become a
+    // reasonable thing to validate.
+
+    $required = array(
+      self::SPECIAL_DEFAULT,
+      self::SPECIAL_CLOSED,
+      self::SPECIAL_DUPLICATE,
+    );
+
+    foreach ($required as $required_special) {
+      if (!isset($special_map[$required_special])) {
+        throw new Exception(
+          pht(
+            'Configuration defines no task status with special attribute '.
+            '"%s", but you must specify a status which fills this special '.
+            'role.',
+            $required_special));
+      }
+    }
   }
 
 }
