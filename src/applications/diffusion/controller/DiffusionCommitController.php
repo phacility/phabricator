6b5308c981ea7a0c627727fae62f450369a12a25
Bob Trahan <btrahan@phacility.com>
2014-04-04 11:29:10 -0700

    Audit - add ability to add user or projects as auditors
    Summary: Ref T4687. Trickier part is adding packages; will require some typeahead core changes
    
    Test Plan: add a project as an auditor succuessfully!
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T4687
    
    Differential Revision: https://secure.phabricator.com/D8704

--- src/applications/diffusion/controller/DiffusionCommitController.php
+++ src/applications/diffusion/controller/DiffusionCommitController.php
@@ -778,9 +778,9 @@ final class DiffusionCommitController extends DiffusionController {
         'dynamic' => array(
           'add-auditors-tokenizer' => array(
             'actions' => array('add_auditors' => 1),
-            'src' => '/typeahead/common/users/',
+            'src' => '/typeahead/common/usersorprojects/',
             'row' => 'add-auditors',
-            'placeholder' => pht('Type a user name...'),
+            'placeholder' => pht('Type a user or project name...'),
           ),
           'add-ccs-tokenizer' => array(
             'actions' => array('add_ccs' => 1),
