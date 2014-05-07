0620386d0e4ac42b523f05bc7683041464d2dfed
epriestley <git@epriestley.com>
2014-05-07 07:19:50 -0700

    Fix typo (demo)
    Summary: There was a typo.
    
    Test Plan: Now there is no typo.
    
    Reviewers: btrahan, chad
    
    Reviewed By: chad
    
    Subscribers: aran, epriestley
    
    Differential Revision: https://secure.phabricator.com/D8995

--- src/applications/config/check/PhabricatorSetupCheckPath.php
+++ src/applications/config/check/PhabricatorSetupCheckPath.php
@@ -100,7 +100,7 @@ final class PhabricatorSetupCheckPath extends PhabricatorSetupCheck {
 
         $this
           ->newIssue('config.PATH.'.$digest)
-          ->setName(pht('$PATH Compontent Unusable'))
+          ->setName(pht('$PATH Component Unusable'))
           ->setSummary(
             pht(
               "A component of the configured PATH can not be used by ".
