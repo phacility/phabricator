323c529c3bac2ec6f6c8eebb273a53254d9ad5b3
Joshua Spence <josh@joshuaspence.com>
2014-05-30 09:48:03 -0700

    Allow the notifications from Aphlict debug messages to expire.
    Summary: Currently, debugging (log) messages from the Aphlict client trigger a visible notification in the web UI (when `notification.debug` is enabled). After D9327, the log messages can be quite verbose and seem to "block" legitimate notifications because they are not automatically dismissed.
    
    Test Plan: Enabled `notification.debug` and observed that the debugging notifications expired and closed after a few seconds.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D9339

--- resources/celerity/map.php
+++ resources/celerity/map.php
@@ -8,7 +8,7 @@ return array(
   'names' =>
   array(
     'core.pkg.css' => '8ecc2a14',
-    'core.pkg.js' => '7db41c19',
+    'core.pkg.js' => '639b2433',
     'darkconsole.pkg.js' => 'ca8671ce',
     'differential.pkg.css' => 'fbf57382',
     'differential.pkg.js' => 'f4c86691',
@@ -344,7 +344,7 @@ return array(
     'rsrc/image/texture/table_header_tall.png' => 'd56b434f',
     'rsrc/js/application/aphlict/Aphlict.js' => '493665ee',
     'rsrc/js/application/aphlict/behavior-aphlict-dropdown.js' => '2a2dba85',
-    'rsrc/js/application/aphlict/behavior-aphlict-listen.js' => '0a6c2de6',
+    'rsrc/js/application/aphlict/behavior-aphlict-listen.js' => '9aa97ab6',
     'rsrc/js/application/auth/behavior-persona-login.js' => '9414ff18',
     'rsrc/js/application/config/behavior-reorder-fields.js' => '938aed89',
     'rsrc/js/application/conpherence/behavior-menu.js' => '7ee23816',
@@ -482,7 +482,7 @@ return array(
     'rsrc/js/phuix/PHUIXActionListView.js' => 'b5c256b8',
     'rsrc/js/phuix/PHUIXActionView.js' => '6e8cefa4',
     'rsrc/js/phuix/PHUIXDropdownMenu.js' => 'bd4c8dca',
-    'rsrc/swf/aphlict.swf' => '731d14a6',
+    'rsrc/swf/aphlict.swf' => 'f22c1e40',
   ),
   'symbols' =>
   array(
@@ -532,7 +532,7 @@ return array(
     'javelin-aphlict' => '493665ee',
     'javelin-behavior' => '8a3ed18b',
     'javelin-behavior-aphlict-dropdown' => '2a2dba85',
-    'javelin-behavior-aphlict-listen' => '0a6c2de6',
+    'javelin-behavior-aphlict-listen' => '9aa97ab6',
     'javelin-behavior-aphront-basic-tokenizer' => 'b3a4b884',
     'javelin-behavior-aphront-crop' => 'b98fc918',
     'javelin-behavior-aphront-drag-and-drop-textarea' => '4a11ea9c',
@@ -890,18 +890,6 @@ return array(
       2 => 'javelin-workflow',
       3 => 'javelin-dom',
       4 => 'javelin-router',
-    ),
-    '0a6c2de6' =>
-    array(
-      0 => 'javelin-behavior',
-      1 => 'javelin-aphlict',
-      2 => 'javelin-stratcom',
-      3 => 'javelin-request',
-      4 => 'javelin-uri',
-      5 => 'javelin-dom',
-      6 => 'javelin-json',
-      7 => 'javelin-router',
-      8 => 'phabricator-notification',
     ),
     '0c33c1a0' =>
     array(
@@ -1538,6 +1526,18 @@ return array(
       2 => 'javelin-workflow',
       3 => 'javelin-dom',
       4 => 'phabricator-draggable-list',
+    ),
+    '9aa97ab6' =>
+    array(
+      0 => 'javelin-behavior',
+      1 => 'javelin-aphlict',
+      2 => 'javelin-stratcom',
+      3 => 'javelin-request',
+      4 => 'javelin-uri',
+      5 => 'javelin-dom',
+      6 => 'javelin-json',
+      7 => 'javelin-router',
+      8 => 'phabricator-notification',
     ),
     '9b9197be' =>
     array(
