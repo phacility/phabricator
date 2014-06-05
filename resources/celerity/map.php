phacility/phabricator

commit 8033a697461d6b621d2d12b54f33e925dd5b21b0

Catch errors that may occur whilst receiving data from the Aphlict server.
Summary: Ref T4324. Currently, if the `AphlictMaster` receives dodgy data from the Aphlict server (invalid JSON, for example) then a syntax error will be thrown and the `AphlictMaster` will die. Instead, catch errors and raise a notification.

Test Plan: {F163466}

Reviewers: #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: epriestley, Korvin

Maniphest Tasks: T4324

Differential Revision: https://secure.phabricator.com/D9380

webroot/rsrc/js/application/aphlict/behavior-aphlict-listen.js
@@ -24,25 +24,39 @@ JX.behavior('aphlict-listen', function(config) {
   // Respond to a notification from the Aphlict notification server. We send
   // a request to Phabricator to get notification details.
   function onaphlictmessage(type, message) {
-    if (type == 'receive') {
-      var routable = new JX.Request('/notification/individual/', onnotification)
-        .addData({key: message.key})
-        .getRoutable();
-
-      routable
-        .setType('notification')
-        .setPriority(250);
-
-      JX.Router.getInstance().queue(routable);
-    } else if (__DEV__) {
-      if (config.debug) {
-        var details = message ? JX.JSON.stringify(message) : '';
-
+    switch (type) {
+      case 'error':
         new JX.Notification()
-          .setContent('(Aphlict) [' + type + '] ' + details)
-          .alterClassName('jx-notification-debug', true)
+          .setContent('(Aphlict) ' + message)
+          .alterClassName('jx-notification-error', true)
           .show();
-      }
+        break;
+
+      case 'receive':
+        var routable = new JX.Request(
+          '/notification/individual/',
+          onnotification);
+
+        routable
+          .addData({key: message.key})
+          .getRoutable();
+
+        routable
+          .setType('notification')
+          .setPriority(250);
+
+        JX.Router.getInstance().queue(routable);
+        break;
+
+      default:
+        if (__DEV__ && config.debug) {
+          var details = message ? JX.JSON.stringify(message) : '';
+
+          new JX.Notification()
+            .setContent('(Aphlict) [' + type + '] ' + details)
+            .alterClassName('jx-notification-debug', true)
+            .show();
+        }
     }
   }
 
@@ -63,8 +77,7 @@ JX.behavior('aphlict-listen', function(config) {
 
     // If the notification affected an object on this page, show a
     // permanent reload notification if we aren't already.
-    if ((response.primaryObjectPHID in config.pageObjects) &&
-        !showing_reload) {
+    if ((response.primaryObjectPHID in config.pageObjects) && !showing_reload) {
       var reload = new JX.Notification()
         .setContent('Page updated, click to reload.')
         .alterClassName('jx-notification-alert', true)

support/aphlict/client/src/Aphlict.as
@@ -31,6 +31,10 @@ package {
       ExternalInterface.call('JX.Aphlict.didReceiveEvent', type, object);
     }
 
+    protected function error(error:Error):void {
+      this.externalInvoke('error', error.toString());
+    }
+
     protected function log(message:String):void {
       this.externalInvoke('log', message);
     }

support/aphlict/client/src/AphlictMaster.as
@@ -131,34 +131,38 @@ package {
     }
 
     private function didReceiveSocket(event:Event):void {
-      var b:ByteArray = this.readBuffer;
-      this.socket.readBytes(b, b.length);
+      try {
+        var b:ByteArray = this.readBuffer;
+        this.socket.readBytes(b, b.length);
 
-      do {
-        b = this.readBuffer;
-        b.position = 0;
+        do {
+          b = this.readBuffer;
+          b.position = 0;
 
-        if (b.length <= 8) {
-          break;
-        }
-
-        var msg_len:Number = parseInt(b.readUTFBytes(8), 10);
-        if (b.length >= msg_len + 8) {
-          var bytes:String = b.readUTFBytes(msg_len);
-          var data:Object = vegas.strings.JSON.deserialize(bytes);
-          var t:ByteArray = new ByteArray();
-          t.writeBytes(b, msg_len + 8);
-          this.readBuffer = t;
-
-          // Send the message to all clients.
-          for (var client:String in this.clients) {
-            this.log('Sending message to client: ' + client);
-            this.send.send(client, 'receiveMessage', data);
+          if (b.length <= 8) {
+            break;
           }
-        } else {
-          break;
-        }
-      } while (true);
+
+          var msg_len:Number = parseInt(b.readUTFBytes(8), 10);
+          if (b.length >= msg_len + 8) {
+            var bytes:String = b.readUTFBytes(msg_len);
+            var data:Object = vegas.strings.JSON.deserialize(bytes);
+            var t:ByteArray = new ByteArray();
+            t.writeBytes(b, msg_len + 8);
+            this.readBuffer = t;
+
+            // Send the message to all clients.
+            for (var client:String in this.clients) {
+              this.log('Sending message to client: ' + client);
+              this.send.send(client, 'receiveMessage', data);
+            }
+          } else {
+            break;
+          }
+        } while (true);
+      } catch (err:Error) {
+        this.error(err);
+      }
     }
 
   }

commit 93f68a07e4196c925d745f96d00050c535bdbdf8

Fix Aphlict routable
Summary: This was broken in D9380 and caused notifications to stop working.

Test Plan: Saw notifications.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: epriestley, Korvin

Differential Revision: https://secure.phabricator.com/D9395

resources/celerity/map.php
@@ -8,7 +8,7 @@ return array(
   'names' =>
   array(
     'core.pkg.css' => 'd9c53a92',
-    'core.pkg.js' => 'b6689fe1',
+    'core.pkg.js' => 'd7e1141d',
     'darkconsole.pkg.js' => 'ca8671ce',
     'differential.pkg.css' => '4a93db37',
     'differential.pkg.js' => 'eca39a2c',
@@ -337,7 +337,7 @@ return array(
     'rsrc/image/texture/table_header_tall.png' => 'd56b434f',
     'rsrc/js/application/aphlict/Aphlict.js' => '493665ee',
     'rsrc/js/application/aphlict/behavior-aphlict-dropdown.js' => '2a2dba85',
-    'rsrc/js/application/aphlict/behavior-aphlict-listen.js' => 'bc75da74',
+    'rsrc/js/application/aphlict/behavior-aphlict-listen.js' => '258e4342',
     'rsrc/js/application/auth/behavior-persona-login.js' => '9414ff18',
     'rsrc/js/application/config/behavior-reorder-fields.js' => '938aed89',
     'rsrc/js/application/conpherence/behavior-menu.js' => '7ee23816',
@@ -528,7 +528,7 @@ return array(
     'javelin-aphlict' => '493665ee',
     'javelin-behavior' => '8a3ed18b',
     'javelin-behavior-aphlict-dropdown' => '2a2dba85',
-    'javelin-behavior-aphlict-listen' => 'bc75da74',
+    'javelin-behavior-aphlict-listen' => '258e4342',
     'javelin-behavior-aphront-basic-tokenizer' => 'b3a4b884',
     'javelin-behavior-aphront-crop' => 'b98fc918',
     'javelin-behavior-aphront-drag-and-drop-textarea' => '4a11ea9c',
@@ -988,6 +988,18 @@ return array(
       1 => 'javelin-dom',
       2 => 'javelin-stratcom',
       3 => 'phuix-dropdown-menu',
+    ),
+    '258e4342' =>
+    array(
+      0 => 'javelin-behavior',
+      1 => 'javelin-aphlict',
+      2 => 'javelin-stratcom',
+      3 => 'javelin-request',
+      4 => 'javelin-uri',
+      5 => 'javelin-dom',
+      6 => 'javelin-json',
+      7 => 'javelin-router',
+      8 => 'phabricator-notification',
     ),
     '263aeb8c' =>
     array(
@@ -1697,18 +1709,6 @@ return array(
       4 => 'phabricator-textareautils',
       5 => 'javelin-workflow',
       6 => 'javelin-vector',
-    ),
-    'bc75da74' =>
-    array(
-      0 => 'javelin-behavior',
-      1 => 'javelin-aphlict',
-      2 => 'javelin-stratcom',
-      3 => 'javelin-request',
-      4 => 'javelin-uri',
-      5 => 'javelin-dom',
-      6 => 'javelin-json',
-      7 => 'javelin-router',
-      8 => 'phabricator-notification',
     ),
     'bd0aedcd' =>
     array(

webroot/rsrc/js/application/aphlict/behavior-aphlict-listen.js
@@ -33,11 +33,11 @@ JX.behavior('aphlict-listen', function(config) {
         break;
 
       case 'receive':
-        var routable = new JX.Request(
+        var request = new JX.Request(
           '/notification/individual/',
           onnotification);
 
-        routable
+        var routable = request
           .addData({key: message.key})
           .getRoutable();
 