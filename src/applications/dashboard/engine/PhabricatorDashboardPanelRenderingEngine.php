71d9a6be07436b5ab0f4886acf942fe8a911fd5e
Chad Little <chad@chadsdomain.com>
2014-06-12 07:11:00 -0700

    Clean up error state on Dashboards
    Summary: A more basic error view
    
    Test Plan:
    Made stuff up
    
    {F165315}
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T5319
    
    Differential Revision: https://secure.phabricator.com/D9495

--- src/applications/dashboard/engine/PhabricatorDashboardPanelRenderingEngine.php
+++ src/applications/dashboard/engine/PhabricatorDashboardPanelRenderingEngine.php
@@ -156,19 +156,24 @@ final class PhabricatorDashboardPanelRenderingEngine extends Phobject {
       case self::HEADER_MODE_EDIT:
         $header = id(new PhabricatorActionHeaderView())
           ->setHeaderTitle($title)
-          ->setHeaderColor(PhabricatorActionHeaderView::HEADER_RED);
+          ->setHeaderColor(PhabricatorActionHeaderView::HEADER_WHITE);
         $header = $this->addPanelHeaderActions($header);
         break;
       case self::HEADER_MODE_NORMAL:
       default:
         $header = id(new PhabricatorActionHeaderView())
           ->setHeaderTitle($title)
-          ->setHeaderColor(PhabricatorActionHeaderView::HEADER_RED);
+          ->setHeaderColor(PhabricatorActionHeaderView::HEADER_WHITE);
         break;
     }
+    $icon = id(new PHUIIconView())
+      ->setIconFont('fa-warning red msr');
+    $content = id(new PHUIBoxView())
+      ->addClass('dashboard-box')
+      ->appendChild($icon)
+      ->appendChild($body);
     return $this->renderPanelDiv(
-      id(new AphrontErrorView())
-      ->appendChild($body),
+      $content,
       $header);
   }
 
