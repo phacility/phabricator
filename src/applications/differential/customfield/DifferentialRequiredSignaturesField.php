20446252ff422bddd00cbed008aa1cb53bc41062
epriestley <git@epriestley.com>
2014-07-02 04:59:15 -0700

    Add a header warning to revisions that need a legal document signature
    Summary: This supplements the footer warning and makes it more visible for authors.
    
    Test Plan: {F173277}
    
    Reviewers: btrahan, chad
    
    Reviewed By: chad
    
    Subscribers: epriestley
    
    Differential Revision: https://secure.phabricator.com/D9794

--- src/applications/differential/customfield/DifferentialRequiredSignaturesField.php
+++ src/applications/differential/customfield/DifferentialRequiredSignaturesField.php
@@ -130,5 +130,17 @@ final class DifferentialRequiredSignaturesField
     return false;
   }
 
+  public function getWarningsForRevisionHeader(array $handles) {
+    if (!$this->haveAnyUnsignedDocuments()) {
+      return array();
+    }
+
+    return array(
+      pht(
+        'This revision can not be accepted until the required legal '.
+        'agreements have been signed.'),
+    );
+  }
+
 
 }
