5a158b5b193276a822ca179c329eeee3948cfcef
epriestley <git@epriestley.com>
2014-07-02 04:58:51 -0700

    Move revision header warnings into custom fields
    Summary:
    Ref T5495. We currently show one warning in revision headers, about not having any reviewers.
    
    I want to add a second warning (for missing Legalpad signatures). At least one install would like to add custom warnings (see T5495) which are so specific that we can't reasonably cover them in the upstream.
    
    Generalize these header warnings by moving them to CustomField, so I can implement the Legalpad stuff without making a mess and the install in T5495 can use an extension.
    
    Test Plan:
    Hit all three header states, they look exactly like they did before this change:
    
    {F173265}
    
    Reviewers: btrahan, chad
    
    Reviewed By: chad
    
    Subscribers: epriestley
    
    Maniphest Tasks: T5495
    
    Differential Revision: https://secure.phabricator.com/D9793

--- src/applications/differential/customfield/DifferentialReviewersField.php
+++ src/applications/differential/customfield/DifferentialReviewersField.php
@@ -191,5 +191,35 @@ final class DifferentialReviewersField
     }
   }
 
+  public function getRequiredHandlePHIDsForRevisionHeaderWarnings() {
+    return mpull($this->getValue(), 'getReviewerPHID');
+  }
+
+  public function getWarningsForRevisionHeader(array $handles) {
+    $revision = $this->getObject();
+
+    $status_needs_review = ArcanistDifferentialRevisionStatus::NEEDS_REVIEW;
+    if ($revision->getStatus() != $status_needs_review) {
+      return array();
+    }
+
+    foreach ($this->getValue() as $reviewer) {
+      if (!$handles[$reviewer->getReviewerPHID()]->isDisabled()) {
+        return array();
+      }
+    }
+
+    $warnings = array();
+    if ($this->getValue()) {
+      $warnings[] = pht(
+        'This revision needs review, but all specified reviewers are '.
+        'disabled or inactive.');
+    } else {
+      $warnings[] = pht(
+        'This revision needs review, but there are no reviewers specified.');
+    }
+
+    return $warnings;
+  }
 
 }
