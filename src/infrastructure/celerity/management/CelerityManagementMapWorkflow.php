phacility/phabricator

commit 7c71e1aefa00c5dfd4f5fbcfd06f254ad11419bc

Use `phutil_var_export` in `CelerityManagementMapWorkflow`
Summary: `phutil_var_export` produces more readable output than `var_export`. Depends on D9923.

Test Plan: Eye-balled the Celerity map.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: epriestley, Korvin

Differential Revision: https://secure.phabricator.com/D9926

src/infrastructure/celerity/management/CelerityManagementMapWorkflow.php
@@ -351,9 +351,7 @@ final class CelerityManagementMapWorkflow
   }
 
   private function formatMapContent(array $data) {
-    $content = var_export($data, true);
-    $content = preg_replace('/\s+$/m', '', $content);
-    $content = preg_replace('/array \(/', 'array(', $content);
+    $content = phutil_var_export($data, true);
 
     $generated = '@'.'generated';
     return <<<EOFILE

commit 24d47643475203e23fd1cd3ddb6ad93648098e45

Minor fix for `CelerityManagementMapWorkflow`
Summary: `phutil_var_export` only accepts a single parameter. This bug was introduced in D9926.

Test Plan: Eye-ball it.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: epriestley, Korvin

Differential Revision: https://secure.phabricator.com/D9930

src/infrastructure/celerity/management/CelerityManagementMapWorkflow.php
@@ -351,7 +351,7 @@ final class CelerityManagementMapWorkflow
   }
 
   private function formatMapContent(array $data) {
-    $content = phutil_var_export($data, true);
+    $content = phutil_var_export($data);
 
     $generated = '@'.'generated';
     return <<<EOFILE