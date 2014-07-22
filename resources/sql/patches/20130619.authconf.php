phacility/phabricator

commit 701bb2ac6ec99d0c754d3bf19216ec79232e10d4

Rename auth classes for consistency
Summary: Ref T5655. Depends on D9998.

Test Plan: `arc unit`

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: 20after4, epriestley, Korvin

Maniphest Tasks: T5655

Differential Revision: https://secure.phabricator.com/D9999

src/applications/auth/provider/PhabricatorJIRAAuthProvider.php
@@ -1,7 +1,6 @@
 <?php
 
-final class PhabricatorAuthProviderOAuth1JIRA
-  extends PhabricatorAuthProviderOAuth1 {
+final class PhabricatorJIRAAuthProvider extends PhabricatorOAuth1AuthProvider {
 
   public function getJIRABaseURI() {
     return $this->getProviderConfig()->getProperty(self::PROPERTY_JIRA_URI);
@@ -58,7 +57,7 @@ final class PhabricatorAuthProviderOAuth1JIRA
   protected function newOAuthAdapter() {
     $config = $this->getProviderConfig();
 
-    return id(new PhutilAuthAdapterOAuthJIRA())
+    return id(new PhutilJIRAAuthAdapter())
       ->setAdapterDomain($config->getProviderDomain())
       ->setJIRABaseURI($config->getProperty(self::PROPERTY_JIRA_URI))
       ->setPrivateKey(
@@ -143,7 +142,7 @@ final class PhabricatorAuthProviderOAuth1JIRA
       $config->setProviderDomain($values[$key_name]);
 
       $consumer_key = 'phjira.'.Filesystem::readRandomCharacters(16);
-      list($public, $private) = PhutilAuthAdapterOAuthJIRA::newJIRAKeypair();
+      list($public, $private) = PhutilJIRAAuthAdapter::newJIRAKeypair();
 
       $config->setProperty(self::PROPERTY_PUBLIC_KEY, $public);
       $config->setProperty(self::PROPERTY_PRIVATE_KEY, $private);

src/applications/auth/provider/PhabricatorLDAPAuthProvider.php
@@ -1,7 +1,6 @@
 <?php
 
-final class PhabricatorAuthProviderLDAP
-  extends PhabricatorAuthProvider {
+final class PhabricatorLDAPAuthProvider extends PhabricatorAuthProvider {
 
   private $adapter;
 
@@ -34,7 +33,7 @@ final class PhabricatorAuthProviderLDAP
       $search_attributes = phutil_split_lines($search_attributes, false);
       $search_attributes = array_filter($search_attributes);
 
-      $adapter = id(new PhutilAuthAdapterLDAP())
+      $adapter = id(new PhutilLDAPAuthAdapter())
         ->setHostname(
           $conf->getProperty(self::KEY_HOSTNAME))
         ->setPort(
@@ -160,7 +159,7 @@ final class PhabricatorAuthProviderLDAP
           $adapter->setLoginPassword($password);
 
           // TODO: This calls ldap_bind() eventually, which dumps cleartext
-          // passwords to the error log. See note in PhutilAuthAdapterLDAP.
+          // passwords to the error log. See note in PhutilLDAPAuthAdapter.
           // See T3351.
 
           DarkConsoleErrorLogPluginAPI::enableDiscardMode();

src/applications/auth/provider/PhabricatorPasswordAuthProvider.php
@@ -1,7 +1,6 @@
 <?php
 
-final class PhabricatorAuthProviderPassword
-  extends PhabricatorAuthProvider {
+final class PhabricatorPasswordAuthProvider extends PhabricatorAuthProvider {
 
   private $adapter;
 

src/applications/auth/provider/PhabricatorAmazonAuthProvider.php
@@ -1,7 +1,7 @@
 <?php
 
-final class PhabricatorAuthProviderOAuthAmazon
-  extends PhabricatorAuthProviderOAuth2 {
+final class PhabricatorAmazonAuthProvider
+  extends PhabricatorOAuth2AuthProvider {
 
   public function getProviderName() {
     return pht('Amazon');
@@ -36,7 +36,7 @@ final class PhabricatorAuthProviderOAuthAmazon
   }
 
   protected function newOAuthAdapter() {
-    return new PhutilAuthAdapterOAuthAmazon();
+    return new PhutilAmazonAuthAdapter();
   }
 
   protected function getLoginIcon() {

src/applications/auth/provider/PhabricatorAsanaAuthProvider.php
@@ -1,7 +1,6 @@
 <?php
 
-final class PhabricatorAuthProviderOAuthAsana
-  extends PhabricatorAuthProviderOAuth2 {
+final class PhabricatorAsanaAuthProvider extends PhabricatorOAuth2AuthProvider {
 
   public function getProviderName() {
     return pht('Asana');
@@ -28,7 +27,7 @@ final class PhabricatorAuthProviderOAuthAsana
   }
 
   protected function newOAuthAdapter() {
-    return new PhutilAuthAdapterOAuthAsana();
+    return new PhutilAsanaAuthAdapter();
   }
 
   protected function getLoginIcon() {
@@ -39,7 +38,7 @@ final class PhabricatorAuthProviderOAuthAsana
     $providers = self::getAllEnabledProviders();
 
     foreach ($providers as $provider) {
-      if ($provider instanceof PhabricatorAuthProviderOAuthAsana) {
+      if ($provider instanceof PhabricatorAsanaAuthProvider) {
         return $provider;
       }
     }

src/applications/auth/provider/PhabricatorBitbucketAuthProvider.php
@@ -1,7 +1,7 @@
 <?php
 
-final class PhabricatorAuthProviderOAuth1Bitbucket
-  extends PhabricatorAuthProviderOAuth1 {
+final class PhabricatorBitbucketAuthProvider
+  extends PhabricatorOAuth1AuthProvider {
 
   public function getProviderName() {
     return pht('Bitbucket');
@@ -17,7 +17,7 @@ final class PhabricatorAuthProviderOAuth1Bitbucket
   }
 
   protected function newOAuthAdapter() {
-    return new PhutilAuthAdapterOAuthBitbucket();
+    return new PhutilBitbucketAuthAdapter();
   }
 
   protected function getLoginIcon() {

src/applications/auth/provider/PhabricatorDisqusAuthProvider.php
@@ -1,7 +1,7 @@
 <?php
 
-final class PhabricatorAuthProviderOAuthDisqus
-  extends PhabricatorAuthProviderOAuth2 {
+final class PhabricatorDisqusAuthProvider
+  extends PhabricatorOAuth2AuthProvider {
 
   public function getProviderName() {
     return pht('Disqus');
@@ -26,7 +26,7 @@ final class PhabricatorAuthProviderOAuthDisqus
   }
 
   protected function newOAuthAdapter() {
-    return new PhutilAuthAdapterOAuthDisqus();
+    return new PhutilDisqusAuthAdapter();
   }
 
   protected function getLoginIcon() {

commit f4b05312cdf5aed45ef38deee17e44f464aafef9

Fix broken references to auth adapters
This was broken in D9999 but somehow didn't fail linting or unit tests.

Auditors: epriestley

resources/sql/patches/20130619.authconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $config_map = array(
-  'PhabricatorAuthProviderLDAP'           => array(
+  'PhabricatorLDAPAuthProvider'           => array(
     'enabled' => 'ldap.auth-enabled',
     'registration' => true,
     'type' => 'ldap',
@@ -16,7 +16,7 @@ $config_map = array(
     'type' => 'disqus',
     'domain' => 'disqus.com',
   ),
-  'PhabricatorAuthProviderOAuthFacebook'  => array(
+  'PhabricatorFacebookAuthProvider'  => array(
     'enabled' => 'facebook.auth-enabled',
     'registration' => 'facebook.registration-enabled',
     'permanent' => 'facebook.auth-permanent',
@@ -43,7 +43,7 @@ $config_map = array(
     'type' => 'google',
     'domain' => 'google.com',
   ),
-  'PhabricatorAuthProviderPassword'       => array(
+  'PhabricatorPasswordAuthProvider'       => array(
     'enabled' => 'auth.password-auth-enabled',
     'enabled-default' => false,
     'registration' => false,
@@ -105,42 +105,42 @@ foreach ($config_map as $provider_class => $spec) {
   }
 
   switch ($provider_class) {
-    case 'PhabricatorAuthProviderOAuthFacebook':
+    case 'PhabricatorFacebookAuthProvider':
       $config->setProperty(
-        PhabricatorAuthProviderOAuthFacebook::KEY_REQUIRE_SECURE,
+        PhabricatorFacebookAuthProvider::KEY_REQUIRE_SECURE,
         (int)PhabricatorEnv::getEnvConfigIfExists(
           'facebook.require-https-auth'));
       break;
-    case 'PhabricatorAuthProviderLDAP':
+    case 'PhabricatorLDAPAuthProvider':
 
       $ldap_map = array(
-        PhabricatorAuthProviderLDAP::KEY_HOSTNAME
+        PhabricatorLDAPAuthProvider::KEY_HOSTNAME
           => 'ldap.hostname',
-        PhabricatorAuthProviderLDAP::KEY_PORT
+        PhabricatorLDAPAuthProvider::KEY_PORT
           => 'ldap.port',
-        PhabricatorAuthProviderLDAP::KEY_DISTINGUISHED_NAME
+        PhabricatorLDAPAuthProvider::KEY_DISTINGUISHED_NAME
           => 'ldap.base_dn',
-        PhabricatorAuthProviderLDAP::KEY_SEARCH_ATTRIBUTES
+        PhabricatorLDAPAuthProvider::KEY_SEARCH_ATTRIBUTES
           => 'ldap.search_attribute',
-        PhabricatorAuthProviderLDAP::KEY_USERNAME_ATTRIBUTE
+        PhabricatorLDAPAuthProvider::KEY_USERNAME_ATTRIBUTE
           => 'ldap.username-attribute',
-        PhabricatorAuthProviderLDAP::KEY_REALNAME_ATTRIBUTES
+        PhabricatorLDAPAuthProvider::KEY_REALNAME_ATTRIBUTES
           => 'ldap.real_name_attributes',
-        PhabricatorAuthProviderLDAP::KEY_VERSION
+        PhabricatorLDAPAuthProvider::KEY_VERSION
           => 'ldap.version',
-        PhabricatorAuthProviderLDAP::KEY_REFERRALS
+        PhabricatorLDAPAuthProvider::KEY_REFERRALS
           => 'ldap.referrals',
-        PhabricatorAuthProviderLDAP::KEY_START_TLS
+        PhabricatorLDAPAuthProvider::KEY_START_TLS
           => 'ldap.start-tls',
-        PhabricatorAuthProviderLDAP::KEY_ANONYMOUS_USERNAME
+        PhabricatorLDAPAuthProvider::KEY_ANONYMOUS_USERNAME
           => 'ldap.anonymous-user-name',
-        PhabricatorAuthProviderLDAP::KEY_ANONYMOUS_PASSWORD
+        PhabricatorLDAPAuthProvider::KEY_ANONYMOUS_PASSWORD
           => 'ldap.anonymous-user-password',
         // Update the old "search first" setting to the newer but similar
         // "always search" setting.
-        PhabricatorAuthProviderLDAP::KEY_ALWAYS_SEARCH
+        PhabricatorLDAPAuthProvider::KEY_ALWAYS_SEARCH
           => 'ldap.search-first',
-        PhabricatorAuthProviderLDAP::KEY_ACTIVEDIRECTORY_DOMAIN
+        PhabricatorLDAPAuthProvider::KEY_ACTIVEDIRECTORY_DOMAIN
           => 'ldap.activedirectory_domain',
       );
 

src/applications/auth/controller/PhabricatorAuthOneTimeLoginController.php
@@ -109,7 +109,7 @@ final class PhabricatorAuthOneTimeLoginController
       unset($unguarded);
 
       $next = '/';
-      if (!PhabricatorAuthProviderPassword::getPasswordProvider()) {
+      if (!PhabricatorPasswordAuthProvider::getPasswordProvider()) {
         $next = '/settings/panel/external/';
       } else if (PhabricatorEnv::getEnvConfig('account.editable')) {
 

src/applications/auth/controller/PhabricatorAuthRegisterController.php
@@ -494,7 +494,7 @@ final class PhabricatorAuthRegisterController
   }
 
   private function loadSetupAccount() {
-    $provider = new PhabricatorAuthProviderPassword();
+    $provider = new PhabricatorPasswordAuthProvider();
     $provider->attachProviderConfig(
       id(new PhabricatorAuthProviderConfig())
         ->setShouldAllowRegistration(1)

src/applications/auth/controller/PhabricatorEmailLoginController.php
@@ -10,7 +10,7 @@ final class PhabricatorEmailLoginController
   public function processRequest() {
     $request = $this->getRequest();
 
-    if (!PhabricatorAuthProviderPassword::getPasswordProvider()) {
+    if (!PhabricatorPasswordAuthProvider::getPasswordProvider()) {
       return new Aphront400Response();
     }
 

src/applications/auth/management/PhabricatorAuthManagementLDAPWorkflow.php
@@ -15,7 +15,7 @@ final class PhabricatorAuthManagementLDAPWorkflow
     $console = PhutilConsole::getConsole();
     $console->getServer()->setEnableLog(true);
 
-    $provider = PhabricatorAuthProviderLDAP::getLDAPProvider();
+    $provider = PhabricatorLDAPAuthProvider::getLDAPProvider();
     if (!$provider) {
       $console->writeOut(
         "%s\n",

src/applications/auth/management/PhabricatorAuthManagementRefreshWorkflow.php
@@ -101,7 +101,7 @@ final class PhabricatorAuthManagementRefreshWorkflow
       }
 
       $provider = $providers[$key];
-      if (!($provider instanceof PhabricatorAuthProviderOAuth2)) {
+      if (!($provider instanceof PhabricatorOAuth2AuthProvider)) {
         $console->writeOut(
           "> %s\n",
           pht('Skipping, provider is not an OAuth2 provider.'));

src/applications/auth/provider/PhabricatorAuthProvider.php
@@ -149,7 +149,7 @@ abstract class PhabricatorAuthProvider {
   /**
    * Should we allow the adapter to be marked as "trusted"
    * This is true for all adapters except those that allow the user to type in
-   * emails (@see PhabricatorAuthProviderPassword)
+   * emails (@see PhabricatorPasswordAuthProvider)
    */
   public function shouldAllowEmailTrustConfiguration() {
     return true;

src/applications/auth/provider/PhabricatorJIRAAuthProvider.php
@@ -265,7 +265,7 @@ final class PhabricatorJIRAAuthProvider extends PhabricatorOAuth1AuthProvider {
     $providers = self::getAllEnabledProviders();
 
     foreach ($providers as $provider) {
-      if ($provider instanceof PhabricatorAuthProviderOAuth1JIRA) {
+      if ($provider instanceof PhabricatorJIRAAuthProvider) {
         return $provider;
       }
     }

src/applications/auth/provider/PhabricatorLDAPAuthProvider.php
@@ -465,7 +465,7 @@ final class PhabricatorLDAPAuthProvider extends PhabricatorAuthProvider {
     $providers = self::getAllEnabledProviders();
 
     foreach ($providers as $provider) {
-      if ($provider instanceof PhabricatorAuthProviderLDAP) {
+      if ($provider instanceof PhabricatorLDAPAuthProvider) {
         return $provider;
       }
     }

src/applications/auth/provider/PhabricatorPasswordAuthProvider.php
@@ -104,7 +104,7 @@ final class PhabricatorPasswordAuthProvider extends PhabricatorAuthProvider {
 
   public function getAdapter() {
     if (!$this->adapter) {
-      $adapter = new PhutilAuthAdapterEmpty();
+      $adapter = new PhutilEmptyAuthAdapter();
       $adapter->setAdapterType('password');
       $adapter->setAdapterDomain('self');
       $this->adapter = $adapter;
@@ -330,7 +330,7 @@ final class PhabricatorPasswordAuthProvider extends PhabricatorAuthProvider {
     $providers = self::getAllEnabledProviders();
 
     foreach ($providers as $provider) {
-      if ($provider instanceof PhabricatorAuthProviderPassword) {
+      if ($provider instanceof PhabricatorPasswordAuthProvider) {
         return $provider;
       }
     }

src/applications/differential/customfield/DifferentialJIRAIssuesField.php
@@ -14,7 +14,7 @@ final class DifferentialJIRAIssuesField
   }
 
   public function isFieldEnabled() {
-    return (bool)PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    return (bool)PhabricatorJIRAAuthProvider::getJIRAProvider();
   }
 
   public function canDisableField() {
@@ -66,7 +66,7 @@ final class DifferentialJIRAIssuesField
   }
 
   private function buildDoorkeeperRefs($value) {
-    $provider = PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    $provider = PhabricatorJIRAAuthProvider::getJIRAProvider();
 
     $refs = array();
     if ($value) {
@@ -97,11 +97,11 @@ final class DifferentialJIRAIssuesField
   }
 
   public function shouldAppearInEditView() {
-    return PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    return PhabricatorJIRAAuthProvider::getJIRAProvider();
   }
 
   public function shouldAppearInApplicationTransactions() {
-    return PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    return PhabricatorJIRAAuthProvider::getJIRAProvider();
   }
 
   public function readValueFromRequest(AphrontRequest $request) {

src/applications/doorkeeper/bridge/DoorkeeperBridgeAsana.php
@@ -27,7 +27,7 @@ final class DoorkeeperBridgeAsana extends DoorkeeperBridge {
     $id_map = mpull($refs, 'getObjectID', 'getObjectKey');
     $viewer = $this->getViewer();
 
-    $provider = PhabricatorAuthProviderOAuthAsana::getAsanaProvider();
+    $provider = PhabricatorAsanaAuthProvider::getAsanaProvider();
     if (!$provider) {
       return;
     }

src/applications/doorkeeper/bridge/DoorkeeperBridgeJIRA.php
@@ -22,7 +22,7 @@ final class DoorkeeperBridgeJIRA extends DoorkeeperBridge {
     $id_map = mpull($refs, 'getObjectID', 'getObjectKey');
     $viewer = $this->getViewer();
 
-    $provider = PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    $provider = PhabricatorJIRAAuthProvider::getJIRAProvider();
     if (!$provider) {
       return;
     }

src/applications/doorkeeper/option/PhabricatorAsanaConfigOptions.php
@@ -49,7 +49,7 @@ final class PhabricatorAsanaConfigOptions
 
     $viewer = $request->getUser();
 
-    $provider = PhabricatorAuthProviderOAuthAsana::getAsanaProvider();
+    $provider = PhabricatorAsanaAuthProvider::getAsanaProvider();
     if (!$provider) {
       return null;
     }

src/applications/doorkeeper/remarkup/DoorkeeperRemarkupRuleJIRA.php
@@ -15,7 +15,7 @@ final class DoorkeeperRemarkupRuleJIRA
     $match_issue = $matches[2];
 
     // TODO: When we support multiple instances, deal with them here.
-    $provider = PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    $provider = PhabricatorJIRAAuthProvider::getJIRAProvider();
     if (!$provider) {
       return $matches[0];
     }

src/applications/doorkeeper/worker/DoorkeeperFeedWorkerAsana.php
@@ -445,7 +445,7 @@ final class DoorkeeperFeedWorkerAsana extends DoorkeeperFeedWorker {
 
   private function getProvider() {
     if (!$this->provider) {
-      $provider = PhabricatorAuthProviderOAuthAsana::getAsanaProvider();
+      $provider = PhabricatorAsanaAuthProvider::getAsanaProvider();
       if (!$provider) {
         throw new PhabricatorWorkerPermanentFailureException(
           'No Asana provider configured.');
@@ -515,7 +515,7 @@ final class DoorkeeperFeedWorkerAsana extends DoorkeeperFeedWorker {
       return $phid_map;
     }
 
-    $provider = PhabricatorAuthProviderOAuthAsana::getAsanaProvider();
+    $provider = PhabricatorAsanaAuthProvider::getAsanaProvider();
 
     $accounts = id(new PhabricatorExternalAccountQuery())
       ->setViewer(PhabricatorUser::getOmnipotentUser())

src/applications/doorkeeper/worker/DoorkeeperFeedWorkerJIRA.php
@@ -16,7 +16,7 @@ final class DoorkeeperFeedWorkerJIRA extends DoorkeeperFeedWorker {
    * This worker is enabled when a JIRA authentication provider is active.
    */
   public function isEnabled() {
-    return (bool)PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+    return (bool)PhabricatorJIRAAuthProvider::getJIRAProvider();
   }
 
 
@@ -105,12 +105,12 @@ final class DoorkeeperFeedWorkerJIRA extends DoorkeeperFeedWorker {
   /**
    * Get the active JIRA provider.
    *
-   * @return PhabricatorAuthProviderOAuth1JIRA Active JIRA auth provider.
+   * @return PhabricatorJIRAAuthProvider Active JIRA auth provider.
    * @task internal
    */
   private function getProvider() {
     if (!$this->provider) {
-      $provider = PhabricatorAuthProviderOAuth1JIRA::getJIRAProvider();
+      $provider = PhabricatorJIRAAuthProvider::getJIRAProvider();
       if (!$provider) {
         throw new PhabricatorWorkerPermanentFailureException(
           'No JIRA provider configured.');

src/applications/people/controller/PhabricatorPeopleController.php
@@ -18,7 +18,7 @@ abstract class PhabricatorPeopleController extends PhabricatorController {
 
     if ($viewer->getIsAdmin()) {
       $nav->addLabel(pht('User Administration'));
-      if (PhabricatorAuthProviderLDAP::getLDAPProvider()) {
+      if (PhabricatorLDAPAuthProvider::getLDAPProvider()) {
         $nav->addFilter('ldap', pht('Import from LDAP'));
       }
 

src/applications/people/controller/PhabricatorPeopleLdapController.php
@@ -130,7 +130,7 @@ final class PhabricatorPeopleLdapController
 
     $search = $request->getStr('query');
 
-    $ldap_provider = PhabricatorAuthProviderLDAP::getLDAPProvider();
+    $ldap_provider = PhabricatorLDAPAuthProvider::getLDAPProvider();
     if (!$ldap_provider) {
       throw new Exception('No LDAP provider enabled!');
     }

src/applications/people/storage/PhabricatorUser.php
@@ -550,7 +550,7 @@ EOBODY;
     $new_username = $this->getUserName();
 
     $password_instructions = null;
-    if (PhabricatorAuthProviderPassword::getPasswordProvider()) {
+    if (PhabricatorPasswordAuthProvider::getPasswordProvider()) {
       $engine = new PhabricatorAuthSessionEngine();
       $uri = $engine->getOneTimeLoginURI(
         $this,

src/applications/phame/storage/PhamePost.php
@@ -124,7 +124,7 @@ final class PhamePost extends PhameDAO
     $options = array();
 
     if ($current == 'facebook' ||
-        PhabricatorAuthProviderOAuthFacebook::getFacebookApplicationID()) {
+        PhabricatorFacebookAuthProvider::getFacebookApplicationID()) {
       $options['facebook'] = 'Facebook';
     }
     if ($current == 'disqus' ||

src/applications/phame/view/PhamePostView.php
@@ -147,7 +147,7 @@ final class PhamePostView extends AphrontView {
   }
 
   private function renderFacebookComments() {
-    $fb_id = PhabricatorAuthProviderOAuthFacebook::getFacebookApplicationID();
+    $fb_id = PhabricatorFacebookAuthProvider::getFacebookApplicationID();
     if (!$fb_id) {
       return null;
     }

src/applications/settings/panel/PhabricatorSettingsPanelPassword.php
@@ -25,7 +25,7 @@ final class PhabricatorSettingsPanelPassword
 
     // ...or this install doesn't support password authentication at all.
 
-    if (!PhabricatorAuthProviderPassword::getPasswordProvider()) {
+    if (!PhabricatorPasswordAuthProvider::getPasswordProvider()) {
       return false;
     }
 