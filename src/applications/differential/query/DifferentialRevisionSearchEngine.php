bf10b7602bece9604fd0e41511e319ee4c6be6f5
Anirudh Sanjeev <anirudh@razorflow.com>
2014-07-30 13:52:26 -0700

    Allow searching diffs by more status options
    Summary:
    This Fixes T5737. Apparently the functionality to search by different
    statuses in differential was already there, but the options weren't
    exposed in the frontend. I can't think of any reason why this should've
    been the case, so I just added the other options.
    
    Test Plan: Tested against some local diffs to match new query option.
    
    Reviewers: joshuaspence, epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T5737
    
    Differential Revision: https://secure.phabricator.com/D10076

--- src/applications/differential/query/DifferentialRevisionSearchEngine.php
+++ src/applications/differential/query/DifferentialRevisionSearchEngine.php
@@ -236,10 +236,13 @@ final class DifferentialRevisionSearchEngine
 
   private function getStatusOptions() {
     return array(
-      DifferentialRevisionQuery::STATUS_ANY       => pht('All'),
-      DifferentialRevisionQuery::STATUS_OPEN      => pht('Open'),
-      DifferentialRevisionQuery::STATUS_CLOSED    => pht('Closed'),
-      DifferentialRevisionQuery::STATUS_ABANDONED => pht('Abandoned'),
+      DifferentialRevisionQuery::STATUS_ANY            => pht('All'),
+      DifferentialRevisionQuery::STATUS_OPEN           => pht('Open'),
+      DifferentialRevisionQuery::STATUS_ACCEPTED       => pht('Accepted'),
+      DifferentialRevisionQuery::STATUS_NEEDS_REVIEW   => pht('Needs Review'),
+      DifferentialRevisionQuery::STATUS_NEEDS_REVISION => pht('Needs Revision'),
+      DifferentialRevisionQuery::STATUS_CLOSED         => pht('Closed'),
+      DifferentialRevisionQuery::STATUS_ABANDONED      => pht('Abandoned'),
     );
   }
 
