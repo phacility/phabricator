ed98a1cc8464192d939e2b4542b9f093f1a92737
Bob Trahan <btrahan@phacility.com>
2014-08-19 12:01:17 -0700

    Paste - fix caching mechanism for S3-stored files
    Summary: Fixes T5798. We basically weren't using the caching mechanism. Also adds service calls for S3 stuff, and support for seeing a little info like you can for conduit.
    
    Test Plan: uploaded a paste, looked at paste list - no s3 service calls. edited the paste, looked at paste list - no s3 service calls and edited content properly shown
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T5798
    
    Differential Revision: https://secure.phabricator.com/D10294

--- src/applications/paste/query/PhabricatorPasteSearchEngine.php
+++ src/applications/paste/query/PhabricatorPasteSearchEngine.php
@@ -27,7 +27,7 @@ final class PhabricatorPasteSearchEngine
 
   public function buildQueryFromSavedQuery(PhabricatorSavedQuery $saved) {
     $query = id(new PhabricatorPasteQuery())
-      ->needRawContent(true)
+      ->needContent(true)
       ->withAuthorPHIDs($saved->getParameter('authorPHIDs', array()))
       ->withLanguages($saved->getParameter('languages', array()));
 
@@ -149,7 +149,7 @@ final class PhabricatorPasteSearchEngine
       $created = phabricator_date($paste->getDateCreated(), $viewer);
       $author = $handles[$paste->getAuthorPHID()]->renderLink();
 
-      $lines = phutil_split_lines($paste->getRawContent());
+      $lines = phutil_split_lines($paste->getContent());
 
       $preview = id(new PhabricatorSourceCodeView())
         ->setLimit(5)
