063a5bf0da805f3ca1da7f6713f4cd4a5986e191
Craig Silverstein <csilvers@khanacademy.org>
2014-09-10 10:05:46 -0700

    I had a merge error, I don't know how, but I didn't resolve it right.
    This now matches what's on origin/master.
    
    Test Plan:
    Fingers crossed.

--- src/applications/transactions/interface/PhabricatorMentionableInterface.php
+++ src/applications/transactions/interface/PhabricatorMentionableInterface.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /**
@@ -11,14 +10,3 @@ interface PhabricatorMentionableInterface {
   public function getPHID();
 
 }
-||||||| merged common ancestors
-=======
-<?php
-
-/**
- * Allow infrastructure to automagically create "mentioned" transactions
- * - actually TYPE_EDGE transactions that add "mentioned" edges - to the
- * implementing object.
- */
-interface PhabricatorMentionableInterface {}
->>>>>>> bee1e09fd13086dab42f11cb54d489e7e7d5a0aa
