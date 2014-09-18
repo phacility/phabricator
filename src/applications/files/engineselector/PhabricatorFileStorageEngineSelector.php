ea602a082a1edac406817b660c35cd1f32e25f08
epriestley <git@epriestley.com>
2014-09-18 08:21:34 -0700

    Fix "are are" in explanatory text
    Summary: See rP8806fb0296c2.
    
    Test Plan:
    me fail english
    
    with bonus!
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: epriestley
    
    Differential Revision: https://secure.phabricator.com/D10514

--- src/applications/files/engineselector/PhabricatorFileStorageEngineSelector.php
+++ src/applications/files/engineselector/PhabricatorFileStorageEngineSelector.php
@@ -23,7 +23,7 @@ abstract class PhabricatorFileStorageEngineSelector {
    * Phabricator when it needs to store a file permanently. It must return a
    * list of valid @{class:PhabricatorFileStorageEngine}s.
    *
-   * If you are are extending this class to provide a custom selector, you
+   * If you are extending this class to provide a custom selector, you
    * probably just want it to look like this:
    *
    *   return array(new MyCustomFileStorageEngine());
