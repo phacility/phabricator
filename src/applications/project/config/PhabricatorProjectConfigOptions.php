7ee7f14b79998538f891ef3d80ca32d9e5a5683d
Bob Trahan <btrahan@phacility.com>
2014-10-21 12:31:18 -0700

    Projects - fix typo in config
    Summary: type "string" should be type "text".  Ref T6366.
    
    Test Plan: viewed config and saw appropriate, working example
    
    Reviewers: epriestley, chad
    
    Reviewed By: chad
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T6366
    
    Differential Revision: https://secure.phabricator.com/D10736

--- src/applications/project/config/PhabricatorProjectConfigOptions.php
+++ src/applications/project/config/PhabricatorProjectConfigOptions.php
@@ -32,7 +32,7 @@ final class PhabricatorProjectConfigOptions
             'Array of custom fields for Projects.'))
         ->addExample(
           '{"mycompany:motto": {"name": "Project Motto", '.
-          '"type": "string"}}',
+          '"type": "text"}}',
           pht('Valid Setting')),
       $this->newOption('projects.fields', $custom_field_type, $default_fields)
         ->setCustomData(id(new PhabricatorProject())->getCustomFieldBaseClass())
