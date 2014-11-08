phacility/phabricator

commit 2ecc04c159b0a021ed71792487710a1719758793

Audit - move over to application search
Summary: ...also kills off "PhabricatorAuditCommitQuery" and "PhabricatorAuditQuery", by moving the work to "DiffusionCommitQuery". Generally cleans up some code around the joint on this too. Also provides policies for audit requests, which is basically the policy for the underlying commit. Fixes T4715. (For the TODO I added about files, I just grabbed T4713.)

Test Plan:
Audit: verified the three default views all showed the correct things, including highligthing. did some custom queries and got the correct results.
Diffusion: verified "blame view" still worked. verified paths were highlighted for packages i owned.
Home: verified audit boxes showed up with proper commits w/ audits
bin/audit: played around with it via --dry-run and got the right audits back

Reviewers: epriestley

Reviewed By: epriestley

Subscribers: chad, epriestley, Korvin

Maniphest Tasks: T4715

Differential Revision: https://secure.phabricator.com/D8805

src/applications/owners/controller/PhabricatorOwnersDetailController.php
@@ -145,17 +145,17 @@ final class PhabricatorOwnersDetailController
           'phid'    => $package->getPHID(),
         ));
 
-    $attention_query = id(new PhabricatorAuditCommitQuery())
-      ->withPackagePHIDs(array($package->getPHID()))
-      ->withStatus(PhabricatorAuditCommitQuery::STATUS_CONCERN)
+    $attention_commits = id(new DiffusionCommitQuery())
+      ->setViewer($request->getUser())
+      ->withAuditorPHIDs(array($package->getPHID()))
+      ->withAuditStatus(DiffusionCommitQuery::AUDIT_STATUS_CONCERN)
       ->needCommitData(true)
-      ->needAudits(true)
-      ->setLimit(10);
-    $attention_commits = $attention_query->execute();
+      ->setLimit(10)
+      ->execute();
     if ($attention_commits) {
-      $view = new PhabricatorAuditCommitListView();
-      $view->setUser($user);
-      $view->setCommits($attention_commits);
+      $view = id(new PhabricatorAuditListView())
+        ->setUser($user)
+        ->setCommits($attention_commits);
 
       $commit_views[] = array(
         'view'    => $view,
@@ -170,17 +170,17 @@ final class PhabricatorOwnersDetailController
       );
     }
 
-    $all_query = id(new PhabricatorAuditCommitQuery())
-      ->withPackagePHIDs(array($package->getPHID()))
+    $all_commits = id(new DiffusionCommitQuery())
+      ->setViewer($request->getUser())
+      ->withAuditorPHIDs(array($package->getPHID()))
       ->needCommitData(true)
-      ->needAudits(true)
-      ->setLimit(100);
-    $all_commits = $all_query->execute();
+      ->setLimit(100)
+      ->execute();
 
-    $view = new PhabricatorAuditCommitListView();
-    $view->setUser($user);
-    $view->setCommits($all_commits);
-    $view->setNoDataString(pht('No commits in this package.'));
+    $view = id(new PhabricatorAuditListView())
+      ->setUser($user)
+      ->setCommits($all_commits)
+      ->setNoDataString(pht('No commits in this package.'));
 
     $commit_views[] = array(
       'view'    => $view,

src/__phutil_library_map__.php
@@ -1189,8 +1189,6 @@ phutil_register_library_map(array(
     'PhabricatorAuditAddCommentController' => 'applications/audit/controller/PhabricatorAuditAddCommentController.php',
     'PhabricatorAuditComment' => 'applications/audit/storage/PhabricatorAuditComment.php',
     'PhabricatorAuditCommentEditor' => 'applications/audit/editor/PhabricatorAuditCommentEditor.php',
-    'PhabricatorAuditCommitListView' => 'applications/audit/view/PhabricatorAuditCommitListView.php',
-    'PhabricatorAuditCommitQuery' => 'applications/audit/query/PhabricatorAuditCommitQuery.php',
     'PhabricatorAuditCommitStatusConstants' => 'applications/audit/constants/PhabricatorAuditCommitStatusConstants.php',
     'PhabricatorAuditController' => 'applications/audit/controller/PhabricatorAuditController.php',
     'PhabricatorAuditDAO' => 'applications/audit/storage/PhabricatorAuditDAO.php',
@@ -1201,7 +1199,6 @@ phutil_register_library_map(array(
     'PhabricatorAuditManagementDeleteWorkflow' => 'applications/audit/management/PhabricatorAuditManagementDeleteWorkflow.php',
     'PhabricatorAuditManagementWorkflow' => 'applications/audit/management/PhabricatorAuditManagementWorkflow.php',
     'PhabricatorAuditPreviewController' => 'applications/audit/controller/PhabricatorAuditPreviewController.php',
-    'PhabricatorAuditQuery' => 'applications/audit/query/PhabricatorAuditQuery.php',
     'PhabricatorAuditReplyHandler' => 'applications/audit/mail/PhabricatorAuditReplyHandler.php',
     'PhabricatorAuditStatusConstants' => 'applications/audit/constants/PhabricatorAuditStatusConstants.php',
     'PhabricatorAuthAccountView' => 'applications/auth/view/PhabricatorAuthAccountView.php',
@@ -1312,6 +1309,7 @@ phutil_register_library_map(array(
     'PhabricatorChatLogQuery' => 'applications/chatlog/query/PhabricatorChatLogQuery.php',
     'PhabricatorCommitBranchesField' => 'applications/repository/customfield/PhabricatorCommitBranchesField.php',
     'PhabricatorCommitCustomField' => 'applications/repository/customfield/PhabricatorCommitCustomField.php',
+    'PhabricatorCommitSearchEngine' => 'applications/audit/query/PhabricatorCommitSearchEngine.php',
     'PhabricatorCommitTagsField' => 'applications/repository/customfield/PhabricatorCommitTagsField.php',
     'PhabricatorCommonPasswords' => 'applications/auth/constants/PhabricatorCommonPasswords.php',
     'PhabricatorConduitAPIController' => 'applications/conduit/controller/PhabricatorConduitAPIController.php',
@@ -3926,7 +3924,6 @@ phutil_register_library_map(array(
       1 => 'PhabricatorMarkupInterface',
     ),
     'PhabricatorAuditCommentEditor' => 'PhabricatorEditor',
-    'PhabricatorAuditCommitListView' => 'AphrontView',
     'PhabricatorAuditController' => 'PhabricatorController',
     'PhabricatorAuditDAO' => 'PhabricatorLiskDAO',
     'PhabricatorAuditInlineComment' =>
@@ -3934,7 +3931,11 @@ phutil_register_library_map(array(
       0 => 'PhabricatorAuditDAO',
       1 => 'PhabricatorInlineCommentInterface',
     ),
-    'PhabricatorAuditListController' => 'PhabricatorAuditController',
+    'PhabricatorAuditListController' =>
+    array(
+      0 => 'PhabricatorAuditController',
+      1 => 'PhabricatorApplicationSearchResultsControllerInterface',
+    ),
     'PhabricatorAuditListView' => 'AphrontView',
     'PhabricatorAuditMailReceiver' => 'PhabricatorObjectMailReceiver',
     'PhabricatorAuditManagementDeleteWorkflow' => 'PhabricatorAuditManagementWorkflow',
@@ -4066,6 +4067,7 @@ phutil_register_library_map(array(
     'PhabricatorChatLogQuery' => 'PhabricatorCursorPagedPolicyAwareQuery',
     'PhabricatorCommitBranchesField' => 'PhabricatorCommitCustomField',
     'PhabricatorCommitCustomField' => 'PhabricatorCustomField',
+    'PhabricatorCommitSearchEngine' => 'PhabricatorApplicationSearchEngine',
     'PhabricatorCommitTagsField' => 'PhabricatorCommitCustomField',
     'PhabricatorCommonPasswords' => 'Phobject',
     'PhabricatorConduitAPIController' => 'PhabricatorConduitController',
@@ -4795,7 +4797,11 @@ phutil_register_library_map(array(
     'PhabricatorRepositoryArcanistProjectDeleteController' => 'PhabricatorRepositoryController',
     'PhabricatorRepositoryArcanistProjectEditController' => 'PhabricatorRepositoryController',
     'PhabricatorRepositoryArcanistProjectQuery' => 'PhabricatorCursorPagedPolicyAwareQuery',
-    'PhabricatorRepositoryAuditRequest' => 'PhabricatorRepositoryDAO',
+    'PhabricatorRepositoryAuditRequest' =>
+    array(
+      0 => 'PhabricatorRepositoryDAO',
+      1 => 'PhabricatorPolicyInterface',
+    ),
     'PhabricatorRepositoryBranch' => 'PhabricatorRepositoryDAO',
     'PhabricatorRepositoryCommit' =>
     array(

src/applications/audit/application/PhabricatorApplicationAudit.php
@@ -27,9 +27,7 @@ final class PhabricatorApplicationAudit extends PhabricatorApplication {
   public function getRoutes() {
     return array(
       '/audit/' => array(
-        '' => 'PhabricatorAuditListController',
-        'view/(?P<filter>[^/]+)/(?:(?P<name>[^/]+)/)?'
-          => 'PhabricatorAuditListController',
+        '(?:query/(?P<queryKey>[^/]+)/)?' => 'PhabricatorAuditListController',
         'addcomment/' => 'PhabricatorAuditAddCommentController',
         'preview/(?P<id>[1-9]\d*)/' => 'PhabricatorAuditPreviewController',
       ),
@@ -49,10 +47,11 @@ final class PhabricatorApplicationAudit extends PhabricatorApplication {
 
     $phids = PhabricatorAuditCommentEditor::loadAuditPHIDsForUser($user);
 
-    $commits = id(new PhabricatorAuditCommitQuery())
-      ->withAuthorPHIDs($phids)
-      ->withStatus(PhabricatorAuditCommitQuery::STATUS_CONCERN)
-      ->execute();
+    $query = id(new DiffusionCommitQuery())
+      ->setViewer($user)
+      ->withAuthorPHIDs(array($user->getPHID()))
+      ->withAuditStatus(DiffusionCommitQuery::AUDIT_STATUS_CONCERN);
+    $commits = $query->execute();
 
     $count = count($commits);
     $type = PhabricatorApplicationStatusView::TYPE_NEEDS_ATTENTION;
@@ -61,13 +60,14 @@ final class PhabricatorApplicationAudit extends PhabricatorApplication {
       ->setText(pht('%d Problem Commit(s)', $count))
       ->setCount($count);
 
-    $audits = id(new PhabricatorAuditQuery())
+    $query = id(new DiffusionCommitQuery())
+      ->setViewer($user)
       ->withAuditorPHIDs($phids)
-      ->withStatus(PhabricatorAuditQuery::STATUS_OPEN)
-      ->withAwaitingUser($user)
-      ->execute();
+      ->withAuditStatus(DiffusionCommitQuery::AUDIT_STATUS_OPEN)
+      ->withAuditAwaitingUser($user);
+    $commits = $query->execute();
 
-    $count = count($audits);
+    $count = count($commits);
     $type = PhabricatorApplicationStatusView::TYPE_WARNING;
     $status[] = id(new PhabricatorApplicationStatusView())
       ->setType($type)

src/applications/audit/conduit/ConduitAPI_audit_query_Method.php
@@ -31,7 +31,8 @@ final class ConduitAPI_audit_query_Method extends ConduitAPI_audit_Method {
 
   protected function execute(ConduitAPIRequest $request) {
 
-    $query = new PhabricatorAuditQuery();
+    $query = id(new DiffusionCommitQuery())
+      ->setViewer($request->getUser());
 
     $auditor_phids = $request->getValue('auditorPHIDs', array());
     if ($auditor_phids) {
@@ -40,26 +41,31 @@ final class ConduitAPI_audit_query_Method extends ConduitAPI_audit_Method {
 
     $commit_phids = $request->getValue('commitPHIDs', array());
     if ($commit_phids) {
-      $query->withCommitPHIDs($commit_phids);
+      $query->withPHIDs($commit_phids);
     }
 
-    $status = $request->getValue('status', PhabricatorAuditQuery::STATUS_ANY);
-    $query->withStatus($status);
+    $status = $request->getValue(
+      'status',
+      DiffusionCommitQuery::AUDIT_STATUS_ANY);
+    $query->withAuditStatus($status);
 
     $query->setOffset($request->getValue('offset', 0));
     $query->setLimit($request->getValue('limit', 100));
 
-    $requests = $query->execute();
+    $commits = $query->execute();
 
     $results = array();
-    foreach ($requests as $request) {
-      $results[] = array(
-        'id'              => $request->getID(),
-        'commitPHID'      => $request->getCommitPHID(),
-        'auditorPHID'     => $request->getAuditorPHID(),
-        'reasons'         => $request->getAuditReasons(),
-        'status'          => $request->getAuditStatus(),
-      );
+    foreach ($commits as $commit) {
+      $requests = $commit->getAudits();
+      foreach ($requests as $request) {
+        $results[] = array(
+          'id'              => $request->getID(),
+          'commitPHID'      => $request->getCommitPHID(),
+          'auditorPHID'     => $request->getAuditorPHID(),
+          'reasons'         => $request->getAuditReasons(),
+          'status'          => $request->getAuditStatus(),
+        );
+      }
     }
 
     return $results;

src/applications/audit/controller/PhabricatorAuditController.php
@@ -2,28 +2,18 @@
 
 abstract class PhabricatorAuditController extends PhabricatorController {
 
-  public $filter;
-
   public function buildSideNavView() {
 
+    $user = $this->getRequest()->getUser();
+
     $nav = new AphrontSideNavFilterView();
-    $nav->setBaseURI(new PhutilURI('/audit/view/'));
-    $nav->addLabel(pht('Active'));
-    $nav->addFilter('active', pht('Need Attention'));
+    $nav->setBaseURI(new PhutilURI($this->getApplicationURI()));
 
-    $nav->addLabel(pht('Audits'));
-    $nav->addFilter('audits', pht('All'));
-    $nav->addFilter('user', pht('By User'));
-    $nav->addFilter('project', pht('By Project'));
-    $nav->addFilter('package', pht('By Package'));
-    $nav->addFilter('repository', pht('By Repository'));
+    id(new PhabricatorCommitSearchEngine())
+      ->setViewer($user)
+      ->addNavigationItems($nav->getMenu());
 
-    $nav->addLabel(pht('Commits'));
-    $nav->addFilter('commits', pht('All'));
-    $nav->addFilter('author', pht('By Author'));
-    $nav->addFilter('packagecommits', pht('By Package'));
-
-    $this->filter = $nav->selectFilter($this->filter, 'active');
+    $nav->selectFilter(null);
 
     return $nav;
   }
@@ -32,18 +22,4 @@ abstract class PhabricatorAuditController extends PhabricatorController {
     return $this->buildSideNavView()->getMenu();
   }
 
-  public function buildStandardPageResponse($view, array $data) {
-
-    $page = $this->buildStandardPageView();
-
-    $page->setApplicationName(pht('Audit'));
-    $page->setBaseURI('/audit/');
-    $page->setTitle(idx($data, 'title'));
-    $page->setGlyph("\xE2\x9C\x8D");
-    $page->appendChild($view);
-
-    $response = new AphrontWebpageResponse();
-    return $response->setContent($page->render());
-
-  }
 }

commit 796428c810a0481660188ccf754da6ddc43e12ca

Owners / Audit - restore link to view audits related to an owners package.
Summary: Fixes T5387. I broke this in D8805. Fix it by using the application search parameters that D8805 introduced.

Test Plan: verified that the two links mentioned in T5387 worked for me. Also tried manual links on secure.phabricator.com and those showed the right data even...!

Reviewers: epriestley

Reviewed By: epriestley

Subscribers: Korvin, epriestley

Maniphest Tasks: T5387

Differential Revision: https://secure.phabricator.com/D10817

src/applications/owners/controller/PhabricatorOwnersDetailController.php
@@ -139,10 +139,10 @@ final class PhabricatorOwnersDetailController
 
     $commit_views = array();
 
-    $commit_uri = id(new PhutilURI('/audit/view/packagecommits/'))
+    $commit_uri = id(new PhutilURI('/audit/'))
       ->setQueryParams(
         array(
-          'phid'    => $package->getPHID(),
+          'auditorPHIDs' => $package->getPHID(),
         ));
 
     $attention_commits = id(new DiffusionCommitQuery())

src/applications/owners/controller/PhabricatorOwnersListController.php
@@ -308,7 +308,7 @@ final class PhabricatorOwnersListController
         phutil_tag(
           'a',
           array(
-            'href' => '/audit/view/packagecommits/?phid='.$package->getPHID(),
+            'href' => '/audit/?auditorPHIDs='.$package->getPHID(),
           ),
           pht('Related Commits')),
       );