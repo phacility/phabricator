34fb98da66eac8fbbe28aef2fb14a03a06896b07
Chad Little <chad@chadsdomain.com>
2014-12-08 13:53:29 -0800

    Update message and notification icons to use fonts
    Summary: Cleans up spacing, updates to fonts instead of images. Fixed some mobile issues.
    
    Test Plan:
    Test with and without counts on desktop, tablet, mobile. Test layout in FF, Chrome, IE.
    
    {F246745}
    
    {F246746}
    
    Reviewers: btrahan, epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin, epriestley
    
    Differential Revision: https://secure.phabricator.com/D10948

--- src/view/page/menu/PhabricatorMainMenuView.php
+++ src/view/page/menu/PhabricatorMainMenuView.php
@@ -270,13 +270,8 @@ final class PhabricatorMainMenuView extends AphrontView {
 
     require_celerity_resource('phabricator-notification-css');
     require_celerity_resource('phabricator-notification-menu-css');
-    require_celerity_resource('sprite-menu-css');
 
-    $container_classes = array(
-      'sprite-menu',
-      'alert-notifications',
-    );
-
+    $container_classes = array('alert-notifications');
     $aural = array();
 
     $message_tag = '';
@@ -324,7 +319,8 @@ final class PhabricatorMainMenuView extends AphrontView {
       $message_icon_tag = phutil_tag(
         'span',
         array(
-          'class' => 'sprite-menu phabricator-main-menu-message-icon',
+          'class' => 'phabricator-main-menu-message-icon phui-icon-view '.
+                     'phui-font-fa fa-comments',
         ),
         '');
 
@@ -408,7 +404,8 @@ final class PhabricatorMainMenuView extends AphrontView {
       $icon_tag = phutil_tag(
         'span',
         array(
-          'class' => 'sprite-menu phabricator-main-menu-alert-icon',
+          'class' => 'phabricator-main-menu-alert-icon phui-icon-view '.
+                     'phui-font-fa fa-bell',
         ),
         '');
 
