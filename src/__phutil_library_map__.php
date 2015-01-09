phacility/phabricator

commit 97cd8c1c75b5326256a50eefda3eb8b22c8045be

Rename `DiffusionSSHWorkflow` subclasses for consistency
Summary: Ref T5655.

Test Plan: `grep`

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: Korvin, epriestley

Maniphest Tasks: T5655

Differential Revision: https://secure.phabricator.com/D11185

src/__phutil_library_map__.php
@@ -500,8 +500,11 @@ phutil_register_library_map(array(
     'DiffusionGitFileContentQuery' => 'applications/diffusion/query/filecontent/DiffusionGitFileContentQuery.php',
     'DiffusionGitFileContentQueryTestCase' => 'applications/diffusion/query/__tests__/DiffusionGitFileContentQueryTestCase.php',
     'DiffusionGitRawDiffQuery' => 'applications/diffusion/query/rawdiff/DiffusionGitRawDiffQuery.php',
+    'DiffusionGitReceivePackSSHWorkflow' => 'applications/diffusion/ssh/DiffusionGitReceivePackSSHWorkflow.php',
     'DiffusionGitRequest' => 'applications/diffusion/request/DiffusionGitRequest.php',
     'DiffusionGitResponse' => 'applications/diffusion/response/DiffusionGitResponse.php',
+    'DiffusionGitSSHWorkflow' => 'applications/diffusion/ssh/DiffusionGitSSHWorkflow.php',
+    'DiffusionGitUploadPackSSHWorkflow' => 'applications/diffusion/ssh/DiffusionGitUploadPackSSHWorkflow.php',
     'DiffusionHistoryController' => 'applications/diffusion/controller/DiffusionHistoryController.php',
     'DiffusionHistoryQueryConduitAPIMethod' => 'applications/diffusion/conduit/DiffusionHistoryQueryConduitAPIMethod.php',
     'DiffusionHistoryTableView' => 'applications/diffusion/view/DiffusionHistoryTableView.php',
@@ -527,7 +530,11 @@ phutil_register_library_map(array(
     'DiffusionMercurialRawDiffQuery' => 'applications/diffusion/query/rawdiff/DiffusionMercurialRawDiffQuery.php',
     'DiffusionMercurialRequest' => 'applications/diffusion/request/DiffusionMercurialRequest.php',
     'DiffusionMercurialResponse' => 'applications/diffusion/response/DiffusionMercurialResponse.php',
+    'DiffusionMercurialSSHWorkflow' => 'applications/diffusion/ssh/DiffusionMercurialSSHWorkflow.php',
+    'DiffusionMercurialServeSSHWorkflow' => 'applications/diffusion/ssh/DiffusionMercurialServeSSHWorkflow.php',
+    'DiffusionMercurialWireClientSSHProtocolChannel' => 'applications/diffusion/ssh/DiffusionMercurialWireClientProtocoSSHChannel.php',
     'DiffusionMercurialWireProtocol' => 'applications/diffusion/protocol/DiffusionMercurialWireProtocol.php',
+    'DiffusionMercurialWireSSHTestCase' => 'applications/diffusion/ssh/__tests__/DiffusionMercurialWireSSHTestCase.php',
     'DiffusionMergedCommitsQueryConduitAPIMethod' => 'applications/diffusion/conduit/DiffusionMergedCommitsQueryConduitAPIMethod.php',
     'DiffusionMirrorDeleteController' => 'applications/diffusion/controller/DiffusionMirrorDeleteController.php',
     'DiffusionMirrorEditController' => 'applications/diffusion/controller/DiffusionMirrorEditController.php',
@@ -581,20 +588,13 @@ phutil_register_library_map(array(
     'DiffusionRequest' => 'applications/diffusion/request/DiffusionRequest.php',
     'DiffusionResolveRefsConduitAPIMethod' => 'applications/diffusion/conduit/DiffusionResolveRefsConduitAPIMethod.php',
     'DiffusionResolveUserQuery' => 'applications/diffusion/query/DiffusionResolveUserQuery.php',
-    'DiffusionSSHGitReceivePackWorkflow' => 'applications/diffusion/ssh/DiffusionSSHGitReceivePackWorkflow.php',
-    'DiffusionSSHGitUploadPackWorkflow' => 'applications/diffusion/ssh/DiffusionSSHGitUploadPackWorkflow.php',
-    'DiffusionSSHGitWorkflow' => 'applications/diffusion/ssh/DiffusionSSHGitWorkflow.php',
-    'DiffusionSSHMercurialServeWorkflow' => 'applications/diffusion/ssh/DiffusionSSHMercurialServeWorkflow.php',
-    'DiffusionSSHMercurialWireClientProtocolChannel' => 'applications/diffusion/ssh/DiffusionSSHMercurialWireClientProtocolChannel.php',
-    'DiffusionSSHMercurialWireTestCase' => 'applications/diffusion/ssh/__tests__/DiffusionSSHMercurialWireTestCase.php',
-    'DiffusionSSHMercurialWorkflow' => 'applications/diffusion/ssh/DiffusionSSHMercurialWorkflow.php',
-    'DiffusionSSHSubversionServeWorkflow' => 'applications/diffusion/ssh/DiffusionSSHSubversionServeWorkflow.php',
-    'DiffusionSSHSubversionWorkflow' => 'applications/diffusion/ssh/DiffusionSSHSubversionWorkflow.php',
     'DiffusionSSHWorkflow' => 'applications/diffusion/ssh/DiffusionSSHWorkflow.php',
     'DiffusionSearchQueryConduitAPIMethod' => 'applications/diffusion/conduit/DiffusionSearchQueryConduitAPIMethod.php',
     'DiffusionServeController' => 'applications/diffusion/controller/DiffusionServeController.php',
     'DiffusionSetPasswordSettingsPanel' => 'applications/diffusion/panel/DiffusionSetPasswordSettingsPanel.php',
     'DiffusionSetupException' => 'applications/diffusion/exception/DiffusionSetupException.php',
+    'DiffusionSubversionSSHWorkflow' => 'applications/diffusion/ssh/DiffusionSubversionSSHWorkflow.php',
+    'DiffusionSubversionServeSSHWorkflow' => 'applications/diffusion/ssh/DiffusionSubversionServeSSHWorkflow.php',
     'DiffusionSubversionWireProtocol' => 'applications/diffusion/protocol/DiffusionSubversionWireProtocol.php',
     'DiffusionSubversionWireProtocolTestCase' => 'applications/diffusion/protocol/__tests__/DiffusionSubversionWireProtocolTestCase.php',
     'DiffusionSvnFileContentQuery' => 'applications/diffusion/query/filecontent/DiffusionSvnFileContentQuery.php',
@@ -3561,8 +3561,11 @@ phutil_register_library_map(array(
     'DiffusionGitFileContentQuery' => 'DiffusionFileContentQuery',
     'DiffusionGitFileContentQueryTestCase' => 'PhabricatorTestCase',
     'DiffusionGitRawDiffQuery' => 'DiffusionRawDiffQuery',
+    'DiffusionGitReceivePackSSHWorkflow' => 'DiffusionGitSSHWorkflow',
     'DiffusionGitRequest' => 'DiffusionRequest',
     'DiffusionGitResponse' => 'AphrontResponse',
+    'DiffusionGitSSHWorkflow' => 'DiffusionSSHWorkflow',
+    'DiffusionGitUploadPackSSHWorkflow' => 'DiffusionGitSSHWorkflow',
     'DiffusionHistoryController' => 'DiffusionController',
     'DiffusionHistoryQueryConduitAPIMethod' => 'DiffusionQueryConduitAPIMethod',
     'DiffusionHistoryTableView' => 'DiffusionView',
@@ -3587,6 +3590,10 @@ phutil_register_library_map(array(
     'DiffusionMercurialRawDiffQuery' => 'DiffusionRawDiffQuery',
     'DiffusionMercurialRequest' => 'DiffusionRequest',
     'DiffusionMercurialResponse' => 'AphrontResponse',
+    'DiffusionMercurialSSHWorkflow' => 'DiffusionSSHWorkflow',
+    'DiffusionMercurialServeSSHWorkflow' => 'DiffusionMercurialSSHWorkflow',
+    'DiffusionMercurialWireClientSSHProtocolChannel' => 'PhutilProtocolChannel',
+    'DiffusionMercurialWireSSHTestCase' => 'PhabricatorTestCase',
     'DiffusionMergedCommitsQueryConduitAPIMethod' => 'DiffusionQueryConduitAPIMethod',
     'DiffusionMirrorDeleteController' => 'DiffusionController',
     'DiffusionMirrorEditController' => 'DiffusionController',
@@ -3632,20 +3639,13 @@ phutil_register_library_map(array(
     'DiffusionRepositoryRemarkupRule' => 'PhabricatorObjectRemarkupRule',
     'DiffusionResolveRefsConduitAPIMethod' => 'DiffusionQueryConduitAPIMethod',
     'DiffusionResolveUserQuery' => 'Phobject',
-    'DiffusionSSHGitReceivePackWorkflow' => 'DiffusionSSHGitWorkflow',
-    'DiffusionSSHGitUploadPackWorkflow' => 'DiffusionSSHGitWorkflow',
-    'DiffusionSSHGitWorkflow' => 'DiffusionSSHWorkflow',
-    'DiffusionSSHMercurialServeWorkflow' => 'DiffusionSSHMercurialWorkflow',
-    'DiffusionSSHMercurialWireClientProtocolChannel' => 'PhutilProtocolChannel',
-    'DiffusionSSHMercurialWireTestCase' => 'PhabricatorTestCase',
-    'DiffusionSSHMercurialWorkflow' => 'DiffusionSSHWorkflow',
-    'DiffusionSSHSubversionServeWorkflow' => 'DiffusionSSHSubversionWorkflow',
-    'DiffusionSSHSubversionWorkflow' => 'DiffusionSSHWorkflow',
     'DiffusionSSHWorkflow' => 'PhabricatorSSHWorkflow',
     'DiffusionSearchQueryConduitAPIMethod' => 'DiffusionQueryConduitAPIMethod',
     'DiffusionServeController' => 'DiffusionController',
     'DiffusionSetPasswordSettingsPanel' => 'PhabricatorSettingsPanel',
     'DiffusionSetupException' => 'AphrontUsageException',
+    'DiffusionSubversionSSHWorkflow' => 'DiffusionSSHWorkflow',
+    'DiffusionSubversionServeSSHWorkflow' => 'DiffusionSubversionSSHWorkflow',
     'DiffusionSubversionWireProtocol' => 'Phobject',
     'DiffusionSubversionWireProtocolTestCase' => 'PhabricatorTestCase',
     'DiffusionSvnFileContentQuery' => 'DiffusionFileContentQuery',

src/applications/diffusion/ssh/DiffusionGitReceivePackSSHWorkflow.php
@@ -1,10 +1,9 @@
 <?php
 
-final class DiffusionSSHGitUploadPackWorkflow
-  extends DiffusionSSHGitWorkflow {
+final class DiffusionGitReceivePackSSHWorkflow extends DiffusionGitSSHWorkflow {
 
   public function didConstruct() {
-    $this->setName('git-upload-pack');
+    $this->setName('git-receive-pack');
     $this->setArguments(
       array(
         array(
@@ -19,7 +18,10 @@ final class DiffusionSSHGitUploadPackWorkflow
     $path = head($args->getArg('dir'));
     $repository = $this->loadRepository($path);
 
-    $command = csprintf('git-upload-pack -- %s', $repository->getLocalPath());
+    // This is a write, and must have write access.
+    $this->requireWriteAccess();
+
+    $command = csprintf('git-receive-pack %s', $repository->getLocalPath());
     $command = PhabricatorDaemon::sudoCommandAsDaemonUser($command);
 
     $future = id(new ExecFuture('%C', $command))
@@ -31,6 +33,9 @@ final class DiffusionSSHGitUploadPackWorkflow
       ->execute();
 
     if (!$err) {
+      $repository->writeStatusMessage(
+        PhabricatorRepositoryStatusMessage::TYPE_NEEDS_UPDATE,
+        PhabricatorRepositoryStatusMessage::CODE_OKAY);
       $this->waitForGitClient();
     }
 

src/applications/diffusion/ssh/DiffusionGitSSHWorkflow.php
@@ -1,6 +1,6 @@
 <?php
 
-abstract class DiffusionSSHGitWorkflow extends DiffusionSSHWorkflow {
+abstract class DiffusionGitSSHWorkflow extends DiffusionSSHWorkflow {
 
   protected function writeError($message) {
     // Git assumes we'll add our own newlines.

src/applications/diffusion/ssh/DiffusionGitUploadPackSSHWorkflow.php
@@ -1,10 +1,9 @@
 <?php
 
-final class DiffusionSSHGitReceivePackWorkflow
-  extends DiffusionSSHGitWorkflow {
+final class DiffusionGitUploadPackSSHWorkflow extends DiffusionGitSSHWorkflow {
 
   public function didConstruct() {
-    $this->setName('git-receive-pack');
+    $this->setName('git-upload-pack');
     $this->setArguments(
       array(
         array(
@@ -19,10 +18,7 @@ final class DiffusionSSHGitReceivePackWorkflow
     $path = head($args->getArg('dir'));
     $repository = $this->loadRepository($path);
 
-    // This is a write, and must have write access.
-    $this->requireWriteAccess();
-
-    $command = csprintf('git-receive-pack %s', $repository->getLocalPath());
+    $command = csprintf('git-upload-pack -- %s', $repository->getLocalPath());
     $command = PhabricatorDaemon::sudoCommandAsDaemonUser($command);
 
     $future = id(new ExecFuture('%C', $command))
@@ -34,9 +30,6 @@ final class DiffusionSSHGitReceivePackWorkflow
       ->execute();
 
     if (!$err) {
-      $repository->writeStatusMessage(
-        PhabricatorRepositoryStatusMessage::TYPE_NEEDS_UPDATE,
-        PhabricatorRepositoryStatusMessage::CODE_OKAY);
       $this->waitForGitClient();
     }
 

src/applications/diffusion/ssh/DiffusionMercurialSSHWorkflow.php
@@ -1,0 +1,3 @@
+<?php
+
+abstract class DiffusionMercurialSSHWorkflow extends DiffusionSSHWorkflow {}

src/applications/diffusion/ssh/DiffusionMercurialServeSSHWorkflow.php
@@ -1,7 +1,7 @@
 <?php
 
-final class DiffusionSSHMercurialServeWorkflow
-  extends DiffusionSSHMercurialWorkflow {
+final class DiffusionMercurialServeSSHWorkflow
+  extends DiffusionMercurialSSHWorkflow {
 
   protected $didSeeWrite;
 
@@ -46,7 +46,7 @@ final class DiffusionSSHMercurialServeWorkflow
       ->setEnv($this->getEnvironment());
 
     $io_channel = $this->getIOChannel();
-    $protocol_channel = new DiffusionSSHMercurialWireClientProtocolChannel(
+    $protocol_channel = new DiffusionMercurialWireClientSSHProtocolChannel(
       $io_channel);
 
     $err = id($this->newPassthruCommand())

src/applications/diffusion/ssh/DiffusionMercurialWireClientProtocoSSHChannel.php
@@ -1,6 +1,6 @@
 <?php
 
-final class DiffusionSSHMercurialWireClientProtocolChannel
+final class DiffusionMercurialWireClientSSHProtocolChannel
   extends PhutilProtocolChannel {
 
   private $buffer = '';

src/applications/diffusion/ssh/DiffusionSSHMercurialWorkflow.php
@@ -1,3 +1,0 @@
-<?php
-
-abstract class DiffusionSSHMercurialWorkflow extends DiffusionSSHWorkflow {}

commit 420f955c2ad87db4bd19e75e4e86ad99c2a4198f

Fix an incorrect file name
Summary: Self explanatory. This was broken in D11185.

Test Plan: `arc lint`

Reviewers: #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: Korvin, epriestley

Differential Revision: https://secure.phabricator.com/D11280

src/__phutil_library_map__.php
@@ -537,7 +537,7 @@ phutil_register_library_map(array(
     'DiffusionMercurialResponse' => 'applications/diffusion/response/DiffusionMercurialResponse.php',
     'DiffusionMercurialSSHWorkflow' => 'applications/diffusion/ssh/DiffusionMercurialSSHWorkflow.php',
     'DiffusionMercurialServeSSHWorkflow' => 'applications/diffusion/ssh/DiffusionMercurialServeSSHWorkflow.php',
-    'DiffusionMercurialWireClientSSHProtocolChannel' => 'applications/diffusion/ssh/DiffusionMercurialWireClientProtocoSSHChannel.php',
+    'DiffusionMercurialWireClientSSHProtocolChannel' => 'applications/diffusion/ssh/DiffusionMercurialWireClientProtocolSSHChannel.php',
     'DiffusionMercurialWireProtocol' => 'applications/diffusion/protocol/DiffusionMercurialWireProtocol.php',
     'DiffusionMercurialWireSSHTestCase' => 'applications/diffusion/ssh/__tests__/DiffusionMercurialWireSSHTestCase.php',
     'DiffusionMergedCommitsQueryConduitAPIMethod' => 'applications/diffusion/conduit/DiffusionMergedCommitsQueryConduitAPIMethod.php',