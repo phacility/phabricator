38d216f0f1f56781ed826de328e4d1adea2dd0c1
Bob Trahan <btrahan@phacility.com>
2015-01-13 16:15:52 -0800

    Policy - add destructible interface
    Summary: Fixes T6957. If / when a policy object is destroyed, access to an object that uses that policy object is denied.
    
    Test Plan: looked around in the code to fail confident enough to write the summary above
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T6957
    
    Differential Revision: https://secure.phabricator.com/D11380

--- src/applications/policy/storage/PhabricatorPolicy.php
+++ src/applications/policy/storage/PhabricatorPolicy.php
@@ -2,7 +2,9 @@
 
 final class PhabricatorPolicy
   extends PhabricatorPolicyDAO
-  implements PhabricatorPolicyInterface {
+  implements
+    PhabricatorPolicyInterface,
+    PhabricatorDestructibleInterface {
 
   const ACTION_ALLOW = 'allow';
   const ACTION_DENY = 'deny';
@@ -360,5 +362,16 @@ final class PhabricatorPolicy
   public function describeAutomaticCapability($capability) {
     return null;
   }
+
+
+/* -(  PhabricatorDestructibleInterface  )----------------------------------- */
+
+
+  public function destroyObjectPermanently(
+    PhabricatorDestructionEngine $engine) {
+
+    $this->delete();
+  }
+
 
 }
