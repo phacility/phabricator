phacility/phabricator

commit 9a7ad972cd2637e566ac7598357b88364d6f2b64

Refactoring of the Aphlict server
Summary: Tidy the Aphlict server by splitting the functionality into two main modules, `AphlictClientServer` and `AphlictAdminServer. There is still further tidying that could be done here, but I feel that this puts us in a much better place.

Test Plan: Sent notifications via `/notification/status/`.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: Korvin, epriestley

Differential Revision: https://secure.phabricator.com/D11383

support/aphlict/server/aphlict_server.js
@@ -44,8 +44,7 @@ var config = parse_command_line_arguments(process.argv);
 
 process.on('uncaughtException', function(err) {
   var context = null;
-  if ((err.code == 'EACCES') &&
-      (err.path == config.log)) {
+  if (err.code == 'EACCES' && err.path == config.log) {
     context = util.format(
       'Unable to open logfile ("%s"). Check that permissions are set ' +
       'correctly.',
@@ -65,9 +64,8 @@ process.on('uncaughtException', function(err) {
   process.exit(1);
 });
 
-var WebSocket;
 try {
-  WebSocket = require('ws');
+  require('ws');
 } catch (ex) {
   throw new Error(
     'You need to install the Node.js "ws" module for websocket support. ' +
@@ -88,7 +86,7 @@ if (ssl_config.enabled) {
 
 // Add the logfile so we'll fail if we can't write to it.
 if (config.log) {
-  debug.addLogfile(config.log);
+  debug.addLog(config.log);
 }
 
 // If we're just doing a configuration test, exit here before starting any
@@ -98,166 +96,30 @@ if (config.test) {
   process.exit(0);
 }
 
-var start_time = new Date().getTime();
-var messages_out = 0;
-var messages_in = 0;
+JX.require('lib/AphlictAdminServer', __dirname);
+JX.require('lib/AphlictClientServer', __dirname);
 
-var clients = new JX.AphlictListenerList();
-
-function https_discard_handler(req, res) {
-  res.writeHead(501);
-  res.end('HTTP/501 Use Websockets\n');
-}
-
-var ws;
+var server;
 if (ssl_config.enabled) {
-  var https_server = https.createServer({
+  server = https.createServer({
     key: ssl_config.key,
     cert: ssl_config.cert
-  }, https_discard_handler).listen(
-    config['client-port'],
-    config['client-host']);
-
-  ws = new WebSocket.Server({server: https_server});
+  }, function(req, res) {
+    res.writeHead(501);
+    res.end('HTTP/501 Use Websockets\n');
+  });
 } else {
-  ws = new WebSocket.Server({
-    port: config['client-port'],
-    host: config['client-host'],
-  });
+  server = http.createServer(function() {});
 }
 
-ws.on('connection', function(ws) {
-  var listener = clients.addListener(ws);
+var client_server = new JX.AphlictClientServer(server);
+var admin_server = new JX.AphlictAdminServer();
 
-  function log() {
-    debug.log(
-      util.format('<%s>', listener.getDescription()) +
-      ' ' +
-      util.format.apply(null, arguments));
-  }
+client_server.setLogger(debug);
+admin_server.setLogger(debug);
+admin_server.setClientServer(client_server);
 
-  log('Connected from %s.', ws._socket.remoteAddress);
-
-  ws.on('message', function(data) {
-    log('Received message: %s', data);
-
-    var message;
-    try {
-      message = JSON.parse(data);
-    } catch (err) {
-      log('Message is invalid: %s', err.message);
-      return;
-    }
-
-    switch (message.command) {
-      case 'subscribe':
-        log(
-          'Subscribed to: %s',
-          JSON.stringify(message.data));
-        listener.subscribe(message.data);
-        break;
-
-      case 'unsubscribe':
-        log(
-          'Unsubscribed from: %s',
-          JSON.stringify(message.data));
-        listener.unsubscribe(message.data);
-        break;
-
-      default:
-        log('Unrecognized command "%s".', message.command || '<undefined>');
-    }
-  });
-
-  ws.on('close', function() {
-    clients.removeListener(listener);
-    log('Disconnected.');
-  });
-
-  ws.on('error', function(err) {
-    log('Error: %s', err.message);
-  });
-});
-
-function transmit(msg) {
-  var listeners = clients.getListeners().filter(function(client) {
-    return client.isSubscribedToAny(msg.subscribers);
-  });
-
-  for (var i = 0; i < listeners.length; i++) {
-    var listener = listeners[i];
-
-    try {
-      listener.writeMessage(msg);
-
-      ++messages_out;
-      debug.log('<%s> Wrote Message', listener.getDescription());
-    } catch (error) {
-      clients.removeListener(listener);
-      debug.log('<%s> Write Error: %s', listener.getDescription(), error);
-    }
-  }
-}
-
-http.createServer(function(request, response) {
-  // Publishing a notification.
-  if (request.url == '/') {
-    if (request.method == 'POST') {
-      var body = '';
-
-      request.on('data', function(data) {
-        body += data;
-      });
-
-      request.on('end', function() {
-        try {
-          var msg = JSON.parse(body);
-
-          debug.log('Received notification: ' + JSON.stringify(msg));
-          ++messages_in;
-
-          try {
-            transmit(msg);
-            response.writeHead(200, {'Content-Type': 'text/plain'});
-          } catch (err) {
-            debug.log(
-              '<%s> Internal Server Error! %s',
-              request.socket.remoteAddress,
-              err);
-            response.writeHead(500, 'Internal Server Error');
-          }
-        } catch (err) {
-          debug.log(
-            '<%s> Bad Request! %s',
-            request.socket.remoteAddress,
-            err);
-          response.writeHead(400, 'Bad Request');
-        } finally {
-          response.end();
-        }
-      });
-    } else {
-      response.writeHead(405, 'Method Not Allowed');
-      response.end();
-    }
-  } else if (request.url == '/status/') {
-    var status = {
-      'uptime': (new Date().getTime() - start_time),
-      'clients.active': clients.getActiveListenerCount(),
-      'clients.total': clients.getTotalListenerCount(),
-      'messages.in': messages_in,
-      'messages.out': messages_out,
-      'log': config.log,
-      'version': 6
-    };
-
-    response.writeHead(200, {'Content-Type': 'application/json'});
-    response.write(JSON.stringify(status));
-    response.end();
-  } else {
-    response.writeHead(404, 'Not Found');
-    response.end();
-  }
-}).listen(config['admin-port'], config['admin-host']);
+client_server.listen(config['client-port'], config['client-host']);
+admin_server.listen(config['admin-port'], config['admin-host']);
 
 debug.log('Started Server (PID %d)', process.pid);

support/aphlict/server/lib/AphlictAdminServer.js
@@ -1,0 +1,135 @@
+var JX = require('javelin').JX;
+
+JX.require('AphlictListenerList', __dirname);
+
+var http = require('http');
+
+JX.install('AphlictAdminServer', {
+
+  construct: function() {
+    this.setLogger(new JX.AphlictLog());
+
+    this._startTime = new Date().getTime();
+    this._messagesIn = 0;
+    this._messagesOut = 0;
+
+    var handler = this._handler.bind(this);
+    this._server = http.createServer(handler);
+  },
+
+  members: {
+    _messagesIn: null,
+    _messagesOut: null,
+    _server: null,
+    _startTime: null,
+
+    getListeners: function() {
+      return this.getListenerList().getListeners();
+    },
+
+    getListenerList: function() {
+      return this.getClientServer().getListenerList();
+    },
+
+    listen: function() {
+      return this._server.listen.apply(this._server, arguments);
+    },
+
+    _handler: function(request, response) {
+      var self = this;
+
+      // Publishing a notification.
+      if (request.url == '/') {
+        if (request.method == 'POST') {
+          var body = '';
+
+          request.on('data', function(data) {
+            body += data;
+          });
+
+          request.on('end', function() {
+            try {
+              var msg = JSON.parse(body);
+
+              self.getLogger().log(
+                'Received notification: ' + JSON.stringify(msg));
+              ++this._messagesIn;
+
+              try {
+                self._transmit(msg);
+                response.writeHead(200, {'Content-Type': 'text/plain'});
+              } catch (err) {
+                self.getLogger().log(
+                  '<%s> Internal Server Error! %s',
+                  request.socket.remoteAddress,
+                  err);
+                response.writeHead(500, 'Internal Server Error');
+              }
+            } catch (err) {
+              self.getLogger().log(
+                '<%s> Bad Request! %s',
+                request.socket.remoteAddress,
+                err);
+              response.writeHead(400, 'Bad Request');
+            } finally {
+              response.end();
+            }
+          });
+        } else {
+          response.writeHead(405, 'Method Not Allowed');
+          response.end();
+        }
+      } else if (request.url == '/status/') {
+        var status = {
+          'uptime': (new Date().getTime() - this._startTime),
+          'clients.active': this.getListenerList().getActiveListenerCount(),
+          'clients.total': this.getListenerList().getTotalListenerCount(),
+          'messages.in': this._messagesIn,
+          'messages.out': this._messagesOut,
+          'version': 6
+        };
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.write(JSON.stringify(status));
+        response.end();
+      } else {
+        response.writeHead(404, 'Not Found');
+        response.end();
+      }
+    },
+
+    /**
+     * Transmits a message to all subscribed listeners.
+     */
+    _transmit: function(message) {
+      var listeners = this.getListeners().filter(function(client) {
+        return client.isSubscribedToAny(message.subscribers);
+      });
+
+      for (var i = 0; i < listeners.length; i++) {
+        var listener = listeners[i];
+
+        try {
+          listener.writeMessage(message);
+
+          ++this._messagesOut;
+          this.getLogger().log(
+            '<%s> Wrote Message',
+            listener.getDescription());
+        } catch (error) {
+          this.getListenerList().removeListener(listener);
+          this.getLogger().log(
+            '<%s> Write Error: %s',
+            listener.getDescription(),
+            error);
+        }
+      }
+    },
+  },
+
+  properties: {
+    clientServer: null,
+    logger: null,
+  }
+
+});

support/aphlict/server/lib/AphlictClientServer.js
@@ -1,0 +1,90 @@
+var JX = require('javelin').JX;
+
+JX.require('AphlictListenerList', __dirname);
+JX.require('AphlictLog', __dirname);
+
+var util = require('util');
+var WebSocket = require('ws');
+
+JX.install('AphlictClientServer', {
+
+  construct: function(server) {
+    this.setListenerList(new JX.AphlictListenerList());
+    this.setLogger(new JX.AphlictLog());
+    this._server = server;
+  },
+
+  members: {
+    _server: null,
+
+    listen: function() {
+      var self = this;
+      var server = this._server.listen.apply(this._server, arguments);
+      var wss = new WebSocket.Server({server: server});
+
+      wss.on('connection', function(ws) {
+        var listener = self.getListenerList().addListener(ws);
+
+        function log() {
+          self.getLogger().log(
+            util.format('<%s>', listener.getDescription()) +
+            ' ' +
+            util.format.apply(null, arguments));
+        }
+
+        log('Connected from %s.', ws._socket.remoteAddress);
+
+        ws.on('message', function(data) {
+          log('Received message: %s', data);
+
+          var message;
+          try {
+            message = JSON.parse(data);
+          } catch (err) {
+            log('Message is invalid: %s', err.message);
+            return;
+          }
+
+          switch (message.command) {
+            case 'subscribe':
+              log(
+                'Subscribed to: %s',
+                JSON.stringify(message.data));
+              listener.subscribe(message.data);
+              break;
+
+            case 'unsubscribe':
+              log(
+                'Unsubscribed from: %s',
+                JSON.stringify(message.data));
+              listener.unsubscribe(message.data);
+              break;
+
+            default:
+              log(
+                'Unrecognized command "%s".',
+                message.command || '<undefined>');
+          }
+        });
+
+        wss.on('close', function() {
+          self.getListenerList().removeListener(listener);
+          log('Disconnected.');
+        });
+
+        wss.on('error', function(err) {
+          log('Error: %s', err.message);
+        });
+
+      });
+
+    },
+
+  },
+
+  properties: {
+    listenerList: null,
+    logger: null,
+  }
+
+});

support/aphlict/server/lib/AphlictListener.js
@@ -50,8 +50,6 @@ JX.install('AphlictListener', {
 
     writeMessage: function(message) {
       this._socket.send(JSON.stringify(message));
-    }
-
-  }
-
+    },
+  },
 });

support/aphlict/server/lib/AphlictListenerList.js
@@ -39,7 +39,7 @@ JX.install('AphlictListenerList', {
     },
 
     getActiveListenerCount: function() {
-      return Object.keys(this._listeners).length;
+      return this._listeners.length;
     },
 
     getTotalListenerCount: function() {
@@ -48,12 +48,10 @@ JX.install('AphlictListenerList', {
 
     _generateNextID: function() {
       do {
-        this._nextID = ((this._nextID + 1) % 1000000000000);
+        this._nextID = (this._nextID + 1) % 1000000000000;
       } while (this._nextID in this._listeners);
 
       return this._nextID;
-    }
-
-  }
-
+    },
+  },
 });

support/aphlict/server/lib/AphlictLog.js
@@ -5,30 +5,25 @@ var util = require('util');
 
 JX.install('AphlictLog', {
   construct: function() {
-    this._writeToLogs = [];
-    this._writeToConsoles = [];
+    this._consoles = [];
+    this._logs = [];
   },
 
   members: {
-    _writeToConsoles: null,
-    _writeToLogs: null,
+    _consoles: null,
+    _logs: null,
 
-    addLogfile: function(path) {
-      var options = {
-        flags: 'a',
-        encoding: 'utf8',
-        mode: 0664,
-      };
-
-      var logfile = fs.createWriteStream(path, options);
-
-      this._writeToLogs.push(logfile);
-
+    addConsole: function(console) {
+      this._consoles.push(console);
       return this;
     },
 
-    addConsole: function(console) {
-      this._writeToConsoles.push(console);
+    addLog: function(path) {
+      this._logs.push(fs.createWriteStream(path, {
+        flags: 'a',
+        encoding: 'utf8',
+        mode: 0664,
+      }));
       return this;
     },
 
@@ -38,15 +33,13 @@ JX.install('AphlictLog', {
       str = '[' + date + '] ' + str;
 
       var ii;
-      for (ii = 0; ii < this._writeToConsoles.length; ii++) {
-        this._writeToConsoles[ii].log(str);
+      for (ii = 0; ii < this._consoles.length; ii++) {
+        this._consoles[ii].log(str);
       }
 
-      for (ii = 0; ii < this._writeToLogs.length; ii++) {
-        this._writeToLogs[ii].write(str + '\n');
+      for (ii = 0; ii < this._logs.length; ii++) {
+        this._logs[ii].write(str + '\n');
       }
-    }
-
-  }
-
+    },
+  },
 });

commit 4636833f3dd8706fd7b66b7689ee644a4dca1940

Fix module imports in Aphlict server
Summary:
This was broken in D11383. Basically, I had the `ws` module installed globally whilst testing, but the changes made do not work if the `ws` module is installed locally (i.e. in the `./support/aphlict/server/node_modules` directory). After poking around, it seems that this is due to the sandboxing that is done by `JX.require`.

A quick fix is to just //not// use `JX.require`, although you may have a better idea?

The error that is occurring is as follows:

```
<<< UNCAUGHT EXCEPTION! >>>

Error: Cannot find module 'ws'
    at Function.Module._resolveFilename (module.js:338:15)
    at Function.Module._load (module.js:280:25)
    at Module.require (module.js:364:17)
    at require (module.js:380:17)
    at extra.require (/usr/src/phabricator/webroot/rsrc/externals/javelin/core/init_node.js:48:16)
    at /usr/src/phabricator/support/aphlict/server/lib/AphlictClientServer.js:10:17
    at Script.(anonymous function) [as runInNewContext] (vm.js:41:22)
    at Object.JX.require (/usr/src/phabricator/webroot/rsrc/externals/javelin/core/init_node.js:58:6)
    at Object.<anonymous> (/usr/src/phabricator/support/aphlict/server/aphlict_server.js:102:4)
    at Module._compile (module.js:456:26)
>>> Server exited!
```

Test Plan: Now able to start the Aphlict server.

Reviewers: joshuaspence

Reviewed By: joshuaspence

Subscribers: Korvin, epriestley

Maniphest Tasks: T6987

Differential Revision: https://secure.phabricator.com/D11425

resources/celerity/map.php
@@ -167,7 +167,7 @@ return array(
     'rsrc/externals/javelin/core/__tests__/stratcom.js' => '88bf7313',
     'rsrc/externals/javelin/core/__tests__/util.js' => 'e251703d',
     'rsrc/externals/javelin/core/init.js' => '76e1fd61',
-    'rsrc/externals/javelin/core/init_node.js' => 'd7dde471',
+    'rsrc/externals/javelin/core/init_node.js' => '7212f324',
     'rsrc/externals/javelin/core/install.js' => '05270951',
     'rsrc/externals/javelin/core/util.js' => 'bdcfee9e',
     'rsrc/externals/javelin/docs/Base.js' => '74676256',

support/aphlict/server/aphlict_server.js
@@ -4,9 +4,6 @@ var https = require('https');
 var util = require('util');
 var fs = require('fs');
 
-JX.require('lib/AphlictListenerList', __dirname);
-JX.require('lib/AphlictLog', __dirname);
-
 function parse_command_line_arguments(argv) {
   var config = {
     'client-port': 22280,
@@ -36,6 +33,8 @@ function parse_command_line_arguments(argv) {
 
   return config;
 }
+
+require('./lib/AphlictLog');
 
 var debug = new JX.AphlictLog()
   .addConsole(console);
@@ -71,6 +70,12 @@ try {
     'documentation for instructions. ' + ex.toString());
 }
 
+// NOTE: Require these only after checking for the "ws" module, since they
+// depend on it.
+
+require('./lib/AphlictAdminServer');
+require('./lib/AphlictClientServer');
+
 var ssl_config = {
   enabled: (config['ssl-key'] || config['ssl-cert'])
 };
@@ -93,9 +98,6 @@ if (config.test) {
   debug.log('Configuration test OK.');
   process.exit(0);
 }
-
-JX.require('lib/AphlictAdminServer', __dirname);
-JX.require('lib/AphlictClientServer', __dirname);
 
 var server;
 if (ssl_config.enabled) {

support/aphlict/server/lib/AphlictAdminServer.js
@@ -1,6 +1,6 @@
-var JX = require('javelin').JX;
+var JX = require('./javelin').JX;
 
-JX.require('AphlictListenerList', __dirname);
+require('./AphlictListenerList');
 
 var http = require('http');
 

support/aphlict/server/lib/AphlictClientServer.js
@@ -1,7 +1,7 @@
-var JX = require('javelin').JX;
+var JX = require('./javelin').JX;
 
-JX.require('AphlictListenerList', __dirname);
-JX.require('AphlictLog', __dirname);
+require('./AphlictListenerList');
+require('./AphlictLog');
 
 var util = require('util');
 var WebSocket = require('ws');

support/aphlict/server/lib/AphlictListener.js
@@ -1,4 +1,4 @@
-var JX = require('javelin').JX;
+var JX = require('./javelin').JX;
 
 JX.install('AphlictListener', {
   construct: function(id, socket) {

support/aphlict/server/lib/AphlictListenerList.js
@@ -1,5 +1,6 @@
-var JX = require('javelin').JX;
-JX.require('AphlictListener', __dirname);
+var JX = require('./javelin').JX;
+
+require('./AphlictListener');
 
 JX.install('AphlictListenerList', {
   construct: function() {

support/aphlict/server/lib/AphlictLog.js
@@ -1,4 +1,4 @@
-var JX = require('javelin').JX;
+var JX = require('./javelin').JX;
 
 var fs = require('fs');
 var util = require('util');

webroot/rsrc/externals/javelin/core/init_node.js
@@ -23,9 +23,8 @@ JX.onload = function(func) {
   func();
 };
 
-JX.require = function(thing, relative) {
-  relative = relative || __dirname + '/..';
-  var path = relative + '/' + thing + '.js';
+JX.require = function(thing) {
+  var path = __dirname + '/../' + thing + '.js';
   var content = fs.readFileSync(path);
   var dir = pathModule.dirname(path);
 
@@ -37,17 +36,10 @@ JX.require = function(thing, relative) {
   }
 
   var extra = {
-    JX : this,
-    __DEV__ : 0,
-    window : {},
-    __dirname : dir,
-    require : function(thing) {
-      if (thing == 'javelin') {
-        return require(dir + '/' + thing);
-      } else {
-        return require(thing);
-      }
-    }
+    JX: this,
+    __DEV__: 0,
+    window: {},
+    __dirname: dir
   };
 
   for (k in extra) {