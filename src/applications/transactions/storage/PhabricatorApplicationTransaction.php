0969b0d8c87ddfec15f56691d966239cc7c14b20
Bob Trahan <btrahan@phacility.com>
2015-02-02 14:41:50 -0800

    Policy - add an explanation for automatic capabilities for transactions and transaction comments
    Summary: Ref T7094. I am not sure when this text is legitimately exposed to users - they should be getting an error about not being able to see the object before they get an error about not being able to see a given transaction... That said, I think this text is logically correct at least.
    
    Test Plan: read the text
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T7094
    
    Differential Revision: https://secure.phabricator.com/D11632

--- src/applications/transactions/storage/PhabricatorApplicationTransaction.php
+++ src/applications/transactions/storage/PhabricatorApplicationTransaction.php
@@ -1140,8 +1140,10 @@ abstract class PhabricatorApplicationTransaction
   }
 
   public function describeAutomaticCapability($capability) {
-    // TODO: (T603) Exact policies are unclear here.
-    return null;
+    return pht(
+      'Transactions are visible to users that can see the object which was '.
+      'acted upon. Some transactions - in particular, comments - are '.
+      'editable by the transaction author.');
   }
 
 
