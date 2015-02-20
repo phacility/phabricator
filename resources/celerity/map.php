5c1fa239131e34583265d9656397a127448d1c78
Chad Little <chad@chadsdomain.com>
2015-02-20 12:57:57 -0800

    Add 1px MORE movement and .1s MORE transition to header alerts
    Summary: A touch more movement / purpose
    
    Test Plan: Evan
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin, epriestley
    
    Differential Revision: https://secure.phabricator.com/D11839

--- resources/celerity/map.php
+++ resources/celerity/map.php
@@ -7,7 +7,7 @@
  */
 return array(
   'names' => array(
-    'core.pkg.css' => 'aba1dfdf',
+    'core.pkg.css' => '26a65a65',
     'core.pkg.js' => '23d653bb',
     'darkconsole.pkg.js' => '8ab24e01',
     'differential.pkg.css' => '380f07e5',
@@ -35,7 +35,7 @@ return array(
     'rsrc/css/aphront/typeahead.css' => '0e403212',
     'rsrc/css/application/almanac/almanac.css' => 'dbb9b3af',
     'rsrc/css/application/auth/auth.css' => '1e655982',
-    'rsrc/css/application/base/main-menu-view.css' => '8acd22bd',
+    'rsrc/css/application/base/main-menu-view.css' => 'f9f5cd1b',
     'rsrc/css/application/base/notification-menu.css' => '6aa0a74b',
     'rsrc/css/application/base/phabricator-application-launch-view.css' => '16ca323f',
     'rsrc/css/application/base/standard-page-view.css' => '60d0df9a',
@@ -729,7 +729,7 @@ return array(
     'phabricator-hovercard-view-css' => '893f4783',
     'phabricator-keyboard-shortcut' => '1ae869f2',
     'phabricator-keyboard-shortcut-manager' => 'c1700f6f',
-    'phabricator-main-menu-view' => '8acd22bd',
+    'phabricator-main-menu-view' => 'f9f5cd1b',
     'phabricator-nav-view-css' => '7aeaf435',
     'phabricator-notification' => '0c6946e7',
     'phabricator-notification-css' => '9c279160',
