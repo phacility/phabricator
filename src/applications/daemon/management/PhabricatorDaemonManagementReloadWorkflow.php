e27c1a4868e140980c4cb256c83934c46d2983d5
epriestley <git@epriestley.com>
2015-02-27 11:38:42 -0800

    Add `phd reload` to send SIGHUP to overseers
    Summary: Ref T7384. This just sends SIGHUP to specified overseers in a nice package.
    
    Test Plan: See D11898.
    
    Reviewers: hach-que, btrahan
    
    Reviewed By: btrahan
    
    Subscribers: joshuaspence, epriestley
    
    Maniphest Tasks: T7384
    
    Differential Revision: https://secure.phabricator.com/D11899

---
+++ src/applications/daemon/management/PhabricatorDaemonManagementReloadWorkflow.php
@@ -0,0 +1,27 @@
+<?php
+
+final class PhabricatorDaemonManagementReloadWorkflow
+  extends PhabricatorDaemonManagementWorkflow {
+
+  protected function didConstruct() {
+    $this
+      ->setName('reload')
+      ->setSynopsis(
+        pht(
+          'Gracefully restart daemon processes in-place to pick up changes '.
+          'to source. This will not disrupt running jobs. This is an '.
+          'advanced workflow; most installs should use __phd restart__.'))
+      ->setArguments(
+        array(
+          array(
+            'name' => 'pids',
+            'wildcard' => true,
+          ),
+        ));
+  }
+
+  public function execute(PhutilArgumentParser $args) {
+    return $this->executeReloadCommand($args->getArg('pids'));
+  }
+
+}
