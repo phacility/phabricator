6b65c578c7349cfc7a78f4b6fa1f46b9924ac4c3
epriestley <git@epriestley.com>
2015-03-03 10:39:43 -0800

    Forbid construction of dashboard panels on various Phortune objects
    Summary:
    Fixes T7102. These panels don't work, aren't meaningful, and don't seem very useful.
    
    We could eventually support providing context to dashboards somehow ("merchant dashboard") but don't have much of an apparent need for this.
    
    Test Plan:
      - Tried to create cart/subscription/charge dashboard panels.
      - Unable to create new ones.
      - The ones from before the change show a relevant error now.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: epriestley
    
    Maniphest Tasks: T7102
    
    Differential Revision: https://secure.phabricator.com/D11953

--- src/applications/phortune/query/PhortuneCartSearchEngine.php
+++ src/applications/phortune/query/PhortuneCartSearchEngine.php
@@ -7,6 +7,11 @@ final class PhortuneCartSearchEngine
   private $account;
   private $subscription;
 
+  public function canUseInPanelContext() {
+    // These only make sense in an account or merchant context.
+    return false;
+  }
+
   public function setAccount(PhortuneAccount $account) {
     $this->account = $account;
     return $this;
