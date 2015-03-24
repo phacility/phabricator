74a4c2cf0b1b93e042fe0ac80511aa3854a677ed
epriestley <git@epriestley.com>
2015-03-24 13:11:37 -0700

    Provide better parsing primitives for hunks
    Summary:
    Ref T1266. This prepares to fix case (2) on T1266 by improving the robustness of hunk parsing.
    
    In particular, the copy detection code abuses this API because it isn't currently expressive or flexible enough.
    
    Make it more flexible and cover it exhaustively.
    
    I'll move callsites to the new stuff in upcoming revisions.
    
    Test Plan: Unit tests.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: epriestley
    
    Maniphest Tasks: T1266
    
    Differential Revision: https://secure.phabricator.com/D12144

--- src/applications/differential/storage/DifferentialHunk.php
+++ src/applications/differential/storage/DifferentialHunk.php
@@ -10,6 +10,8 @@ abstract class DifferentialHunk extends DifferentialDAO
   protected $newLen;
 
   private $changeset;
+  private $structuredLines;
+  private $structuredFiles = array();
 
   const FLAG_LINES_ADDED     = 1;
   const FLAG_LINES_REMOVED   = 2;
@@ -35,6 +37,102 @@ abstract class DifferentialHunk extends DifferentialDAO
     return implode('', $this->makeContent($include = '-+'));
   }
 
+  public function getStructuredOldFile() {
+    return $this->getStructuredFile('-');
+  }
+
+  public function getStructuredNewFile() {
+    return $this->getStructuredFile('+');
+  }
+
+  private function getStructuredFile($kind) {
+    if ($kind !== '+' && $kind !== '-') {
+      throw new Exception(
+        pht(
+          'Structured file kind should be "+" or "-", got "%s".',
+          $kind));
+    }
+
+    if (!isset($this->structuredFiles[$kind])) {
+      if ($kind == '+') {
+        $number = $this->newOffset;
+      } else {
+        $number = $this->oldOffset;
+      }
+
+      $lines = $this->getStructuredLines();
+
+      // NOTE: We keep the "\ No newline at end of file" line if it appears
+      // after a line which is not excluded. For example, if we're constructing
+      // the "+" side of the diff, we want to ignore this one since it's
+      // relevant only to the "-" side of the diff:
+      //
+      //    - x
+      //    \ No newline at end of file
+      //    + x
+      //
+      // ...but we want to keep this one:
+      //
+      //    - x
+      //    + x
+      //    \ No newline at end of file
+
+      $file = array();
+      $keep = true;
+      foreach ($lines as $line) {
+        switch ($line['type']) {
+          case ' ':
+          case $kind:
+            $file[$number++] = $line;
+            $keep = true;
+            break;
+          case '\\':
+            if ($keep) {
+              // Strip the actual newline off the line's text.
+              $text = $file[$number - 1]['text'];
+              $text = rtrim($text, "\r\n");
+              $file[$number - 1]['text'] = $text;
+
+              $file[$number++] = $line;
+              $keep = false;
+            }
+            break;
+          default:
+            $keep = false;
+            break;
+        }
+      }
+
+      $this->structuredFiles[$kind] = $file;
+    }
+
+    return $this->structuredFiles[$kind];
+  }
+
+  private function getStructuredLines() {
+    if ($this->structuredLines === null) {
+      $lines = phutil_split_lines($this->getChanges());
+
+      $structured = array();
+      foreach ($lines as $line) {
+        if (empty($line[0])) {
+          // TODO: Can we just get rid of this?
+          continue;
+        }
+
+        $structured[] = array(
+          'type' => $line[0],
+          'text' => substr($line, 1),
+        );
+      }
+
+      $this->structuredLines = $structured;
+    }
+
+    return $this->structuredLines;
+  }
+
+
   public function getContentWithMask($mask) {
     $include = array();
 
@@ -59,21 +157,6 @@ abstract class DifferentialHunk extends DifferentialDAO
     $results = array();
     $lines = explode("\n", $this->getChanges());
 
-    // NOTE: To determine whether the recomposed file should have a trailing
-    // newline, we look for a "\ No newline at end of file" line which appears
-    // after a line which we don't exclude. For example, if we're constructing
-    // the "new" side of a diff (excluding "-"), we want to ignore this one:
-    //
-    //    - x
-    //    \ No newline at end of file
-    //    + x
-    //
-    // ...since it's talking about the "old" side of the diff, but interpret
-    // this as meaning we should omit the newline:
-    //
-    //    - x
-    //    + x
-    //    \ No newline at end of file
 
     $n = (strpos($include, '+') !== false ?
       $this->newOffset :
