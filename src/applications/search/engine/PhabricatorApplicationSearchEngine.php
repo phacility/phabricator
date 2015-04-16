ba48e05964d4b1aa6ec341c842c477b0fbdd7f11
epriestley <git@epriestley.com>
2015-04-16 12:11:17 -0700

    Uncheat Macro datasource browse support
    Summary: Ref T5750. Macro browse support is actually useful (in the meme dialog) and fairly straightforward to implement.
    
    Test Plan:
      - Browsed macros.
      - Queired macros.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: epriestley
    
    Maniphest Tasks: T5750
    
    Differential Revision: https://secure.phabricator.com/D12439

--- src/applications/search/engine/PhabricatorApplicationSearchEngine.php
+++ src/applications/search/engine/PhabricatorApplicationSearchEngine.php
@@ -602,18 +602,16 @@ abstract class PhabricatorApplicationSearchEngine {
     PhabricatorSavedQuery $saved) {
 
     $order = $saved->getParameter('order');
-    if (strlen($order)) {
-      $builtin = $query->getBuiltinOrders();
-      if (isset($builtin[$order])) {
-        $query->setOrder($order);
-      } else {
-        // If the order is invalid or not available, we choose the first
-        // builtin order. This isn't always the default order for the query,
-        // but is the first value in the "Order" dropdown, and makes the query
-        // behavior more consistent with the UI. In queries where the two
-        // orders differ, this order is the preferred order for humans.
-        $query->setOrder(head_key($builtin));
-      }
+    $builtin = $query->getBuiltinOrders();
+    if (strlen($order) && isset($builtin[$order])) {
+      $query->setOrder($order);
+    } else {
+      // If the order is invalid or not available, we choose the first
+      // builtin order. This isn't always the default order for the query,
+      // but is the first value in the "Order" dropdown, and makes the query
+      // behavior more consistent with the UI. In queries where the two
+      // orders differ, this order is the preferred order for humans.
+      $query->setOrder(head_key($builtin));
     }
 
     return $this;
