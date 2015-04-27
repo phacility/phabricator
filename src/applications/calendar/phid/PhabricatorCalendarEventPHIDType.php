3b226e8fb330d4175d9349ed41addbc381d91a01
lkassianik <lyubakassianik@gmail.com>
2015-04-27 15:31:18 -0700

    Calendar event monograms, part 4. Typing E{id} makes the object available in global search.
    Summary: Closes T7928, E{id} is available via global search.
    
    Test Plan: Create calendar event, search for its monogram in global search, event should be accessible.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T7928
    
    Differential Revision: https://secure.phabricator.com/D12581

--- src/applications/calendar/phid/PhabricatorCalendarEventPHIDType.php
+++ src/applications/calendar/phid/PhabricatorCalendarEventPHIDType.php
@@ -38,4 +38,32 @@ final class PhabricatorCalendarEventPHIDType extends PhabricatorPHIDType {
     }
   }
 
+  public function canLoadNamedObject($name) {
+    return preg_match('/^E[1-9]\d*$/i', $name);
+  }
+
+  public function loadNamedObjects(
+    PhabricatorObjectQuery $query,
+    array $names) {
+
+    $id_map = array();
+    foreach ($names as $name) {
+      $id = (int)substr($name, 1);
+      $id_map[$id][] = $name;
+    }
+
+    $objects = id(new PhabricatorCalendarEventQuery())
+      ->setViewer($query->getViewer())
+      ->withIDs(array_keys($id_map))
+      ->execute();
+
+    $results = array();
+    foreach ($objects as $id => $object) {
+      foreach (idx($id_map, $id, array()) as $name) {
+        $results[$name] = $object;
+      }
+    }
+
+    return $results;
+  }
 }
