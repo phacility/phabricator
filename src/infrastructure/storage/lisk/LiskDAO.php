phacility/phabricator

commit 70c864914210e160c9fd445e3a86a5d0012c53ae

Use phutil_json_decode instead of json_decode
Summary: Generally, `phutil_json_decode` should be preferred over `json_decode`.

Test Plan: Eyellballed.

Reviewers: #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: Korvin, epriestley

Differential Revision: https://secure.phabricator.com/D12680

src/infrastructure/storage/lisk/LiskDAO.php
@@ -1637,7 +1637,7 @@ abstract class LiskDAO {
             break;
           case self::SERIALIZATION_JSON:
             if ($deserialize) {
-              $data[$col] = json_decode($data[$col], true);
+              $data[$col] = phutil_json_decode($data[$col]);
             } else {
               $data[$col] = json_encode($data[$col]);
             }

scripts/celerity/generate_sprites.php
@@ -53,7 +53,7 @@ foreach ($sheets as $name => $sheet) {
   if (!$args->getArg('force')) {
     if (Filesystem::pathExists($manifest_path)) {
       $data = Filesystem::readFile($manifest_path);
-      $data = json_decode($data, true);
+      $data = phutil_json_decode($data);
       if (!$sheet->needsRegeneration($data)) {
         continue;
       }

src/applications/conduit/controller/PhabricatorConduitAPIController.php
@@ -647,12 +647,15 @@ final class PhabricatorConduitAPIController
     // entire param dictionary JSON encoded.
     $params_json = $request->getStr('params');
     if (strlen($params_json)) {
-      $params = json_decode($params_json, true);
-      if (!is_array($params)) {
-        throw new Exception(
-          "Invalid parameter information was passed to method ".
-          "'{$method}', could not decode JSON serialization. Data: ".
-          $params_json);
+      $params = null;
+      try {
+        $params = phutil_json_decode($params_json);
+      } catch (PhutilJSONParserException $ex) {
+        throw new PhutilProxyException(
+          pht(
+            "Invalid parameter information was passed to method '%s'",
+            $method),
+          $ex);
       }
 
       $metadata = idx($params, '__conduit__', array());

src/applications/conduit/ssh/ConduitSSHWorkflow.php
@@ -26,13 +26,17 @@ final class ConduitSSHWorkflow extends PhabricatorSSHWorkflow {
     $method = head($methodv);
 
     $json = $this->readAllInput();
-    $raw_params = json_decode($json, true);
-    if (!is_array($raw_params)) {
-      throw new Exception('Invalid JSON input.');
+    $raw_params = null;
+    try {
+      $raw_params = phutil_json_decode($json);
+    } catch (PhutilJSONParserException $ex) {
+      throw new PhutilProxyException(
+        pht('Invalid JSON input.'),
+        $ex);
     }
 
     $params = idx($raw_params, 'params', '[]');
-    $params = json_decode($params, true);
+    $params = phutil_json_decode($params);
     $metadata = idx($params, '__conduit__', array());
     unset($params['__conduit__']);
 

src/applications/console/controller/DarkConsoleDataController.php
@@ -33,9 +33,9 @@ final class DarkConsoleDataController extends PhabricatorController {
       return new Aphront400Response();
     }
 
-    $result = json_decode($result, true);
-
-    if (!is_array($result)) {
+    try {
+      $result = phutil_json_decode($result);
+    } catch (PhutilJSONParserException $ex) {
       return new Aphront400Response();
     }
 

src/applications/differential/__tests__/DifferentialParseRenderTestCase.php
@@ -17,9 +17,12 @@ final class DifferentialParseRenderTestCase extends PhabricatorTestCase {
       $opt_file = $dir.$file.'.options';
       if (Filesystem::pathExists($opt_file)) {
         $options = Filesystem::readFile($opt_file);
-        $options = json_decode($options, true);
-        if (!is_array($options)) {
-          throw new Exception("Invalid options file: {$opt_file}.");
+        try {
+          $options = phutil_json_decode($options);
+        } catch (PhutilJSONParserException $ex) {
+          throw new PhutilProxyException(
+            pht('Invalid options file: %s.', $opt_file),
+            $ex);
         }
       } else {
         $options = array();

src/applications/differential/conduit/DifferentialSetDiffPropertyConduitAPIMethod.php
@@ -86,7 +86,7 @@ final class DifferentialSetDiffPropertyConduitAPIMethod
   protected function execute(ConduitAPIRequest $request) {
     $diff_id = $request->getValue('diff_id');
     $name = $request->getValue('name');
-    $data = json_decode($request->getValue('data'), true);
+    $data = phutil_json_decode($request->getValue('data'));
 
     self::updateDiffProperty($diff_id, $name, $data);
 

src/applications/diffusion/DiffusionLintSaveRunner.php
@@ -154,9 +154,11 @@ final class DiffusionLintSaveRunner {
         $files);
 
       foreach (new LinesOfALargeExecFuture($future) as $json) {
-        $paths = json_decode($json, true);
-        if (!is_array($paths)) {
-          fprintf(STDERR, "Invalid JSON: {$json}\n");
+        $paths = null;
+        try {
+          $paths = phutil_json_decode($json);
+        } catch (PhutilJSONParserException $ex) {
+          fprintf(STDERR, pht("Invalid JSON: %s\n", $json));
           continue;
         }
 

commit 8d128646b31acf3694de86fed26b8a0ceb9ac6dc

Fix a lisk issue
Summary:
This was broken in D12680.

```
EXCEPTION: (PhutilJSONParserException) Parse error on line 0 at column 0: 'null' is not a valid JSON object. at [<phutil>/src/parser/PhutilJSONParser.php:41]
  #0 PhutilJSONParser::parse(string) called at [<phutil>/src/utils/utils.php:1062]
  #1 phutil_json_decode(string) called at [<phabricator>/src/infrastructure/storage/lisk/LiskDAO.php:1640]
  #2 LiskDAO::applyLiskDataSerialization(array, boolean) called at [<phabricator>/src/infrastructure/storage/lisk/LiskDAO.php:1386]
  #3 LiskDAO::willReadData(array) called at [<phabricator>/src/infrastructure/storage/lisk/PhabricatorLiskDAO.php:214]
  #4 PhabricatorLiskDAO::willReadData(array) called at [<phabricator>/src/infrastructure/storage/lisk/LiskDAO.php:608]
  #5 LiskDAO::loadFromArray(array) called at [<phabricator>/src/infrastructure/storage/lisk/LiskDAO.php:652]
  #6 LiskDAO::loadAllFromArray(array) called at [<phabricator>/src/applications/transactions/query/PhabricatorApplicationTransactionQuery.php:62]
  #7 PhabricatorApplicationTransactionQuery::loadPage() called at [<phabricator>/src/infrastructure/query/policy/PhabricatorPolicyAwareQuery.php:227]
  #8 PhabricatorPolicyAwareQuery::execute() called at [<phabricator>/src/infrastructure/query/policy/PhabricatorCursorPagedPolicyAwareQuery.php:143]
  #9 PhabricatorCursorPagedPolicyAwareQuery::executeWithCursorPager(AphrontCursorPagerView) called at [<phabricator>/src/applications/base/controller/PhabricatorController.php:577]
  #10 PhabricatorController::buildTransactionTimeline(PhabricatorPaste, PhabricatorPasteTransactionQuery) called at [<phabricator>/src/applications/paste/controller/PhabricatorPasteViewControll
```

Test Plan: No exception shown.

Reviewers: epriestley, #blessed_reviewers

Reviewed By: epriestley, #blessed_reviewers

Subscribers: Korvin, epriestley

Differential Revision: https://secure.phabricator.com/D12714

src/infrastructure/storage/lisk/LiskDAO.php
@@ -1637,7 +1637,7 @@ abstract class LiskDAO {
             break;
           case self::SERIALIZATION_JSON:
             if ($deserialize) {
-              $data[$col] = phutil_json_decode($data[$col]);
+              $data[$col] = json_decode($data[$col], true);
             } else {
               $data[$col] = json_encode($data[$col]);
             }