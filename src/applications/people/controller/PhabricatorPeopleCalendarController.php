97611958b0ead8280514d0678634ab9763fc2347
lkassianik <lyubakassianik@gmail.com>
2015-05-13 15:58:46 -0700

    Refactor `PHUICalendarMonthView` to be a little more readable
    Summary: Refactor `PHUICalendarMonthView` to be a little more readable
    
    Test Plan: Make sure month view still works
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin, epriestley
    
    Differential Revision: https://secure.phabricator.com/D12831

--- src/applications/people/controller/PhabricatorPeopleCalendarController.php
+++ src/applications/people/controller/PhabricatorPeopleCalendarController.php
@@ -35,12 +35,6 @@ final class PhabricatorPeopleCalendarController
     $month   = $request->getInt('month', $month_d);
     $day   = phabricator_format_local_time($now, $user, 'j');
 
-
-    $holidays = id(new PhabricatorCalendarHoliday())->loadAllWhere(
-      'day BETWEEN %s AND %s',
-      "{$year}-{$month}-01",
-      "{$year}-{$month}-31");
-
     $start_epoch = strtotime("{$year}-{$month}-01");
     $end_epoch = strtotime("{$year}-{$month}-01 next month");
 
@@ -76,7 +70,6 @@ final class PhabricatorPeopleCalendarController
 
     $month_view->setBrowseURI($request->getRequestURI());
     $month_view->setUser($user);
-    $month_view->setHolidays($holidays);
     $month_view->setImage($picture);
 
     $phids = mpull($statuses, 'getUserPHID');
