phacility/phabricator

commit e6174ed45cbd69ce2c702ca07e9edf3161082ab7

Fix an issue where pastes could be reordered as a side effect of cache fills
Summary: Ref T7803. Pastes which needed a cache fill would incorrectly be dropped to the bottom of the list. Stop doing that.

Test Plan: Loaded a list of pastes with some that needed cache fills, saw them appear in the correct order.

Reviewers: btrahan

Reviewed By: btrahan

Subscribers: epriestley

Maniphest Tasks: T7803

Differential Revision: https://secure.phabricator.com/D12354

src/applications/paste/query/PhabricatorPasteQuery.php
@@ -90,7 +90,7 @@ final class PhabricatorPasteQuery
     }
 
     if ($this->needContent) {
-      $pastes = $this->loadContent($pastes);
+      $this->loadContent($pastes);
     }
 
     return $pastes;
@@ -203,21 +203,19 @@ final class PhabricatorPasteQuery
     }
 
     $caches = $cache->getKeys($keys);
-    $results = array();
 
     $need_raw = array();
     foreach ($pastes as $key => $paste) {
       $key = $this->getContentCacheKey($paste);
       if (isset($caches[$key])) {
         $paste->attachContent(phutil_safe_html($caches[$key]));
-        $results[$paste->getID()] = $paste;
       } else {
         $need_raw[$key] = $paste;
       }
     }
 
     if (!$need_raw) {
-      return $results;
+      return;
     }
 
     $write_data = array();
@@ -226,14 +224,10 @@ final class PhabricatorPasteQuery
     foreach ($need_raw as $key => $paste) {
       $content = $this->buildContent($paste);
       $paste->attachContent($content);
-
       $write_data[$this->getContentCacheKey($paste)] = (string)$content;
-      $results[$paste->getID()] = $paste;
     }
 
     $cache->setKeys($write_data);
-
-    return $results;
   }
 
   private function buildContent(PhabricatorPaste $paste) {

commit b0862a81760d60d8de61545d65a4ce9efab2b90e

Filter pastes at the query level if we're unable to load their content
Summary:
Fixes T8230. I think I broke this in D12354 since I missed the fact that this could filter results, because it's constructed in a sort of weird way. Try to make the logic a little more clear, while retaining the relevant properties:

  - Pastes with unloadable content should be discarded;
  - the input order should be retained in the result set.

Test Plan: Faked content misses and saw things work instead of fatal.

Reviewers: btrahan

Reviewed By: btrahan

Subscribers: epriestley

Maniphest Tasks: T8230

Differential Revision: https://secure.phabricator.com/D12909

src/applications/paste/query/PhabricatorPasteQuery.php
@@ -90,7 +90,7 @@ final class PhabricatorPasteQuery
     }
 
     if ($this->needContent) {
-      $this->loadContent($pastes);
+      $pastes = $this->loadContent($pastes);
     }
 
     return $pastes;
@@ -205,29 +205,46 @@ final class PhabricatorPasteQuery
     $caches = $cache->getKeys($keys);
 
     $need_raw = array();
-    foreach ($pastes as $key => $paste) {
+    $have_cache = array();
+    foreach ($pastes as $paste) {
       $key = $this->getContentCacheKey($paste);
       if (isset($caches[$key])) {
         $paste->attachContent(phutil_safe_html($caches[$key]));
+        $have_cache[$paste->getPHID()] = true;
       } else {
         $need_raw[$key] = $paste;
       }
     }
 
     if (!$need_raw) {
-      return;
+      return $pastes;
     }
 
     $write_data = array();
 
-    $need_raw = $this->loadRawContent($need_raw);
-    foreach ($need_raw as $key => $paste) {
+    $have_raw = $this->loadRawContent($need_raw);
+    $have_raw = mpull($have_raw, null, 'getPHID');
+    foreach ($pastes as $key => $paste) {
+      $paste_phid = $paste->getPHID();
+      if (isset($have_cache[$paste_phid])) {
+        continue;
+      }
+
+      if (empty($have_raw[$paste_phid])) {
+        unset($pastes[$key]);
+        continue;
+      }
+
       $content = $this->buildContent($paste);
       $paste->attachContent($content);
       $write_data[$this->getContentCacheKey($paste)] = (string)$content;
     }
 
-    $cache->setKeys($write_data);
+    if ($write_data) {
+      $cache->setKeys($write_data);
+    }
+
+    return $pastes;
   }
 
   private function buildContent(PhabricatorPaste $paste) {