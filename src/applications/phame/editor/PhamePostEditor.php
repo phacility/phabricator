8ff7222d0e018ed66461b32764e9088787bb5585
Bob Trahan <btrahan@phacility.com>
2015-05-19 10:46:28 -0700

    Phame - add edge transaction implementations.
    Summary: Fixes T8252. See T6403 for longer term fix.
    
    Test Plan: made a post with a task mention in it and it worked
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T8252
    
    Differential Revision: https://secure.phabricator.com/D12926

--- src/applications/phame/editor/PhamePostEditor.php
+++ src/applications/phame/editor/PhamePostEditor.php
@@ -64,6 +64,8 @@ final class PhamePostEditor
         return $object->setBody($xaction->getNewValue());
       case PhamePostTransaction::TYPE_COMMENTS_WIDGET:
         return $object->setCommentsWidget($xaction->getNewValue());
+      case PhabricatorTransactions::TYPE_EDGE:
+        return;
     }
 
     return parent::applyCustomInternalTransaction($object, $xaction);
@@ -78,6 +80,7 @@ final class PhamePostEditor
       case PhamePostTransaction::TYPE_PHAME_TITLE:
       case PhamePostTransaction::TYPE_BODY:
       case PhamePostTransaction::TYPE_COMMENTS_WIDGET:
+      case PhabricatorTransactions::TYPE_EDGE:
         return;
     }
 
