phacility/phabricator

commit 16c8d44c37c964fcd4041aa08544385e2b4d234a

Transactions - make customization of TYPE_EDGE optional
Summary: Ref T6403. This does TYPE_EDGE since I just had to deal with T8252. Look like this fixes a few editors (maybe) that would have had fatals with mentions like slowvote and ponder.

Test Plan: made a phame post mentioning a task and it worked! joined / left a project, watched / unwatched a project and that worked! blind faith for other sites.

Reviewers: epriestley

Reviewed By: epriestley

Subscribers: Korvin, epriestley

Maniphest Tasks: T6403

Differential Revision: https://secure.phabricator.com/D12929

src/applications/audit/editor/PhabricatorAuditEditor.php
@@ -117,7 +117,6 @@ final class PhabricatorAuditEditor
     switch ($xaction->getTransactionType()) {
       case PhabricatorTransactions::TYPE_COMMENT:
       case PhabricatorTransactions::TYPE_SUBSCRIBERS:
-      case PhabricatorTransactions::TYPE_EDGE:
       case PhabricatorAuditActionConstants::ACTION:
       case PhabricatorAuditActionConstants::INLINE:
       case PhabricatorAuditActionConstants::ADD_AUDITORS:
@@ -135,7 +134,6 @@ final class PhabricatorAuditEditor
     switch ($xaction->getTransactionType()) {
       case PhabricatorTransactions::TYPE_COMMENT:
       case PhabricatorTransactions::TYPE_SUBSCRIBERS:
-      case PhabricatorTransactions::TYPE_EDGE:
       case PhabricatorAuditActionConstants::ACTION:
       case PhabricatorAuditTransaction::TYPE_COMMIT:
         return;

src/applications/differential/editor/DifferentialTransactionEditor.php
@@ -199,8 +199,6 @@ final class DifferentialTransactionEditor
       case PhabricatorTransactions::TYPE_COMMENT:
       case DifferentialTransaction::TYPE_INLINE:
         return;
-      case PhabricatorTransactions::TYPE_EDGE:
-        return;
       case DifferentialTransaction::TYPE_UPDATE:
         if (!$this->getIsCloseByCommit()) {
           switch ($object->getStatus()) {
@@ -581,7 +579,6 @@ final class DifferentialTransactionEditor
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
         return;
       case PhabricatorTransactions::TYPE_SUBSCRIBERS:
-      case PhabricatorTransactions::TYPE_EDGE:
       case PhabricatorTransactions::TYPE_COMMENT:
       case DifferentialTransaction::TYPE_ACTION:
         return;

src/applications/project/editor/PhabricatorProjectTransactionEditor.php
@@ -103,7 +103,6 @@ final class PhabricatorProjectTransactionEditor
         $object->setIsMembershipLocked($xaction->getNewValue());
         return;
       case PhabricatorTransactions::TYPE_SUBSCRIBERS:
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
         $object->setViewPolicy($xaction->getNewValue());
@@ -170,6 +169,20 @@ final class PhabricatorProjectTransactionEditor
       case PhabricatorProjectTransaction::TYPE_COLOR:
       case PhabricatorProjectTransaction::TYPE_LOCKED:
         return;
+     }
+
+    return parent::applyCustomExternalTransaction($object, $xaction);
+  }
+
+  protected function applyBuiltinExternalTransaction(
+    PhabricatorLiskDAO $object,
+    PhabricatorApplicationTransaction $xaction) {
+
+    $old = $xaction->getOldValue();
+    $new = $xaction->getNewValue();
+
+    switch ($xaction->getTransactionType()) {
+
       case PhabricatorTransactions::TYPE_EDGE:
         $edge_type = $xaction->getMetadataValue('edge:type');
         switch ($edge_type) {
@@ -225,7 +238,7 @@ final class PhabricatorProjectTransactionEditor
         return;
     }
 
-    return parent::applyCustomExternalTransaction($object, $xaction);
+    return parent::applyBuiltinExternalTransaction($object, $xaction);
   }
 
   protected function validateTransaction(

src/applications/almanac/editor/AlmanacDeviceEditor.php
@@ -57,7 +57,6 @@ final class AlmanacDeviceEditor
       case AlmanacDeviceTransaction::TYPE_INTERFACE:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
     }
 
@@ -72,7 +71,6 @@ final class AlmanacDeviceEditor
       case AlmanacDeviceTransaction::TYPE_NAME:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
       case AlmanacDeviceTransaction::TYPE_INTERFACE:
         $old = $xaction->getOldValue();

src/applications/almanac/editor/AlmanacServiceEditor.php
@@ -63,7 +63,6 @@ final class AlmanacServiceEditor
         return;
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
     }
 
@@ -78,7 +77,6 @@ final class AlmanacServiceEditor
       case AlmanacServiceTransaction::TYPE_NAME:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
       case AlmanacServiceTransaction::TYPE_LOCK:
         $service = id(new AlmanacServiceQuery())

src/applications/calendar/editor/PhabricatorCalendarEventEditor.php
@@ -111,7 +111,6 @@ final class PhabricatorCalendarEventEditor
       case PhabricatorTransactions::TYPE_COMMENT:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
       case PhabricatorTransactions::TYPE_SUBSCRIBERS:
         return;
     }
@@ -154,7 +153,6 @@ final class PhabricatorCalendarEventEditor
       case PhabricatorTransactions::TYPE_COMMENT:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
       case PhabricatorTransactions::TYPE_SUBSCRIBERS:
         return;
     }

src/applications/dashboard/editor/PhabricatorDashboardPanelTransactionEditor.php
@@ -68,8 +68,6 @@ final class PhabricatorDashboardPanelTransactionEditor
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
         $object->setEditPolicy($xaction->getNewValue());
         return;
-      case PhabricatorTransactions::TYPE_EDGE:
-        return;
     }
 
     return parent::applyCustomInternalTransaction($object, $xaction);
@@ -84,7 +82,6 @@ final class PhabricatorDashboardPanelTransactionEditor
       case PhabricatorDashboardPanelTransaction::TYPE_ARCHIVE:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
     }
 

src/applications/dashboard/editor/PhabricatorDashboardTransactionEditor.php
@@ -115,8 +115,6 @@ final class PhabricatorDashboardTransactionEditor
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
         $object->setEditPolicy($xaction->getNewValue());
         return;
-      case PhabricatorTransactions::TYPE_EDGE:
-        return;
     }
 
     return parent::applyCustomInternalTransaction($object, $xaction);
@@ -131,8 +129,6 @@ final class PhabricatorDashboardTransactionEditor
       case PhabricatorDashboardTransaction::TYPE_LAYOUT_MODE:
       case PhabricatorTransactions::TYPE_VIEW_POLICY:
       case PhabricatorTransactions::TYPE_EDIT_POLICY:
-        return;
-      case PhabricatorTransactions::TYPE_EDGE:
         return;
     }
 

commit 2154f17b3d7b7c4a47aee4ec49669f9a952f2e7c

Transactions - fix bug adding people to projects
Summary: Fixes T8264. Broken in D12929. Sweep all the applyBuiltin implementations and always break; rather than return

Test Plan: added myself to a project successfully (showed up as a member)

Reviewers: epriestley, chad

Subscribers: Korvin, epriestley

Maniphest Tasks: T6403, T8264

Differential Revision: https://secure.phabricator.com/D12940

src/applications/audit/editor/PhabricatorAuditEditor.php
@@ -200,7 +200,7 @@ final class PhabricatorAuditEditor
             $state,
             $phid);
         }
-        return;
+        break;
     }
 
     return parent::applyBuiltinExternalTransaction($object, $xaction);

src/applications/differential/editor/DifferentialTransactionEditor.php
@@ -609,7 +609,7 @@ final class DifferentialTransactionEditor
             $state,
             $phid);
         }
-        return;
+        break;
     }
 
     return parent::applyBuiltinExternalTransaction($object, $xaction);

src/applications/project/editor/PhabricatorProjectTransactionEditor.php
@@ -163,11 +163,7 @@ final class PhabricatorProjectTransactionEditor
     PhabricatorLiskDAO $object,
     PhabricatorApplicationTransaction $xaction) {
 
-    $old = $xaction->getOldValue();
-    $new = $xaction->getNewValue();
-
     switch ($xaction->getTransactionType()) {
-
       case PhabricatorTransactions::TYPE_EDGE:
         $edge_type = $xaction->getMetadataValue('edge:type');
         switch ($edge_type) {
@@ -220,7 +216,7 @@ final class PhabricatorProjectTransactionEditor
             }
             break;
         }
-        return;
+        break;
     }
 
     return parent::applyBuiltinExternalTransaction($object, $xaction);