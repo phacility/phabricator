04584131f79159376105c23b9bfb9d76249467f9
lkassianik <lyubakassianik@gmail.com>
2015-05-22 14:46:48 -0700

    Swapping out foreach out of behavior
    Summary: Ref T8300, swapping out foreach out of behavior
    
    Test Plan: no user facing changes
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: Korvin, epriestley
    
    Maniphest Tasks: T8300
    
    Differential Revision: https://secure.phabricator.com/D12980

--- webroot/rsrc/js/application/calendar/behavior-day-view.js
+++ webroot/rsrc/js/application/calendar/behavior-day-view.js
@@ -7,6 +7,7 @@ JX.behavior('day-view', function(config) {
   var hourly_events = config.hourlyEvents;
   var today_events = config.todayEvents;
 
+
   function findTodayClusters() {
     var events = today_events.sort(function(x, y){
       return (x.eventStartEpoch - y.eventStartEpoch);
@@ -14,34 +15,42 @@ JX.behavior('day-view', function(config) {
 
     var clusters = [];
 
-    events.forEach(function(e){
-      var destination_cluster_index = null;
-      var event_start = e.eventStartEpoch - (30*60);
-      var event_end = e.eventEndEpoch + (30*60);
+    for (var i=0; i < events.length; i++) {
+      var today_event = events[i];
 
-      clusters.forEach(function(cluster, index){
-        for(var i=0; i < cluster.length; i++) {
-          var clustered_event = cluster[i];
+      var destination_cluster_index = null;
+      var event_start = today_event.eventStartEpoch - (30*60);
+      var event_end = today_event.eventEndEpoch + (30*60);
+
+      for (var j=0; j < clusters.length; j++) {
+        var cluster = clusters[j];
+
+        for(var k=0; k < cluster.length; k++) {
+          var clustered_event = cluster[k];
           var compare_event_start = clustered_event.eventStartEpoch;
           var compare_event_end = clustered_event.eventEndEpoch;
 
           if (event_start < compare_event_end &&
             event_end > compare_event_start) {
-            destination_cluster_index = index;
+            destination_cluster_index = j;
             break;
           }
         }
-      });
+
+        if (destination_cluster_index !== null) {
+          break;
+        }
+      }
 
       if (destination_cluster_index !== null) {
-        clusters[destination_cluster_index].push(e);
+        clusters[destination_cluster_index].push(today_event);
         destination_cluster_index = null;
       } else {
         var next_cluster = [];
-        next_cluster.push(e);
+        next_cluster.push(today_event);
         clusters.push(next_cluster);
       }
-    });
+    }
 
     return clusters;
   }
