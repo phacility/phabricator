cdef3e8bc868098de98525b26ccca3dfe294c32b
epriestley <git@epriestley.com>
2015-06-07 07:32:09 -0700

    Convert Files to SearchFields
    Summary:
    Ref T8441. Ref T7715.
    
      - Update FileSearchEngine.
      - Nothing too special/fancy.
    
    Test Plan:
      - Searched for various files.
    
    Reviewers: btrahan
    
    Reviewed By: btrahan
    
    Subscribers: epriestley
    
    Maniphest Tasks: T7715, T8441
    
    Differential Revision: https://secure.phabricator.com/D13175

---
+++ src/applications/search/field/PhabricatorSearchThreeStateField.php
@@ -0,0 +1,48 @@
+<?php
+
+final class PhabricatorSearchThreeStateField
+  extends PhabricatorSearchField {
+
+  private $options;
+
+  public function setOptions($null, $yes, $no) {
+    $this->options = array(
+      '' => $null,
+      'true' => $yes,
+      'false' => $no,
+    );
+    return $this;
+  }
+
+  public function getOptions() {
+    return $this->options;
+  }
+
+  protected function getDefaultValue() {
+    return null;
+  }
+
+  protected function getValueFromRequest(AphrontRequest $request, $key) {
+    if (!strlen($request->getStr($key))) {
+      return null;
+    }
+    return $request->getBool($key);
+  }
+
+  protected function newControl() {
+    return id(new AphrontFormSelectControl())
+      ->setOptions($this->getOptions());
+  }
+
+  protected function getValueForControl() {
+    $value = parent::getValueForControl();
+    if ($value === true) {
+      return 'true';
+    }
+    if ($value === false) {
+      return 'false';
+    }
+    return null;
+  }
+
+}
