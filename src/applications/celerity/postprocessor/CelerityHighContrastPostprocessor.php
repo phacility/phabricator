f93ba518d292dca2d3d826f0b19cb7436fbbbf49
Chad Little <chad@phacility.com>
2015-06-21 13:34:33 +0100

    [Redesign] Full High Contrast Mode
    Summary: Ref T8099, Ref T8614. Adds a more complete full contrast mode.
    
    Test Plan: Bop through UIExamples, check tokens, diffs, tasks, workboards, buttons, info views, looks sharp.
    
    Reviewers: btrahan, epriestley
    
    Reviewed By: epriestley
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T8099, T8614
    
    Differential Revision: https://secure.phabricator.com/D13375

--- src/applications/celerity/postprocessor/CelerityHighContrastPostprocessor.php
+++ src/applications/celerity/postprocessor/CelerityHighContrastPostprocessor.php
@@ -13,14 +13,56 @@ final class CelerityHighContrastPostprocessor
 
   public function buildVariables() {
     return array(
-      'page.background.light' => '#dfdfdf',
-      'page.background.dark' => '#dfdfdf',
+      'blue' => '#226B9B',
+      'sky' => '#226B9B',
+      'anchor' => '#226B9B',
 
-      'lightblueborder' => '#000099',
-      'blueborder' => '#000066',
+      'thinblueborder' => '#BFCFDA',
+      'lightblueborder' => '#8C98B8',
+      'blueborder' => '#626E82',
 
-      'lightbluetext' => '#333366',
-      'bluetext' => '#222244',
+      'lightgreyborder' => '#555',
+      'greyborder' => '#333',
+
+      'lightbluetext' => '#555',
+      'bluetext' => '#333',
+      'darkbluetext' => '#000',
+
+      'lightgreytext' => '#555',
+      'greytext' => '#333',
+      'darkgreytext' => '#000',
+
+      'sh-redtext' => '#333',
+      'sh-redborder' => '#777',
+
+      'sh-greentext' => '#333',
+      'sh-greenborder' => '#777',
+
+      'sh-bluetext' => '#333',
+      'sh-blueborder' => '#777',
+
+      'sh-yellowtext' => '#333',
+      'sh-yellowborder' => '#777',
+
+      'sh-orangetext' => '#333',
+      'sh-orangeborder' => '#777',
+
+      'sh-violettext' => '#333',
+      'sh-violetborder' => '#777',
+
+      'sh-indigotext' => '#333',
+      'sh-indigoborder' => '#777',
+
+      'sh-pinktext' => '#333',
+      'sh-pinkborder' => '#777',
+
+      'sh-greytext' => '#333',
+      'sh-greyborder' => '#777',
+
+      'sh-disabledtext' => '#555',
+      'sh-disabledborder' => '#777',
+
+
     );
   }
 
