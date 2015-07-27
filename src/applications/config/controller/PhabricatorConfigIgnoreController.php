564aa2c4ba6ef30ac048bbd84ab9076dfc9934fc
Chad Little <chad@chadsdomain.com>
2015-07-27 09:06:26 -0700

    Update Config app for handleRequest
    Summary: Update all found callsites in Config.
    
    Test Plan: Check setup issues, databases, edit a value, remove a warning, everything I could click on.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: epriestley, Korvin
    
    Maniphest Tasks: T8628
    
    Differential Revision: https://secure.phabricator.com/D13727

--- src/applications/config/controller/PhabricatorConfigIgnoreController.php
+++ src/applications/config/controller/PhabricatorConfigIgnoreController.php
@@ -3,38 +3,33 @@
 final class PhabricatorConfigIgnoreController
   extends PhabricatorConfigController {
 
-  private $verb;
-  private $issue;
+  public function handleRequest(AphrontRequest $request) {
+    $viewer = $request->getViewer();
+    $issue = $request->getURIData('key');
+    $verb = $request->getURIData('verb');
 
-  public function willProcessRequest(array $data) {
-    $this->verb = $data['verb'];
-    $this->issue = $data['key'];
-  }
-
-  public function processRequest() {
-    $request = $this->getRequest();
-    $issue_uri = $this->getApplicationURI('issue/'.$this->issue.'/');
+    $issue_uri = $this->getApplicationURI('issue/'.$issue.'/');
 
     if ($request->isDialogFormPost()) {
-      $this->manageApplication();
+      $this->manageApplication($issue);
       return id(new AphrontRedirectResponse())->setURI($issue_uri);
     }
 
-    if ($this->verb == 'ignore') {
+    if ($verb == 'ignore') {
       $title = pht('Really ignore this setup issue?');
       $submit_title = pht('Ignore');
       $body = pht(
         "You can ignore an issue if you don't want to fix it, or plan to ".
         "fix it later. Ignored issues won't appear on every page but will ".
         "still be shown in the list of open issues.");
-    } else if ($this->verb == 'unignore') {
+    } else if ($verb == 'unignore') {
       $title = pht('Unignore this setup issue?');
       $submit_title = pht('Unignore');
       $body = pht(
         'This issue will no longer be suppressed, and will return to its '.
         'rightful place as a global setup warning.');
     } else {
-      throw new Exception(pht('Unrecognized verb: %s', $this->verb));
+      throw new Exception(pht('Unrecognized verb: %s', $verb));
     }
 
     $dialog = id(new AphrontDialogView())
@@ -47,15 +42,15 @@ final class PhabricatorConfigIgnoreController
     return id(new AphrontDialogResponse())->setDialog($dialog);
   }
 
-  public function manageApplication() {
+  public function manageApplication($issue) {
     $key = 'config.ignore-issues';
     $config_entry = PhabricatorConfigEntry::loadConfigEntry($key);
     $list = $config_entry->getValue();
 
-    if (isset($list[$this->issue])) {
-      unset($list[$this->issue]);
+    if (isset($list[$issue])) {
+      unset($list[$issue]);
     } else {
-      $list[$this->issue] = true;
+      $list[$issue] = true;
     }
 
     PhabricatorConfigEditor::storeNewValue(
