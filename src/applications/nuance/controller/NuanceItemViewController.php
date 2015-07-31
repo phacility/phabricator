d902dd6589658dd565092fd1851dde08801d0711
Chad Little <chad@phacility.com>
2015-07-31 09:14:33 -0700

    Update Nuance for handleRequest
    Summary: Updates Nuance controllers
    
    Test Plan: Read carefully, lint.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: epriestley, Korvin
    
    Differential Revision: https://secure.phabricator.com/D13762

--- src/applications/nuance/controller/NuanceItemViewController.php
+++ src/applications/nuance/controller/NuanceItemViewController.php
@@ -2,28 +2,13 @@
 
 final class NuanceItemViewController extends NuanceController {
 
-  private $itemID;
+  public function handleRequest(AphrontRequest $request) {
+    $viewer = $request->getViewer();
+    $id = $request->getURIData('id');
 
-  public function setItemID($item_id) {
-    $this->itemID = $item_id;
-    return $this;
-  }
-  public function getItemID() {
-    return $this->itemID;
-  }
-
-  public function willProcessRequest(array $data) {
-    $this->setItemID($data['id']);
-  }
-
-  public function processRequest() {
-    $request = $this->getRequest();
-    $user = $request->getUser();
-
-    $item_id = $this->getItemID();
     $item = id(new NuanceItemQuery())
-      ->setViewer($user)
-      ->withIDs(array($item_id))
+      ->setViewer($viewer)
+      ->withIDs(array($id))
       ->executeOne();
 
     if (!$item) {
