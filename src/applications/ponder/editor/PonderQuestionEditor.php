3db9e4b4e5a20759777e66fa1069650fa4d759b4
Chad Little <chad@chadsdomain.com>
2015-08-13 13:00:47 -0700

    Add mailtags to Ponder
    Summary: Ref T3578  Adds mailtags to Ponder, answer stuff not quite ready, but that's another diff.
    
    Test Plan: set preferences to notify, second account updates a question, get notification on first.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Maniphest Tasks: T3578
    
    Differential Revision: https://secure.phabricator.com/D13886

--- src/applications/ponder/editor/PonderQuestionEditor.php
+++ src/applications/ponder/editor/PonderQuestionEditor.php
@@ -212,6 +212,19 @@ final class PonderQuestionEditor
     return true;
   }
 
+  public function getMailTagsMap() {
+    return array(
+      PonderQuestionTransaction::MAILTAG_DETAILS =>
+        pht('Someone changes the questions details.'),
+      PonderQuestionTransaction::MAILTAG_ANSWERS =>
+        pht('Someone adds a new answer.'),
+      PonderQuestionTransaction::MAILTAG_COMMENT =>
+        pht('Someone comments on the question.'),
+      PonderQuestionTransaction::MAILTAG_OTHER =>
+        pht('Other question activity not listed above occurs.'),
+    );
+  }
+
   protected function buildReplyHandler(PhabricatorLiskDAO $object) {
     return id(new PonderQuestionReplyHandler())
       ->setMailReceiver($object);
