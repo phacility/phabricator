4c437049b8e51762289ff8169751e37c902d4aa6
Chad Little <chad@phacility.com>
2015-08-16 17:48:19 -0700

    Update style of remarkup tt
    Summary: Standardize color and spacing.
    
    Test Plan:
    `Wrote a test plan`
    
    {F720873}
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D13916

--- resources/celerity/map.php
+++ resources/celerity/map.php
@@ -7,7 +7,7 @@
  */
 return array(
   'names' => array(
-    'core.pkg.css' => '33799ec4',
+    'core.pkg.css' => '12dcd407',
     'core.pkg.js' => 'a590b451',
     'darkconsole.pkg.js' => 'e7393ebb',
     'differential.pkg.css' => '9451634c',
@@ -104,7 +104,7 @@ return array(
     'rsrc/css/application/tokens/tokens.css' => '3d0f239e',
     'rsrc/css/application/uiexample/example.css' => '528b19de',
     'rsrc/css/core/core.css' => 'a76cefc9',
-    'rsrc/css/core/remarkup.css' => 'de13dcbe',
+    'rsrc/css/core/remarkup.css' => '66f5facd',
     'rsrc/css/core/syntax.css' => '9fd11da8',
     'rsrc/css/core/z-index.css' => '57ddcaa2',
     'rsrc/css/diviner/diviner-shared.css' => '5a337049',
@@ -737,7 +737,7 @@ return array(
     'phabricator-object-selector-css' => '85ee8ce6',
     'phabricator-phtize' => 'd254d646',
     'phabricator-prefab' => '6920d200',
-    'phabricator-remarkup-css' => 'de13dcbe',
+    'phabricator-remarkup-css' => '66f5facd',
     'phabricator-search-results-css' => '7dea472c',
     'phabricator-shaped-request' => '7cbe244b',
     'phabricator-side-menu-view-css' => 'bec2458e',
