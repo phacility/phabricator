a62337dcd83128aebb0940171524eb375ebac293
Chad Little <chad@phacility.com>
2015-09-15 18:32:01 -0700

    Update PHUIActionPanelView
    Summary: Making these a little more fun, a little more flexible and better looking. Will have an update for rSAAS in a bit.
    
    Test Plan:
    Make lots of them. Click.
    
    {F815658}
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D14115

--- src/view/phui/PHUIActionPanelView.php
+++ src/view/phui/PHUIActionPanelView.php
@@ -52,29 +52,8 @@ final class PHUIActionPanelView extends AphrontTagView {
     return $this;
   }
 
-  protected function getStateIcon() {
-    $icon = new PHUIIconView();
-    switch ($this->state) {
-      case self::STATE_WARN:
-        $icon->setIconFont('fa-exclamation-circle msr');
-      break;
-      case self::STATE_INFO:
-        $icon->setIconFont('fa-info-circle msr');
-      break;
-      case self::STATE_ERROR:
-        $icon->setIconFont('fa-exclamation-triangle msr');
-      break;
-      case self::STATE_PROGRESS:
-        $icon->setIconFont('fa-refresh ph-spin msr');
-      break;
-      case self::STATE_SUCCESS:
-        $icon->setIconFont('fa-check msr');
-      break;
-      case self::STATE_NONE:
-        return null;
-      break;
-    }
-    return $icon;
+  protected function getTagName() {
+    return 'div';
   }
 
   protected function getTagAttributes() {
@@ -82,43 +61,26 @@ final class PHUIActionPanelView extends AphrontTagView {
 
     $classes = array();
     $classes[] = 'phui-action-panel';
-    if ($this->status) {
-      $classes[] = 'phui-action-panel-has-status';
+    if ($this->state) {
       $classes[] = $this->state;
-
+    }
+    if ($this->bigText) {
+      $classes[] = 'phui-action-panel-bigtext';
     }
 
     return array(
       'class' => implode(' ', $classes),
     );
-
   }
 
   protected function getTagContent() {
 
     $icon = null;
-    if ($this->fontIcon || $this->bigText) {
-      if ($this->fontIcon) {
-        $fonticon = id(new PHUIIconView())
-          ->setIconFont($this->fontIcon);
-      } else {
-        $fonticon = phutil_tag(
-          'span',
-          array(
-            'class' => 'phui-action-panel-bigtext',
-          ),
-          $this->bigText);
-      }
-      if ($this->href) {
-        $fonticon = phutil_tag(
-          'a',
-          array(
-            'href' => $this->href,
-          ),
-          $fonticon);
-      }
+    if ($this->fontIcon) {
+      $fonticon = id(new PHUIIconView())
+        ->setIconFont($this->fontIcon);
       $icon = phutil_tag(
-        'div',
+        'span',
         array(
           'class' => 'phui-action-panel-icon',
         ),
@@ -147,26 +109,39 @@ final class PHUIActionPanelView extends AphrontTagView {
     $subheader = null;
     if ($this->subHeader) {
       $subheader = phutil_tag(
-        'div',
+        'span',
         array(
           'class' => 'phui-action-panel-subheader',
         ),
         $this->subHeader);
     }
 
-    $status = null;
-    if ($this->status && $this->state) {
-      $state_icon = $this->getStateIcon();
-      $status = phutil_tag(
-        ($this->href) ? 'a' : 'div',
-        array(
-          'class' => 'phui-action-panel-status',
-          'href' => ($this->href) ? $this->href : null,
-        ),
-        array($state_icon, $this->status));
-    }
+    $row = phutil_tag(
+      'span',
+      array(
+        'class' => 'phui-action-panel-row',
+      ),
+      array(
+        $icon,
+        $subheader,
+      ));
 
-    return array($icon, $header, $subheader, $status);
+    $table = phutil_tag(
+      'span',
+      array(
+        'class' => 'phui-action-panel-table',
+      ),
+      $row);
+
+    $content = phutil_tag(
+      'a',
+      array(
+        'href' => $this->href,
+        'class' => 'phui-action-panel-hitarea',
+      ),
+      $table);
+
+    return array($header, $content);
 
   }
 
