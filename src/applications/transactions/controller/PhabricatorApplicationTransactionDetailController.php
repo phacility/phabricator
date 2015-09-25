3e60740c7cbffbe8e5422cda10e71225edaa9dbc
epriestley <git@epriestley.com>
2015-09-25 11:15:57 -0700

    Slightly modernize transaction diff controller
    Summary: Ref T9272. This doesn't fix anything, just a little cleanup while I was looking at it.
    
    Test Plan: Clicked "Show Details" on a couple description changes, got the same effect for less code.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T9272
    
    Differential Revision: https://secure.phabricator.com/D14168

--- src/applications/transactions/controller/PhabricatorApplicationTransactionDetailController.php
+++ src/applications/transactions/controller/PhabricatorApplicationTransactionDetailController.php
@@ -3,22 +3,16 @@
 final class PhabricatorApplicationTransactionDetailController
   extends PhabricatorApplicationTransactionController {
 
-  private $phid;
-
   public function shouldAllowPublic() {
     return true;
   }
 
-  public function willProcessRequest(array $data) {
-    $this->phid = $data['phid'];
-  }
-
-  public function processRequest() {
-    $request = $this->getRequest();
-    $viewer = $request->getUser();
+  public function handleRequest(AphrontRequest $request) {
+    $viewer = $this->getViewer();
+    $phid = $request->getURIData('phid');
 
     $xaction = id(new PhabricatorObjectQuery())
-      ->withPHIDs(array($this->phid))
+      ->withPHIDs(array($phid))
       ->setViewer($viewer)
       ->executeOne();
     if (!$xaction) {
@@ -26,17 +20,14 @@ final class PhabricatorApplicationTransactionDetailController
     }
 
     $details = $xaction->renderChangeDetails($viewer);
-
     $cancel_uri = $this->guessCancelURI($viewer, $xaction);
-    $dialog = id(new AphrontDialogView())
-      ->setUser($viewer)
+
+    return $this->newDialog()
       ->setTitle(pht('Change Details'))
       ->setWidth(AphrontDialogView::WIDTH_FULL)
       ->setFlush(true)
       ->appendChild($details)
       ->addCancelButton($cancel_uri);
-
-    return id(new AphrontDialogResponse())->setDialog($dialog);
   }
 
 }
