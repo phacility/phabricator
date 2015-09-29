phacility/phabricator

commit ed77b639f0cf287494f39e9a8629cbb8608641bd

Fix Ponder Answer email reply handler
Summary: Should fix all email reply issues, but no solid means of testing at home (how do you local reply test?)

Test Plan: Check for answer mail in /mail/ and see proper headers. Make sure question mail works too.

Reviewers: epriestley

Reviewed By: epriestley

Subscribers: Korvin

Maniphest Tasks: T3846

Differential Revision: https://secure.phabricator.com/D13951

src/applications/ponder/editor/PonderAnswerEditor.php
@@ -86,16 +86,17 @@ final class PonderAnswerEditor extends PonderEditor {
   }
 
   protected function buildReplyHandler(PhabricatorLiskDAO $object) {
-    $question = $object->getQuestion();
-    return id(new PonderQuestionReplyHandler())
-      ->setMailReceiver($question);
+    return id(new PonderAnswerReplyHandler())
+      ->setMailReceiver($object);
   }
 
   protected function buildMailTemplate(PhabricatorLiskDAO $object) {
-    $question = $object->getQuestion();
-    return parent::buildMailTemplate($question);
-  }
+    $id = $object->getID();
 
+    return id(new PhabricatorMetaMTAMail())
+      ->setSubject("ANSR{$id}")
+      ->addHeader('Thread-Topic', "ANSR{$id}");
+  }
 
   protected function buildMailBody(
     PhabricatorLiskDAO $object,

src/applications/ponder/editor/PonderEditor.php
@@ -7,23 +7,6 @@ abstract class PonderEditor
     return 'PhabricatorPonderApplication';
   }
 
-  protected function shouldPublishFeedStory(
-    PhabricatorLiskDAO $object,
-    array $xactions) {
-    return true;
-  }
-
-  protected function buildMailTemplate(PhabricatorLiskDAO $object) {
-    $id = $object->getID();
-    $title = $object->getTitle();
-    $original_title = $object->getOriginalTitle();
-
-    return id(new PhabricatorMetaMTAMail())
-      ->setSubject("Q{$id}: {$title}")
-      ->addHeader('Thread-Topic', "Q{$id}: {$original_title}");
-  }
-
-
   protected function getMailTo(PhabricatorLiskDAO $object) {
     return array(
       $object->getAuthorPHID(),

src/applications/ponder/editor/PonderQuestionEditor.php
@@ -231,6 +231,16 @@ final class PonderQuestionEditor
       ->setMailReceiver($object);
   }
 
+  protected function buildMailTemplate(PhabricatorLiskDAO $object) {
+    $id = $object->getID();
+    $title = $object->getTitle();
+    $original_title = $object->getOriginalTitle();
+
+    return id(new PhabricatorMetaMTAMail())
+      ->setSubject("Q{$id}: {$title}")
+      ->addHeader('Thread-Topic', "Q{$id}: {$original_title}");
+  }
+
   protected function buildMailBody(
     PhabricatorLiskDAO $object,
     array $xactions) {

commit ae082c60338d3bbf166f75d25de5f8baa7a17ab7

Make Ponder Emails a little more consistently delivered
Summary: Ref T9271, maybe fixes it. This restores feed publishing for answers (broken in D13951) and sends the author of the question an email for new answers. Also, unsure how to pull all question subsribers to the answer email, or is it automagical?

Test Plan: notchad asks a question, chad answers, log into notchad and see that mail was delivered, see feed story.

Reviewers: epriestley

Reviewed By: epriestley

Subscribers: revi, Korvin

Maniphest Tasks: T9271

Differential Revision: https://secure.phabricator.com/D14171

src/applications/ponder/editor/PonderAnswerEditor.php
@@ -85,6 +85,27 @@ final class PonderAnswerEditor extends PonderEditor {
     return true;
   }
 
+  protected function getMailTo(PhabricatorLiskDAO $object) {
+    $phids = array();
+    $phids[] = $object->getAuthorPHID();
+    $phids[] = $this->requireActor()->getPHID();
+
+    $question = id(new PonderQuestionQuery())
+      ->setViewer($this->requireActor())
+      ->withIDs(array($object->getQuestionID()))
+      ->executeOne();
+
+    $phids[] = $question->getAuthorPHID();
+
+    return $phids;
+  }
+
+  protected function shouldPublishFeedStory(
+    PhabricatorLiskDAO $object,
+    array $xactions) {
+      return true;
+  }
+
   protected function buildReplyHandler(PhabricatorLiskDAO $object) {
     return id(new PonderAnswerReplyHandler())
       ->setMailReceiver($object);

src/applications/ponder/editor/PonderEditor.php
@@ -7,13 +7,6 @@ abstract class PonderEditor
     return 'PhabricatorPonderApplication';
   }
 
-  protected function getMailTo(PhabricatorLiskDAO $object) {
-    return array(
-      $object->getAuthorPHID(),
-      $this->requireActor()->getPHID(),
-    );
-  }
-
    protected function getMailSubjectPrefix() {
     return '[Ponder]';
   }

src/applications/ponder/editor/PonderQuestionEditor.php
@@ -209,6 +209,13 @@ final class PonderQuestionEditor
       return true;
   }
 
+  protected function getMailTo(PhabricatorLiskDAO $object) {
+    return array(
+      $object->getAuthorPHID(),
+      $this->requireActor()->getPHID(),
+    );
+  }
+
   protected function shouldPublishFeedStory(
     PhabricatorLiskDAO $object,
     array $xactions) {