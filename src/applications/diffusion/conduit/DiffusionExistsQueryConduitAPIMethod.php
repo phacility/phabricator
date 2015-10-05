b2e89a9e48761aa33da458da75f68b67c702d6d3
epriestley <git@epriestley.com>
2015-10-05 15:57:41 -0700

    Fix several error handling issues with Subversion commits in Diffusion
    Summary:
    Ref T9513. I checked this briefly but didn't do a very thorough job of it.
    
      - Don't try to query merges for Subversion, since it doesn't support them.
      - Fix up "existsquery" to work properly (and efficiently) for both hosted and imported repositories.
      - Fix up "parentsquery" to have similar behavior on invalid commits to other VCSes (throw an exception).
    
    Test Plan:
      - No more merges warning on SVN.
      - Hosted SVN gets the right exists result now.
      - Visiting "r23980283789287" now 404's instead of "not parsed yet".
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T9513
    
    Differential Revision: https://secure.phabricator.com/D14239

--- src/applications/diffusion/conduit/DiffusionExistsQueryConduitAPIMethod.php
+++ src/applications/diffusion/conduit/DiffusionExistsQueryConduitAPIMethod.php
@@ -33,16 +33,13 @@ final class DiffusionExistsQueryConduitAPIMethod
   protected function getSVNResult(ConduitAPIRequest $request) {
     $repository = $this->getDiffusionRequest()->getRepository();
     $commit = $request->getValue('commit');
-    list($info) = $repository->execxRemoteCommand(
-      'info %s',
-      $repository->getRemoteURI());
-    $exists = false;
-    $matches = null;
-    if (preg_match('/^Revision: (\d+)$/m', $info, $matches)) {
-      $base_revision = $matches[1];
-      $exists = $base_revision >= $commit;
-    }
-    return $exists;
+
+    $refs = id(new DiffusionCachedResolveRefsQuery())
+      ->setRepository($repository)
+      ->withRefs(array($commit))
+      ->execute();
+
+    return (bool)$refs;
   }
 
   protected function getMercurialResult(ConduitAPIRequest $request) {
