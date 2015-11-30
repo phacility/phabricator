47a5ebb4fec26b4cfe04365f3d53633a0c3a7ea5
lkassianik <lyubakassianik@gmail.com>
2015-11-30 06:44:03 -0800

    Correctly implementing mailkey for Phurl
    Summary: Re T6049, Correctly implementing mailkey for Phurl
    
    Test Plan: Edit Phurl URL, receive email.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D14605

--- src/applications/phurl/storage/PhabricatorPhurlURL.php
+++ src/applications/phurl/storage/PhabricatorPhurlURL.php
@@ -22,6 +22,8 @@ final class PhabricatorPhurlURL extends PhabricatorPhurlDAO
   protected $authorPHID;
   protected $spacePHID;
 
+  protected $mailKey;
+
   const DEFAULT_ICON = 'fa-compress';
 
   public static function initializeNewPhurlURL(PhabricatorUser $actor) {
@@ -45,6 +47,7 @@ final class PhabricatorPhurlURL extends PhabricatorPhurlDAO
         'alias' => 'sort64?',
         'longURL' => 'text',
         'description' => 'text',
+        'mailKey' => 'bytes20',
       ),
       self::CONFIG_KEY_SCHEMA => array(
         'key_instance' => array(
@@ -56,6 +59,13 @@ final class PhabricatorPhurlURL extends PhabricatorPhurlDAO
         ),
       ),
     ) + parent::getConfiguration();
+  }
+
+  public function save() {
+    if (!$this->getMailKey()) {
+      $this->setMailKey(Filesystem::readRandomCharacters(20));
+    }
+    return parent::save();
   }
 
   public function generatePHID() {
