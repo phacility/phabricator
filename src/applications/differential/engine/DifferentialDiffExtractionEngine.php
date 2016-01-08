413ca12fda6b97cd64a01b7b974dc8343f6d4c11
epriestley <git@epriestley.com>
2016-01-08 04:59:31 -0800

    Move commit attachment to a separate CLI command
    Summary:
    Ref T9319. See D14967. As before, this is making a deeply-buried, complex operation easier to test by providing a CLI command.
    
    This adds `bin/differential attach-commit rXnnnn Dnnnn` to pretend that `rXnnnn` was just committed and matched `Dnnnn`.
    
    Test Plan:
      - Ran `bin/differential attach-commit X Y` for several different values, saw updates in the UI.
      - Faked the message parser to make sure stuff still worked there.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T9319
    
    Differential Revision: https://secure.phabricator.com/D14968

--- src/applications/differential/engine/DifferentialDiffExtractionEngine.php
+++ src/applications/differential/engine/DifferentialDiffExtractionEngine.php
@@ -87,4 +87,174 @@ final class DifferentialDiffExtractionEngine extends Phobject {
     return $diff->save();
   }
 
+  public function isDiffChangedBeforeCommit(
+    PhabricatorRepositoryCommit $commit,
+    DifferentialDiff $old,
+    DifferentialDiff $new) {
+
+    $viewer = $this->getViewer();
+    $repository = $commit->getRepository();
+    $identifier = $commit->getCommitIdentifier();
+
+    $vs_changesets = array();
+    foreach ($old->getChangesets() as $changeset) {
+      $path = $changeset->getAbsoluteRepositoryPath($repository, $old);
+      $path = ltrim($path, '/');
+      $vs_changesets[$path] = $changeset;
+    }
+
+    $changesets = array();
+    foreach ($new->getChangesets() as $changeset) {
+      $path = $changeset->getAbsoluteRepositoryPath($repository, $new);
+      $path = ltrim($path, '/');
+      $changesets[$path] = $changeset;
+    }
+
+    if (array_fill_keys(array_keys($changesets), true) !=
+        array_fill_keys(array_keys($vs_changesets), true)) {
+      return true;
+    }
+
+    $file_phids = array();
+    foreach ($vs_changesets as $changeset) {
+      $metadata = $changeset->getMetadata();
+      $file_phid = idx($metadata, 'new:binary-phid');
+      if ($file_phid) {
+        $file_phids[$file_phid] = $file_phid;
+      }
+    }
+
+    $files = array();
+    if ($file_phids) {
+      $files = id(new PhabricatorFileQuery())
+        ->setViewer(PhabricatorUser::getOmnipotentUser())
+        ->withPHIDs($file_phids)
+        ->execute();
+      $files = mpull($files, null, 'getPHID');
+    }
+
+    foreach ($changesets as $path => $changeset) {
+      $vs_changeset = $vs_changesets[$path];
+
+      $file_phid = idx($vs_changeset->getMetadata(), 'new:binary-phid');
+      if ($file_phid) {
+        if (!isset($files[$file_phid])) {
+          return true;
+        }
+
+        $drequest = DiffusionRequest::newFromDictionary(array(
+          'user' => $viewer,
+          'repository' => $repository,
+          'commit' => $identifier,
+          'path' => $path,
+        ));
+
+        $corpus = DiffusionFileContentQuery::newFromDiffusionRequest($drequest)
+          ->setViewer(PhabricatorUser::getOmnipotentUser())
+          ->loadFileContent()
+          ->getCorpus();
+
+        if ($files[$file_phid]->loadFileData() != $corpus) {
+          return true;
+        }
+      } else {
+        $context = implode("\n", $changeset->makeChangesWithContext());
+        $vs_context = implode("\n", $vs_changeset->makeChangesWithContext());
+
+        // We couldn't just compare $context and $vs_context because following
+        // diffs will be considered different:
+        //
+        //   -(empty line)
+        //   -echo 'test';
+        //    (empty line)
+        //
+        //    (empty line)
+        //   -echo "test";
+        //   -(empty line)
+
+        $hunk = id(new DifferentialModernHunk())->setChanges($context);
+        $vs_hunk = id(new DifferentialModernHunk())->setChanges($vs_context);
+        if ($hunk->makeOldFile() != $vs_hunk->makeOldFile() ||
+            $hunk->makeNewFile() != $vs_hunk->makeNewFile()) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  public function updateRevisionWithCommit(
+    DifferentialRevision $revision,
+    PhabricatorRepositoryCommit $commit,
+    array $more_xactions,
+    PhabricatorContentSource $content_source) {
+
+    $viewer = $this->getViewer();
+    $result_data = array();
+
+    $new_diff = $this->newDiffFromCommit($commit);
+
+    $old_diff = $revision->getActiveDiff();
+    $changed_uri = null;
+    if ($old_diff) {
+      $old_diff = id(new DifferentialDiffQuery())
+        ->setViewer($viewer)
+        ->withIDs(array($old_diff->getID()))
+        ->needChangesets(true)
+        ->executeOne();
+      if ($old_diff) {
+        $has_changed = $this->isDiffChangedBeforeCommit(
+          $commit,
+          $old_diff,
+          $new_diff);
+        if ($has_changed) {
+          $result_data['vsDiff'] = $old_diff->getID();
+
+          $revision_monogram = $revision->getMonogram();
+          $old_id = $old_diff->getID();
+          $new_id = $new_diff->getID();
+
+          $changed_uri = "/{$revision_monogram}?vs={$old_id}&id={$new_id}#toc";
+          $changed_uri = PhabricatorEnv::getProductionURI($changed_uri);
+        }
+      }
+    }
+
+    $xactions = array();
+
+    $xactions[] = id(new DifferentialTransaction())
+      ->setTransactionType(DifferentialTransaction::TYPE_UPDATE)
+      ->setIgnoreOnNoEffect(true)
+      ->setNewValue($new_diff->getPHID())
+      ->setMetadataValue('isCommitUpdate', true);
+
+    foreach ($more_xactions as $more_xaction) {
+      $xactions[] = $more_xaction;
+    }
+
+    $editor = id(new DifferentialTransactionEditor())
+      ->setActor($viewer)
+      ->setContinueOnMissingFields(true)
+      ->setContentSource($content_source)
+      ->setChangedPriorToCommitURI($changed_uri)
+      ->setIsCloseByCommit(true);
+
+    $author_phid = $this->getAuthorPHID();
+    if ($author_phid !== null) {
+      $editor->setActingAsPHID($author_phid);
+    }
+
+    try {
+      $editor->applyTransactions($revision, $xactions);
+    } catch (PhabricatorApplicationTransactionNoEffectException $ex) {
+      // NOTE: We've marked transactions other than the CLOSE transaction
+      // as ignored when they don't have an effect, so this means that we
+      // lost a race to close the revision. That's perfectly fine, we can
+      // just continue normally.
+    }
+
+    return $result_data;
+  }
+
 }
