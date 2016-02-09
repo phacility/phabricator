3309ae1a304f915feaa194ea55efe354c9c72d8e
epriestley <git@epriestley.com>
2016-02-09 05:03:54 -0800

    Move workboard panning to new Workboard code
    Summary: Ref T4900. Continuing to move this over into a more structured approach.
    
    Test Plan: Panned a workboard.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T4900
    
    Differential Revision: https://secure.phabricator.com/D15225

--- webroot/rsrc/js/application/projects/Workboard.js
+++ webroot/rsrc/js/application/projects/Workboard.js
@@ -19,12 +19,17 @@ JX.install('Workboard', {
     this._boardNodes = {};
 
     this._setupCoverImageHandlers();
+    this._setupPanHandlers();
   },
 
   members: {
     _config: null,
     _boardNodes: null,
     _currentBoard: null,
+
+    _panOrigin: null,
+    _panNode: null,
+    _panX: null,
 
     addBoard: function(board_phid, board_node) {
       this._currentBoard = board_phid;
@@ -73,7 +78,51 @@ JX.install('Workboard', {
       });
 
       drop.start();
+    },
+
+    _setupPanHandlers: function() {
+      var mousedown = JX.bind(this, this._onpanmousedown);
+      var mousemove = JX.bind(this, this._onpanmousemove);
+      var mouseup = JX.bind(this, this._onpanmouseup);
+
+      JX.Stratcom.listen('mousedown', 'workboard-shadow', mousedown);
+      JX.Stratcom.listen('mousemove', null, mousemove);
+      JX.Stratcom.listen('mouseup', null, mouseup);
+    },
+
+    _onpanmousedown: function(e) {
+      if (!JX.Device.isDesktop()) {
+        return;
+      }
+
+      if (e.getNode('workpanel')) {
+        return;
+      }
+
+      if (JX.Stratcom.pass()) {
+        return;
+      }
+
+      e.kill();
+
+      this._panOrigin = JX.$V(e);
+      this._panNode = e.getNode('workboard-shadow');
+      this._panX = this._panNode.scrollLeft;
+    },
+
+    _onpanmousemove: function(e) {
+      if (!this._panOrigin) {
+        return;
+      }
+
+      var cursor = JX.$V(e);
+      this._panNode.scrollLeft = this._panX + (this._panOrigin.x - cursor.x);
+    },
+
+    _onpanmouseup: function() {
+      this._panOrigin = null;
     }
+
   }
 
 });
