735b722cb25690c53c42b0cac340850898fd3efa
epriestley <git@epriestley.com>
2016-02-09 04:17:35 -0800

    Fix a bad parameter in a parent::shouldHideForMail() call in Maniphest
    Fixes T10303.
    
    Auditors: chad

--- src/applications/maniphest/storage/ManiphestTransaction.php
+++ src/applications/maniphest/storage/ManiphestTransaction.php
@@ -182,7 +182,7 @@ final class ManiphestTransaction
         return true;
     }
 
-    return parent::shouldHideForMail();
+    return parent::shouldHideForMail($xactions);
   }
 
   public function shouldHideForFeed() {
