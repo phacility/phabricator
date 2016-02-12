7e94d2f80819fcc8c6fe7785274774b8fd6237ca
epriestley <git@epriestley.com>
2016-02-11 17:32:44 -0800

    Permit users to touch `maniphest.points`
    Summary: Ref T4427. Seems fine / not egregiously broken.
    
    Test Plan: Edited points configuration. Tried to set a bad value. Set a good value. Persued examples and help text.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T4427
    
    Differential Revision: https://secure.phabricator.com/D15256

--- src/applications/maniphest/constants/ManiphestTaskPoints.php
+++ src/applications/maniphest/constants/ManiphestTaskPoints.php
@@ -21,4 +21,21 @@ final class ManiphestTaskPoints extends Phobject {
     return PhabricatorEnv::getEnvConfig('maniphest.points');
   }
 
+  public static function validateConfiguration($config) {
+    if (!is_array($config)) {
+      throw new Exception(
+        pht(
+          'Configuration is not valid. Maniphest points configuration must '.
+          'be a dictionary.'));
+    }
+
+    PhutilTypeSpec::checkMap(
+      $config,
+      array(
+        'enabled' => 'optional bool',
+        'label' => 'optional string',
+        'action' => 'optional string',
+      ));
+  }
+
 }
