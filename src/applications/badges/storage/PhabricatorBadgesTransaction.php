e6d2e66ea250a78db5f056253dcba65240ba62e8
lkassianik <lyubakassianik@gmail.com>
2016-03-26 19:36:13 -0700

    Adding basic transaction titles to awarding/revoking badges
    Summary: Ref T10677, awarding/revoking a badge should create timeline entries with titles that are more clear (excludes homepage feed stories)
    
    Test Plan: Award/revoke a badge to single or multiple users. See timeline entries that reflect those actions.
    
    Reviewers: epriestley, #blessed_reviewers
    
    Reviewed By: epriestley, #blessed_reviewers
    
    Subscribers: Korvin
    
    Maniphest Tasks: T10677
    
    Differential Revision: https://secure.phabricator.com/D15533

--- src/applications/badges/storage/PhabricatorBadgesTransaction.php
+++ src/applications/badges/storage/PhabricatorBadgesTransaction.php
@@ -109,6 +109,26 @@ final class PhabricatorBadgesTransaction
             $qual_new);
         }
         break;
+      case self::TYPE_AWARD:
+        if (!is_array($new)) {
+          $new = array();
+        }
+        $handles = $this->renderHandleList($new);
+        return pht(
+          '%s awarded this badge to %s recipient(s): %s.',
+          $this->renderHandleLink($author_phid),
+          new PhutilNumber(count($new)),
+          $handles);
+      case self::TYPE_REVOKE:
+        if (!is_array($new)) {
+          $new = array();
+        }
+        $handles = $this->renderHandleList($new);
+        return pht(
+          '%s revoked this badge from %s recipient(s): %s.',
+          $this->renderHandleLink($author_phid),
+          new PhutilNumber(count($new)),
+          $handles);
     }
 
     return parent::getTitle();
@@ -222,5 +242,25 @@ final class PhabricatorBadgesTransaction
       $viewer,
       $this->getOldValue(),
       $this->getNewValue());
+  }
+
+  public function getRequiredHandlePHIDs() {
+    $phids = parent::getRequiredHandlePHIDs();
+
+    $type = $this->getTransactionType();
+    switch ($type) {
+       case self::TYPE_AWARD:
+       case self::TYPE_REVOKE:
+        $new = $this->getNewValue();
+        if (!is_array($new)) {
+          $new = array();
+        }
+        foreach ($new as $phid) {
+          $phids[] = $phid;
+        }
+        break;
+    }
+
+    return $phids;
   }
 }
