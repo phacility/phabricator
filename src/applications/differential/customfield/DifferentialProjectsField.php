phacility/phabricator

commit 091a64e91bf6242cb7e76a671980fb8e31e5babd

Rename Differential field Projects to Tags
Summary: Users can't find the "Tags" field in the Edit Menu; Added keyword "Tag".

Test Plan: Looked in Edit page; I think this shouldn't change anything else?

Reviewers: #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: Korvin

Differential Revision: https://secure.phabricator.com/D15749

src/applications/differential/customfield/DifferentialProjectsField.php
@@ -8,7 +8,7 @@ final class DifferentialProjectsField
   }
 
   public function getFieldName() {
-    return pht('Projects');
+    return pht('Tags');
   }
 
   public function getFieldDescription() {

commit 1344dda75692d93490cad242920b3f967ffaefcd

Parse Tags in commits message for revisions
Summary: This will stop breaking if you have subscribers and tags when updating a revision (`Error parsing field "Subscribers": The objects you have listed include objects which do not exist (Tags:)`), which I broke in D15749.

Test Plan: run through arc-diff --update that failed earlier.

Reviewers: chad, #blessed_reviewers, epriestley

Reviewed By: #blessed_reviewers, epriestley

Subscribers: Korvin

Differential Revision: https://secure.phabricator.com/D15762

src/applications/differential/customfield/DifferentialProjectsField.php
@@ -76,6 +76,7 @@ final class DifferentialProjectsField
 
   public function getCommitMessageLabels() {
     return array(
+      'Tags',
       'Project',
       'Projects',
     );