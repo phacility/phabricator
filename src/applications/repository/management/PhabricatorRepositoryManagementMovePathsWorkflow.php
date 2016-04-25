550a82d438f72477f7efc39b65906c2e7ad09a28
epriestley <git@epriestley.com>
2016-04-25 12:22:25 -0700

    Fix two minor formatting issues with `bin/repository move-paths`
    Summary: This gets over-escaped instead of bolded right now, but I only ever hit it when exporting/importing and never both cleaning it up.
    
    Test Plan: Ran `bin/repository move-paths`, saw bolded "Move" instead of ANSI escape sequences.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Differential Revision: https://secure.phabricator.com/D15797

--- src/applications/repository/management/PhabricatorRepositoryManagementMovePathsWorkflow.php
+++ src/applications/repository/management/PhabricatorRepositoryManagementMovePathsWorkflow.php
@@ -66,7 +66,7 @@ final class PhabricatorRepositoryManagementMovePathsWorkflow
       } else {
         $dst = $to.substr($src, strlen($from));
 
-        $row['action'] = phutil_console_format('**%s**', pht('Move'));
+        $row['action'] = tsprintf('**%s**', pht('Move'));
         $row['dst'] = $dst;
         $row['move'] = true;
         $any_changes = true;
@@ -94,7 +94,7 @@ final class PhabricatorRepositoryManagementMovePathsWorkflow
       ->addColumn(
         'dst',
         array(
-          'title' => pht('dst'),
+          'title' => pht('Dst'),
         ))
       ->setBorders(true);
 
