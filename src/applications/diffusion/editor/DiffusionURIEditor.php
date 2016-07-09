phacility/phabricator

commit 921d56efb0f0fbbcb975bf1845b094dc32dcd760

Make repository URI creation work regardless of "repository" transaction order
Summary: Fixes T11276. This feels slightly iffy (we `attachRepository()` here, and also when applying the TYPE_REPOSITORY transaction) but simpler than trying to reorder things.

Test Plan: Created a repository URI with transactions in `["uri", "repository"]` order.

Reviewers: chad, avivey

Reviewed By: avivey

Maniphest Tasks: T11276

Differential Revision: https://secure.phabricator.com/D16237

src/applications/diffusion/editor/DiffusionURIEditor.php
@@ -77,6 +77,11 @@ final class DiffusionURIEditor
           $old_uri = $object->getEffectiveURI();
         } else {
           $old_uri = null;
+
+          // When creating a URI, we may not have processed the repository
+          // transaction yet. Attach the repository here to make sure we
+          // have it for the calls below.
+          $object->attachRepository($this->repository);
         }
 
         $object->setURI($xaction->getNewValue());

commit 46ceba728fee8a775e2ddf0cdae332a0679413a4

(stable) Fix an issue with creating new Repository URIs via the Web UI
Summary I broke this in D16237: that made the CLI workflow work, but we attach the repository earlier in the web workflow and won't have one when we arrive here.

Test Plan: Created a new repository URI from the web UI.

Auditors: chad

src/applications/diffusion/editor/DiffusionURIEditor.php
@@ -78,10 +78,12 @@ final class DiffusionURIEditor
         } else {
           $old_uri = null;
 
-          // When creating a URI, we may not have processed the repository
-          // transaction yet. Attach the repository here to make sure we
-          // have it for the calls below.
-          $object->attachRepository($this->repository);
+          // When creating a URI via the API, we may not have processed the
+          // repository transaction yet. Attach the repository here to make
+          // sure we have it for the calls below.
+          if ($this->repository) {
+            $object->attachRepository($this->repository);
+          }
         }
 
         $object->setURI($xaction->getNewValue());