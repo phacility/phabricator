f379858874a3336dc36cc8b7e77b0d0b698b2fff
Chad Little <chad@phacility.com>
2016-08-19 12:49:38 -0700

    Add setBackground to PHUIIconView
    Summary: Ref T11132. Adds a background color option to PHUIIconView, for use whereever, and NUX. Also normalize icon placement for mixed image/icon result list.
    
    Test Plan: Test in UIExamples, and Global Settings.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Maniphest Tasks: T11132
    
    Differential Revision: https://secure.phabricator.com/D16424

--- src/applications/settings/query/PhabricatorUserPreferencesSearchEngine.php
+++ src/applications/settings/query/PhabricatorUserPreferencesSearchEngine.php
@@ -62,11 +62,14 @@ final class PhabricatorUserPreferencesSearchEngine
       ->setViewer($viewer);
     foreach ($settings as $setting) {
 
+      $icon = id(new PHUIIconView())
+        ->setIcon('fa-globe')
+        ->setBackground('bg-sky');
+
       $item = id(new PHUIObjectItemView())
         ->setHeader($setting->getDisplayName())
         ->setHref($setting->getEditURI())
-        // TODO: Replace this with NUX Style bg / white icon when built
-        ->setImageIcon('fa-globe')
+        ->setImageIcon($icon)
         ->addAttribute(pht('Edit global default settings for all users.'));
 
       $list->addItem($item);
