0fc05ab47edd451969c025a09661ac71f5c5a708
Josh Cox <joshcox@uberatc.com>
2016-09-27 02:15:59 -0400

    Link to badge view from people profile view
    Summary: Fixes T10715. Badges on the profile view now link to the badge view
    
    Test Plan: Went to the profile view and clicked the link.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: epriestley, yelirekim
    
    Maniphest Tasks: T10715
    
    Differential Revision: https://secure.phabricator.com/D16604

--- src/applications/people/controller/PhabricatorPeopleProfileViewController.php
+++ src/applications/people/controller/PhabricatorPeopleProfileViewController.php
@@ -293,6 +293,7 @@ final class PhabricatorPeopleProfileViewController
             ->setHeader($badge->getName())
             ->setSubhead($badge->getFlavor())
             ->setQuality($badge->getQuality())
+            ->setHref($badge->getViewURI())
             ->addByLine($awarder_info);
 
           $flex->addItem($item);
