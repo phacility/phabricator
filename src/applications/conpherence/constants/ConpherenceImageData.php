75fe750ee3671170c6c0e7dc8fbeeb50d053edc8
Chad Little <chad@phacility.com>
2016-10-03 13:39:39 -0700

    Allow Conpherence room images up to 200px
    Summary: Provide higher resolution for Conpherence room images. Fixes T11728
    
    Test Plan:
    Upload a new photo, see it pulls in 200px image as background.
    
    {F1858660}
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Maniphest Tasks: T11728
    
    Differential Revision: https://secure.phabricator.com/D16659

--- src/applications/conpherence/constants/ConpherenceImageData.php
+++ src/applications/conpherence/constants/ConpherenceImageData.php
@@ -5,7 +5,7 @@ final class ConpherenceImageData extends ConpherenceConstants {
   const SIZE_ORIG = 'original';
   const SIZE_CROP = 'crop';
 
-  const CROP_WIDTH = 35;
-  const CROP_HEIGHT = 35;
+  const CROP_WIDTH = 200;
+  const CROP_HEIGHT = 200;
 
 }
