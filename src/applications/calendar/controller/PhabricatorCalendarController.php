ff97ed21959904cce16a4a071049683d1f9de71b
epriestley <git@epriestley.com>
2016-10-06 14:46:50 -0700

    Document how to export Calendar events
    Summary:
    Ref T10747. This explains how exports work.
    
    Also make mail exports use the same logic as other stuff.
    
    Test Plan: Read documentation. Did some exports.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T10747
    
    Differential Revision: https://secure.phabricator.com/D16680

--- src/applications/calendar/controller/PhabricatorCalendarController.php
+++ src/applications/calendar/controller/PhabricatorCalendarController.php
@@ -6,34 +6,11 @@ abstract class PhabricatorCalendarController extends PhabricatorController {
     PhabricatorUser $viewer,
     $file_name,
     array $events) {
-    $events = mpull($events, null, 'getPHID');
 
-    if ($events) {
-      $child_map = id(new PhabricatorCalendarEventQuery())
-        ->setViewer($viewer)
-        ->withParentEventPHIDs(array_keys($events))
-        ->execute();
-      $child_map = mpull($child_map, null, 'getPHID');
-    } else {
-      $child_map = array();
-    }
-
-    $all_events = $events + $child_map;
-    $child_groups = mgroup($child_map, 'getInstanceOfEventPHID');
-
-    $document_node = new PhutilCalendarDocumentNode();
-
-    foreach ($all_events as $event) {
-      $child_events = idx($child_groups, $event->getPHID(), array());
-      $event_node = $event->newIntermediateEventNode($viewer, $child_events);
-      $document_node->appendChild($event_node);
-    }
-
-    $root_node = id(new PhutilCalendarRootNode())
-      ->appendChild($document_node);
-
-    $ics_data = id(new PhutilICSWriter())
-      ->writeICSDocument($root_node);
+    $ics_data = id(new PhabricatorCalendarICSWriter())
+      ->setViewer($viewer)
+      ->setEvents($events)
+      ->writeICSDocument();
 
     return id(new AphrontFileResponse())
       ->setDownload($file_name)
