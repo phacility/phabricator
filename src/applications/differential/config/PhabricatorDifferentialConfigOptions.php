93c0ffd02c82f903f9c8989fcf78e1433d67ea0c
epriestley <git@epriestley.com>
2016-12-16 06:52:50 -0800

    Remove "Child Revisions" custom field in Differential
    Summary: Ref T11114. This was obsoleted by the "Stack" graph and does nothing.
    
    Test Plan: Viewed revisions, still saw dependency graphs.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T11114
    
    Differential Revision: https://secure.phabricator.com/D17072

--- src/applications/differential/config/PhabricatorDifferentialConfigOptions.php
+++ src/applications/differential/config/PhabricatorDifferentialConfigOptions.php
@@ -40,7 +40,6 @@ final class PhabricatorDifferentialConfigOptions
       new DifferentialEditPolicyField(),
 
       new DifferentialParentRevisionsField(),
-      new DifferentialChildRevisionsField(),
       new DifferentialManiphestTasksField(),
       new DifferentialCommitsField(),
 
