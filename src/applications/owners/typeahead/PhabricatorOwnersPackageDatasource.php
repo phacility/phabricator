10171e21013c4f50580598f67796e193dafd2208
epriestley <git@epriestley.com>
2017-01-04 14:40:09 -0800

    Allow "O42" to find packages by monogram in Owners typeaheads
    Summary: When a user queries by package monogram explicitly, search by package ID.
    
    Test Plan: {F2305075}
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Differential Revision: https://secure.phabricator.com/D17142

--- src/applications/owners/typeahead/PhabricatorOwnersPackageDatasource.php
+++ src/applications/owners/typeahead/PhabricatorOwnersPackageDatasource.php
@@ -22,8 +22,17 @@ final class PhabricatorOwnersPackageDatasource
     $results = array();
 
     $query = id(new PhabricatorOwnersPackageQuery())
-      ->withNameNgrams($raw_query)
       ->setOrder('name');
+
+    // If the user is querying by monogram explicitly, like "O123", do an ID
+    // search. Otherwise, do an ngram substring search.
+    if (preg_match('/^[oO]\d+\z/', $raw_query)) {
+      $id = trim($raw_query, 'oO');
+      $id = (int)$id;
+      $query->withIDs(array($id));
+    } else {
+      $query->withNameNgrams($raw_query);
+    }
 
     $packages = $this->executeQuery($query);
     foreach ($packages as $package) {
