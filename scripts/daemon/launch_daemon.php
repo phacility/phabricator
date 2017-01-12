e4f49f08065748d3ee62465513e4265edb72fae8
epriestley <git@epriestley.com>
2017-01-12 15:50:04 -0800

    When available, use async_signals in Phabricator
    Summary: Ref T9640. See D17200 for the analogous change in libphutil.
    
    Test Plan: See D17200.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T9640
    
    Differential Revision: https://secure.phabricator.com/D17201

--- scripts/daemon/launch_daemon.php
+++ scripts/daemon/launch_daemon.php
@@ -5,7 +5,11 @@
 // script, except it loads the Phabricator environment and adds some Phabricator
 // specific flags.
 
-declare(ticks = 1);
+if (function_exists('pcntl_async_signals')) {
+  pcntl_async_signals(true);
+} else {
+  declare(ticks = 1);
+}
 
 $root = dirname(dirname(dirname(__FILE__)));
 require_once $root.'/scripts/__init_script__.php';
