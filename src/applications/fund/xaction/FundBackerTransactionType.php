a9604c4604b739f28a5d89e823a5db5be87e37b4
Chad Little <chad@phacility.com>
2017-05-05 11:02:54 -0700

    Update Fund for modular transactions
    Summary: Fixes T12627. Updates FundInitiative and FundBacker with modular transactions.
    
    Test Plan: Create an Initiative, back it with fake monies, close initiative, reopen, edit various fields.
    
    Reviewers: epriestley
    
    Reviewed By: epriestley
    
    Subscribers: Korvin
    
    Maniphest Tasks: T12627
    
    Differential Revision: https://secure.phabricator.com/D17782

---
+++ src/applications/fund/xaction/FundBackerTransactionType.php
@@ -0,0 +1,4 @@
+<?php
+
+abstract class FundBackerTransactionType
+  extends PhabricatorModularTransactionType {}
---
+++ src/applications/fund/xaction/FundInitiativeNameTransaction.php
@@ -0,0 +1,71 @@
+<?php
+
+final class FundInitiativeNameTransaction
+  extends FundInitiativeTransactionType {
+
+  const TRANSACTIONTYPE = 'fund:name';
+
+  public function generateOldValue($object) {
+    return $object->getName();
+  }
+
+  public function applyInternalEffects($object, $value) {
+    $object->setName($value);
+  }
+
+  public function getTitle() {
+    $old = $this->getOldValue();
+    if (!strlen($old)) {
+      return pht(
+        '%s created this initiative.',
+        $this->renderAuthor());
+    } else {
+      return pht(
+        '%s renamed this initiative from %s to %s.',
+        $this->renderAuthor(),
+        $this->renderOldValue(),
+        $this->renderNewValue());
+    }
+  }
+
+  public function getTitleForFeed() {
+    $old = $this->getOldValue();
+    if (!strlen($old)) {
+      return pht(
+        '%s created initiative %s.',
+        $this->renderAuthor(),
+        $this->renderObject());
+    } else {
+      return pht(
+        '%s renamed %s initiative from %s to %s.',
+        $this->renderAuthor(),
+        $this->renderObject(),
+        $this->renderOldValue(),
+        $this->renderNewValue());
+    }
+  }
+
+  public function validateTransactions($object, array $xactions) {
+    $errors = array();
+
+    if ($this->isEmptyTextTransaction($object->getName(), $xactions)) {
+      $errors[] = $this->newRequiredError(
+        pht('Initiatives must have a name.'));
+    }
+
+    $max_length = $object->getColumnMaximumByteLength('name');
+    foreach ($xactions as $xaction) {
+      $new_value = $xaction->getNewValue();
+      $new_length = strlen($new_value);
+      if ($new_length > $max_length) {
+        $errors[] = $this->newInvalidError(
+          pht('The name can be no longer than %s characters.',
+          new PhutilNumber($max_length)));
+      }
+    }
+
+    return $errors;
+  }
+
+
+}
