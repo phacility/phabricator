0a87881e9813ac5d3662ab13577f04ae2152cf3e
Chad Little <chad@phacility.com>
2017-05-05 19:50:05 -0700

    Fix file attach bug in Macro
    Summary: This was mis-tested by only using one account, which could always see the image. External transaction moved file attachment to the modular transaction for file and audio instead.
    
    Test Plan: Test adding audio and a macro on a pleb account, visit with normal account and see macro fine.
    
    Reviewers: epriestley, amckinley
    
    Reviewed By: amckinley
    
    Subscribers: Korvin
    
    Differential Revision: https://secure.phabricator.com/D17836

--- src/applications/macro/editor/PhabricatorMacroEditor.php
+++ src/applications/macro/editor/PhabricatorMacroEditor.php
@@ -19,44 +19,6 @@ final class PhabricatorMacroEditor
     return pht('%s created %s.', $author, $object);
   }
 
-  protected function applyCustomExternalTransaction(
-    PhabricatorLiskDAO $object,
-    PhabricatorApplicationTransaction $xaction) {
-
-    switch ($xaction->getTransactionType()) {
-      case PhabricatorMacroFileTransaction::TRANSACTIONTYPE:
-      case PhabricatorMacroAudioTransaction::TRANSACTIONTYPE:
-        // When changing a macro's image or audio, attach the underlying files
-        // to the macro (and detach the old files).
-        $old = $xaction->getOldValue();
-        $new = $xaction->getNewValue();
-        $all = array();
-        if ($old) {
-          $all[] = $old;
-        }
-        if ($new) {
-          $all[] = $new;
-        }
-
-        $files = id(new PhabricatorFileQuery())
-          ->setViewer($this->requireActor())
-          ->withPHIDs($all)
-          ->execute();
-        $files = mpull($files, null, 'getPHID');
-
-        $old_file = idx($files, $old);
-        if ($old_file) {
-          $old_file->detachFromObject($object->getPHID());
-        }
-
-        $new_file = idx($files, $new);
-        if ($new_file) {
-          $new_file->attachToObject($object->getPHID());
-        }
-        break;
-    }
-  }
-
   protected function shouldSendMail(
     PhabricatorLiskDAO $object,
     array $xactions) {
