404fe482d91492308dc839b5635de579b17dff5b
epriestley <git@epriestley.com>
2017-05-08 07:58:02 -0700

    Add a Quicksand-aware page-level container to diffs
    Summary:
    Ref T12616. Ref T8047. Ref T11093. We currently have several bugs where diff state sticks across Quicksand pages.
    
    Add a new top-level object to handle this, with `sleep()` and `wake()` methods. In `sleep()`, future changes will remove/deacivate all the reticles/editors/etc.
    
    See T12616 for high-level discussion of plans here.
    
    This general idea is likely to become more formal eventually (e.g. for "sheets" or whatever we call them, in T10469) but I think this is probably a reasonable place to draw a line for now.
    
    Test Plan:
      - Added some logging to sleep(), wake() and construct().
      - Viewed changes in Differential.
      - With Quicksand on, browsed around; saw state change logs fire properly.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T12616, T11093, T8047
    
    Differential Revision: https://secure.phabricator.com/D17840

---
+++ webroot/rsrc/js/application/diff/DiffChangesetList.js
@@ -0,0 +1,25 @@
+/**
+ * @provides phabricator-diff-changeset-list
+ * @requires javelin-install
+ * @javelin
+ */
+
+JX.install('DiffChangesetList', {
+
+  construct: function() {
+
+  },
+
+  members: {
+
+    sleep: function() {
+
+    },
+
+    wake: function() {
+
+    }
+
+  }
+
+});
--- webroot/rsrc/js/application/differential/behavior-populate.js
+++ webroot/rsrc/js/application/differential/behavior-populate.js
@@ -5,9 +5,67 @@
  *           javelin-stratcom
  *           phabricator-tooltip
  *           changeset-view-manager
+ *           phabricator-diff-changeset-list
+ * @javelin
  */
 
-JX.behavior('differential-populate', function(config) {
+JX.behavior('differential-populate', function(config, statics) {
+
+  // When we perform a Quicksand navigation, deactivate the changeset lists on
+  // the current page and activate the changeset lists on the new page.
+  var onredraw = function(page_id) {
+    // If the current page is already active, we don't need to do anything.
+    if (statics.pageID === page_id) {
+      return;
+    }
+
+    var ii;
+
+    // Put the old lists to sleep.
+    var old_lists = get_lists(statics.pageID);
+    for (ii = 0; ii < old_lists.length; ii++) {
+      old_lists[ii].sleep();
+    }
+    statics.pageID = null;
+
+    // Awaken the new lists, if they exist.
+    if (statics.pages.hasOwnProperty(page_id)) {
+      var new_lists = get_lists(page_id);
+      for (ii = 0; ii < new_lists.length; ii++) {
+        new_lists[ii].wake();
+      }
+
+      statics.pageID = page_id;
+    }
+  };
+
+  // Get changeset lists on the current page.
+  var get_lists = function(page_id) {
+    if (page_id === null) {
+      return [];
+    }
+
+    return statics.pages[page_id] || [];
+  };
+
+  if (!statics.installed) {
+    statics.installed = true;
+    statics.pages = {};
+    statics.pageID = null;
+
+    JX.Stratcom.listen('quicksand-redraw', null, function(e) {
+      onredraw(e.getData().newResponseID);
+    });
+  }
+
+  var changeset_list = new JX.DiffChangesetList();
+
+  // Install and activate the current page.
+  var page_id = JX.Quicksand.getCurrentPageID();
+  statics.pages[page_id] = [changeset_list];
+  onredraw(page_id);
+
+
 
   for (var ii = 0; ii < config.changesetViewIDs.length; ii++) {
     var id = config.changesetViewIDs[ii];
