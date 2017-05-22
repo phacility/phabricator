5b035af6ffe7576e13fc3dfa1d3cc274c4739e2b
epriestley <git@epriestley.com>
2017-05-22 10:29:14 -0700

    Convert Nuance queues to modular transactions
    Summary:
    Ref T12738. Swaps queues over.
    
    Also fixes a typo from D17990.
    
    Test Plan: Renamed a queue.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T12738
    
    Differential Revision: https://secure.phabricator.com/D17992

--- src/applications/nuance/storage/NuanceQueueTransaction.php
+++ src/applications/nuance/storage/NuanceQueueTransaction.php
@@ -2,8 +2,6 @@
 
 final class NuanceQueueTransaction extends NuanceTransaction {
 
-  const TYPE_NAME = 'nuance.queue.name';
-
   public function getApplicationTransactionType() {
     return NuanceQueuePHIDType::TYPECONST;
   }
@@ -13,29 +11,7 @@ final class NuanceQueueTransaction extends NuanceTransaction {
   }
 
   public function getBaseTransactionClass() {
-    return 'NuanceSourceTransactionType';
+    return 'NuanceQueueTransactionType';
   }
 
-  public function getTitle() {
-    $old = $this->getOldValue();
-    $new = $this->getNewValue();
-    $type = $this->getTransactionType();
-
-    $author_phid = $this->getAuthorPHID();
-
-    switch ($type) {
-      case PhabricatorTransactions::TYPE_CREATE:
-        return pht(
-          '%s created this queue.',
-          $this->renderHandleLink($author_phid));
-      case self::TYPE_NAME:
-        return pht(
-          '%s renamed this queue from "%s" to "%s".',
-          $this->renderHandleLink($author_phid),
-          $old,
-          $new);
-    }
-
-    return parent::getTitle();
-  }
 }
