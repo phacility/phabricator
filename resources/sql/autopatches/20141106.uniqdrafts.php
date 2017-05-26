04fd93e51e0368543773b72ed2dfef09d94080aa
Austin McKinley <austin@phacility.com>
2017-05-26 13:47:02 -0700

    Drop DifferentialDraft storage
    Summary: Fixes T12104.
    
    Test Plan: Ran `bin/storage upgrade` and observed table dun got dropped.
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: epriestley
    
    Maniphest Tasks: T12104
    
    Differential Revision: https://secure.phabricator.com/D18034

--- resources/sql/autopatches/20141106.uniqdrafts.php
+++ resources/sql/autopatches/20141106.uniqdrafts.php
@@ -1,30 +1,3 @@
 <?php
 
-// Destroy duplicate drafts before storage adjustment adds a unique key to this
-// table. See T1191. We retain the newest draft.
-
-// (We can't easily do this in a single SQL statement because MySQL won't let us
-// modify a table that's joined in a subquery.)
-
-$table = new DifferentialDraft();
-$conn_w = $table->establishConnection('w');
-
-$duplicates = queryfx_all(
-  $conn_w,
-  'SELECT DISTINCT u.id id FROM %T u
-    JOIN %T v
-      ON u.objectPHID = v.objectPHID
-      AND u.authorPHID = v.authorPHID
-      AND u.draftKey = v.draftKey
-      AND u.id < v.id',
-  $table->getTableName(),
-  $table->getTableName());
-
-$duplicates = ipull($duplicates, 'id');
-foreach (PhabricatorLiskDAO::chunkSQL($duplicates) as $chunk) {
-  queryfx(
-    $conn_w,
-    'DELETE FROM %T WHERE id IN (%Q)',
-    $table->getTableName(),
-    $chunk);
-}
+// This table has been removed; see T12104 for details.
