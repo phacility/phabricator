ab018e1b49d52025c69aa359061fd3f34f417eda
epriestley <git@epriestley.com>
2017-08-01 08:48:39 -0700

    When destorying a repository, print a notification about removing the working copy
    Summary:
    Fixes T12946. `bin/remove destroy` does not remove working copies: it's more dangerous than usual, and we can't do it in the general (clustered) case.
    
    Print a notification message after destroying a repository.
    
    Test Plan:
      - Destroyed a repository, got a hint about the working copy.
      - Destroyed a task, things worked normally.
    
    Reviewers: chad
    
    Reviewed By: chad
    
    Maniphest Tasks: T12946
    
    Differential Revision: https://secure.phabricator.com/D18313

---
+++ src/applications/system/interface/PhabricatorDestructibleCodexInterface.php
@@ -0,0 +1,18 @@
+<?php
+
+interface PhabricatorDestructibleCodexInterface {
+
+  public function newDestructibleCodex();
+
+}
+
+// TEMPLATE IMPLEMENTATION /////////////////////////////////////////////////////
+
+/* -(  PhabricatorDestructibleCodexInterface  )------------------------------ */
+/*
+
+  public function newDestructibleCodex() {
+    return new <<...>>DestructibleCodex();
+  }
+
+*/
