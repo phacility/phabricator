84cf4938798473a4e0c370702fae56d24116052f
epriestley <git@epriestley.com>
2017-12-23 08:36:58 -0800

    Allow "Manage" to be the default menu item for projects
    Summary:
    Fixes T13033. Currently (prior to D18836) all the default items for projects can be hidden. When this occurs, the main project page fatals.
    
    If we fix the fatal narrowly (don't try to call `null->getBuiltinKey()` when `$default` is `null`), it would 404, which is a little better but not by much.
    
    After D18836 you can't hide "Profile", which is pretty sensible, and effectively fixes this. This change doubles down: let "Manage" be a default, and send the user there if we can't find a different default.
    
    Ideally, the MenuEngine itself will do more rendering eventually (as it does for some of the newer Home stuff) and could handle this defaulting behavior with less special casing, but we'd still end up in a similar situation if a project had only one "Link" item to "coolcats.com" or something: redirecting the user to "coolcats.com" is probably better than fataling, but not by a huge margin, and not likely to be what they expect.
    
    Test Plan:
    Before D18836, disabled both "Profile" and "Workboard" items on a project. Visited project page.
    
    Before patch: fatal. After patch: manage page.
    
    After D18836, you can't do this and just get the profile, so this is sort of moot and mostly future-proofing/for-completeness.
    
    Reviewers: 20after4, amckinley
    
    Reviewed By: amckinley
    
    Maniphest Tasks: T13033
    
    Differential Revision: https://secure.phabricator.com/D18843

--- src/applications/project/controller/PhabricatorProjectViewController.php
+++ src/applications/project/controller/PhabricatorProjectViewController.php
@@ -20,12 +20,23 @@ final class PhabricatorProjectViewController
     $engine = $this->getProfileMenuEngine();
     $default = $engine->getDefaultItem();
 
+    // If defaults are broken somehow, serve the manage page. See T13033 for
+    // discussion.
+    if ($default) {
+      $default_key = $default->getBuiltinKey();
+    } else {
+      $default_key = PhabricatorProject::ITEM_MANAGE;
+    }
+
     switch ($default->getBuiltinKey()) {
       case PhabricatorProject::ITEM_WORKBOARD:
         $controller_object = new PhabricatorProjectBoardViewController();
         break;
       case PhabricatorProject::ITEM_PROFILE:
         $controller_object = new PhabricatorProjectProfileController();
+        break;
+      case PhabricatorProject::ITEM_MANAGE:
+        $controller_object = new PhabricatorProjectManageController();
         break;
       default:
         return $engine->buildResponse();
