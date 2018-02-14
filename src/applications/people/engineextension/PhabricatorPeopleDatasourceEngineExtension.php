4bccb1547d6bfe9c4eaa1f5dea93994d488dd26c
epriestley <git@epriestley.com>
2018-02-14 15:49:00 -0800

    Modularize the "jump nav" behaviors in global search
    Summary: Depends on D19087. Ref T13079. This still doesn't feel like the most clean, general system in the world, but is a step forward from hard-coded `switch()` stuff.
    
    Test Plan:
    - Jumped to `r`.
    - Jumped to `a`.
    - Jumped to `r poe` (multiple results).
    - Jumped to `r poetry` (one result).
    - Jumped to `r syzygy` (no results).
    - Jumped to `p`.
    - Jumped to `p robot` (multiple results); `p assessment` (one result).
      - The behavior for `p <string>` has changed slightly but should be more powerful now (it's consistent with `r <string>`).
    - Jumped to `s <symbol>` and `s <context>-><symbol>`.
    - Jumped to `d`.
    - Jumped to `f`.
    - Jumped to `t`.
    - Jumped to `T123`, `D123`, `@dog`, `PHID-DREV-abcd`, etc.
    
    Maniphest Tasks: T13079
    
    Differential Revision: https://secure.phabricator.com/D19088

--- src/applications/people/engineextension/PhabricatorPeopleDatasourceEngineExtension.php
+++ src/applications/people/engineextension/PhabricatorPeopleDatasourceEngineExtension.php
@@ -8,4 +8,29 @@ final class PhabricatorPeopleDatasourceEngineExtension
       new PhabricatorPeopleDatasource(),
     );
   }
+
+  public function newJumpURI($query) {
+    $viewer = $this->getViewer();
+
+    // Send "u" to the user directory.
+    if (preg_match('/^u\z/i', $query)) {
+      return '/people/';
+    }
+
+    // Send "u <string>" to the user's profile page.
+    $matches = null;
+    if (preg_match('/^u\s+(.+)\z/i', $query, $matches)) {
+      $raw_query = $matches[1];
+
+      // TODO: We could test that this is a valid username and jump to
+      // a search in the user directory if it isn't.
+
+      return urisprintf(
+        '/p/%s/',
+        $raw_query);
+    }
+
+    return null;
+  }
+
 }
--- src/applications/search/engineextension/PhabricatorDatasourceEngineExtension.php
+++ src/applications/search/engineextension/PhabricatorDatasourceEngineExtension.php
@@ -2,12 +2,33 @@
 
 abstract class PhabricatorDatasourceEngineExtension extends Phobject {
 
-  abstract public function newQuickSearchDatasources();
+  private $viewer;
 
-  final public static function getAllQuickSearchDatasources() {
-    $extensions = id(new PhutilClassMapQuery())
+  final public function setViewer(PhabricatorUser $viewer) {
+    $this->viewer = $viewer;
+    return $this;
+  }
+
+  final public function getViewer() {
+    return $this->viewer;
+  }
+
+  public function newQuickSearchDatasources() {
+    return array();
+  }
+
+  public function newJumpURI($query) {
+    return null;
+  }
+
+  final public static function getAllExtensions() {
+    return id(new PhutilClassMapQuery())
       ->setAncestorClass(__CLASS__)
       ->execute();
+  }
+
+  final public static function getAllQuickSearchDatasources() {
+    $extensions = self::getAllExtensions();
 
     $datasources = array();
     foreach ($extensions as $extension) {
