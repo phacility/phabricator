f74e6bbf8d70e957cebb2a47f30f9e3c9c301338
epriestley <git@epriestley.com>
2018-02-14 11:30:25 -0800

    Make "phabricator.silent" disable build steps which rely on external services
    Summary:
    Depends on D19084. Fixes T13078. When `phabricator.silent` is enabled, immediately fail the "HTTP Request", "CircleCI" and "Buildkite" build steps.
    
    This doesn't feel quite as clean as most of the other behavior of `phabricator.silent`, since these calls are not exactly notifications in the same way that email is, and failing to make these calls means that builds run differently (whereas failing to deliver email doesn't really do anything).
    
    However, I suspect that this behavior is almost always reasonable/correct, and that we can probably get away with it until this grey area between "notifications" and "external service calls" is more clearly defined.
    
    Test Plan:
      - Created a build with HTTP, CircleCI, and Buildkite steps.
      - Put install in `phabricator.silent` mode: all three steps failed with "declining, because silent" messages.
      - Put install back in normal mode: all three steps made HTTP requests.
      - Read updated documentation.
    
    Subscribers: PHID-OPKG-gm6ozazyms6q6i22gyam
    
    Maniphest Tasks: T13078
    
    Differential Revision: https://secure.phabricator.com/D19085

--- src/applications/harbormaster/step/HarbormasterBuildkiteBuildStepImplementation.php
+++ src/applications/harbormaster/step/HarbormasterBuildkiteBuildStepImplementation.php
@@ -72,6 +72,11 @@ EOTEXT
     HarbormasterBuildTarget $build_target) {
     $viewer = PhabricatorUser::getOmnipotentUser();
 
+    if (PhabricatorEnv::getEnvConfig('phabricator.silent')) {
+      $this->logSilencedCall($build, $build_target, pht('Buildkite'));
+      throw new HarbormasterBuildFailureException();
+    }
+
     $buildable = $build->getBuildable();
 
     $object = $buildable->getBuildableObject();
