f713e1dfc160009a7ee0d1b2b73e0f9aff29e7d3
epriestley <git@epriestley.com>
2018-02-16 04:30:48 -0800

    Add Owners Package support for "Commit Hook: Content" Herald rules
    Summary:
    See PHI370. Support the "Affected packages" and "Affected package owners" Herald fields in pre-commit hooks.
    
    I believe there's no technical reason these fields aren't supported and this was just overlooked.
    
    Test Plan: Wrote a rule which makes use of the new fields, pushed commits through it. Checked transcripts and saw sensible-looking values.
    
    Differential Revision: https://secure.phabricator.com/D19104

---
+++ src/applications/diffusion/herald/DiffusionPreCommitContentPackageOwnerHeraldField.php
@@ -0,0 +1,34 @@
+<?php
+
+final class DiffusionPreCommitContentPackageOwnerHeraldField
+  extends DiffusionPreCommitContentHeraldField {
+
+  const FIELDCONST = 'diffusion.pre.content.package.owners';
+
+  public function getHeraldFieldName() {
+    return pht('Affected package owners');
+  }
+
+  public function getFieldGroupKey() {
+    return HeraldRelatedFieldGroup::FIELDGROUPKEY;
+  }
+
+  public function getHeraldFieldValue($object) {
+    $packages = $this->getAdapter()->loadAffectedPackages();
+    if (!$packages) {
+      return array();
+    }
+
+    $owners = PhabricatorOwnersOwner::loadAllForPackages($packages);
+    return mpull($owners, 'getUserPHID');
+  }
+
+  protected function getHeraldFieldStandardType() {
+    return self::STANDARD_PHID_LIST;
+  }
+
+  protected function getDatasource() {
+    return new PhabricatorProjectOrUserDatasource();
+  }
+
+}
