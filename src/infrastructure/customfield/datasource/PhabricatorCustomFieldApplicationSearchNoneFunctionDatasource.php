3203fd9eea7d711418037b1a45b7d49de0ea57d8
epriestley <git@epriestley.com>
2018-02-22 12:23:41 -0800

    Support "Any Value" and "No Value" search constraints for datasource Custom Fields
    Summary: Depends on D19126. Ref T13090. For datasource custom fields, this proxies the datasource and provides "none()" and "any()" functions to allow you to search for objects with no values or any values.
    
    Test Plan:
      - Created a custom "Owning Group" field in Maniphest using a Projects datasource.
      - For a task with no owner assigned, searched for "none()" (hit) and "any()" (miss).
      - Assigned the task to an owning project.
      - Searched for "none()" (miss), "any()" (hit), the project it is now a member of (hit) and some random other project (miss).
    
    Maniphest Tasks: T13090
    
    Differential Revision: https://secure.phabricator.com/D19127

---
+++ src/infrastructure/customfield/datasource/PhabricatorCustomFieldApplicationSearchNoneFunctionDatasource.php
@@ -0,0 +1,72 @@
+<?php
+
+final class PhabricatorCustomFieldApplicationSearchNoneFunctionDatasource
+  extends PhabricatorTypeaheadDatasource {
+
+  public function getBrowseTitle() {
+    return pht('Browse No Value');
+  }
+
+  public function getPlaceholderText() {
+    return pht('Type "none()"...');
+  }
+
+  public function getDatasourceApplicationClass() {
+    return null;
+  }
+
+  public function getDatasourceFunctions() {
+    return array(
+      'none' => array(
+        'name' => pht('No Value'),
+        'summary' => pht('Find results with no value.'),
+        'description' => pht(
+          "This function includes results which have no value. Use a query ".
+          "like this to find results with no value:\n\n%s\n\n",
+          'If you combine this function with other constraints, results '.
+          'which have no value or the specified values will be returned.',
+          '> any()'),
+      ),
+    );
+  }
+
+  public function loadResults() {
+    $results = array(
+      $this->newNoneFunction(),
+    );
+    return $this->filterResultsAgainstTokens($results);
+  }
+
+  protected function evaluateFunction($function, array $argv_list) {
+    $results = array();
+
+    foreach ($argv_list as $argv) {
+      $results[] = new PhabricatorQueryConstraint(
+        PhabricatorQueryConstraint::OPERATOR_NULL,
+        null);
+    }
+
+    return $results;
+  }
+
+  public function renderFunctionTokens($function, array $argv_list) {
+    $results = array();
+    foreach ($argv_list as $argv) {
+      $results[] = PhabricatorTypeaheadTokenView::newFromTypeaheadResult(
+        $this->newNoneFunction());
+    }
+    return $results;
+  }
+
+  private function newNoneFunction() {
+    $name = pht('No Value');
+    return $this->newFunctionResult()
+      ->setName($name.' none')
+      ->setDisplayName($name)
+      ->setIcon('fa-ban')
+      ->setPHID('none()')
+      ->setUnique(true)
+      ->addAttribute(pht('Select results with no value.'));
+  }
+
+}
