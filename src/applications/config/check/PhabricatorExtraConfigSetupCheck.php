a48e6897a4180ec074f4581c677448b2ac400812
epriestley <git@epriestley.com>
2018-08-16 11:07:37 -0700

    Remove obsolete setup check call to Maniphest "Can Edit <X>" field checks
    Summary: Ref T13164. Missed this in D19581.
    
    Test Plan:
      - Forced setup checks to re-run by visiting {nav Config > Setup Issues} explicitly.
      - Before patch: fatal on call to nonexistent method.
      - After patch: setup issues.
    
    Reviewers: amckinley
    
    Reviewed By: amckinley
    
    Maniphest Tasks: T13164
    
    Differential Revision: https://secure.phabricator.com/D19593

--- src/applications/config/check/PhabricatorExtraConfigSetupCheck.php
+++ src/applications/config/check/PhabricatorExtraConfigSetupCheck.php
@@ -84,8 +84,6 @@ final class PhabricatorExtraConfigSetupCheck extends PhabricatorSetupCheck {
         $issue->addPhabricatorConfig($key);
       }
     }
-
-    $this->executeManiphestFieldChecks();
   }
 
   /**
