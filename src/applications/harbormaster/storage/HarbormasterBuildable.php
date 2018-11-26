433a7321ff2585aec3ebbb23f59c0392f7e34e7e
John Linahan <linahan.john@gmail.com>
2018-11-26 14:16:57 +0000

    Add harbormaster.buildable.search API Method
    Summary:
    This revision adds a Conduit search method for buildables. It exposes:
      * `objectPHID`
      * `containerPHID`
      * `buildableStatus`
      * `isManual`
    
    Test Plan:
    Use the API Console to run searches. Example:
    ```
    {
      "data": [
        {
          "id": 2,
          "type": "HMBB",
          "phid": "PHID-HMBB-m4k5lodx6naq22576a7d",
          "fields": {
            "objectPHID": "PHID-DIFF-vzvgqqcyscpd7ta4osy2",
            "containerPHID": "PHID-DREV-vsivs5276c7vtgpmssn2",
            "buildableStatus": {
              "value": "passed"
            },
            "isManual": true,
            "dateCreated": 1542407155,
            "dateModified": 1542407156,
            "policy": {
              "view": "users",
              "edit": "users"
            }
          },
          "attachments": {}
        },
        {
          "id": 1,
          "type": "HMBB",
          "phid": "PHID-HMBB-opxfl4auoz3ey5klplrx",
          "fields": {
            "objectPHID": "PHID-DIFF-vzvgqqcyscpd7ta4osy2",
            "containerPHID": null,
            "buildableStatus": {
              "value": "passed"
            },
            "isManual": false,
            "dateCreated": 1542406968,
            "dateModified": 1542406968,
            "policy": {
              "view": "users",
              "edit": "users"
            }
          },
          "attachments": {}
        }
      ],
      "maps": {},
      "query": {
        "queryKey": null
      },
      "cursor": {
        "limit": 100,
        "after": null,
        "before": null,
        "order": null
      }
    }
    ```
    
    Reviewers: #blessed_reviewers, epriestley
    
    Reviewed By: #blessed_reviewers, epriestley
    
    Subscribers: Korvin, O14 ATC Monitoring
    
    Differential Revision: https://secure.phabricator.com/D19818

--- src/applications/harbormaster/storage/HarbormasterBuildable.php
+++ src/applications/harbormaster/storage/HarbormasterBuildable.php
@@ -6,6 +6,7 @@ final class HarbormasterBuildable
     PhabricatorApplicationTransactionInterface,
     PhabricatorPolicyInterface,
     HarbormasterBuildableInterface,
+    PhabricatorConduitResultInterface,
     PhabricatorDestructibleInterface {
 
   protected $buildablePHID;
@@ -352,6 +353,46 @@ final class HarbormasterBuildable
 
   public function newBuildableEngine() {
     return $this->getBuildableObject()->newBuildableEngine();
+  }
+
+
+/* -(  PhabricatorConduitResultInterface  )---------------------------------- */
+
+
+  public function getFieldSpecificationsForConduit() {
+    return array(
+      id(new PhabricatorConduitSearchFieldSpecification())
+        ->setKey('objectPHID')
+        ->setType('phid')
+        ->setDescription(pht('PHID of the object that is built.')),
+      id(new PhabricatorConduitSearchFieldSpecification())
+        ->setKey('containerPHID')
+        ->setType('phid')
+        ->setDescription(pht('PHID of the object containing this buildable.')),
+      id(new PhabricatorConduitSearchFieldSpecification())
+        ->setKey('buildableStatus')
+        ->setType('map<string, wild>')
+        ->setDescription(pht('The current status of this buildable.')),
+      id(new PhabricatorConduitSearchFieldSpecification())
+        ->setKey('isManual')
+        ->setType('bool')
+        ->setDescription(pht('True if this is a manual buildable.')),
+    );
+  }
+
+  public function getFieldValuesForConduit() {
+    return array(
+      'objectPHID' => $this->getBuildablePHID(),
+      'containerPHID' => $this->getContainerPHID(),
+      'buildableStatus' => array(
+        'value' => $this->getBuildableStatus(),
+      ),
+      'isManual' => (bool)$this->getIsManualBuildable(),
+    );
+  }
+
+  public function getConduitSearchAttachments() {
+    return array();
   }
 
 
