fc3b90e1d1b1cd64866ad9274594072e6c4c2486
epriestley <git@epriestley.com>
2019-02-05 11:43:01 -0800

    Allow users to unlink their last external account with a warning, instead of preventing the action
    Summary:
    Depends on D20105. Fixes T7732. T7732 describes a case where a user had their Google credentials swapped and had trouble regaining access to their account.
    
    Since we now allow email login even if password auth is disabled, it's okay to let users unlink their final account, and it's even reasonable for users to unlink their final account if it is mis-linked.
    
    Just give them a warning that what they're doing is a little sketchy, rather than preventing the workflow.
    
    Test Plan: Unlinked my only login account, got a stern warning instead of a dead end.
    
    Reviewers: amckinley
    
    Reviewed By: amckinley
    
    Maniphest Tasks: T7732
    
    Differential Revision: https://secure.phabricator.com/D20106

--- src/applications/settings/panel/PhabricatorExternalAccountsSettingsPanel.php
+++ src/applications/settings/panel/PhabricatorExternalAccountsSettingsPanel.php
@@ -41,13 +41,6 @@ final class PhabricatorExternalAccountsSettingsPanel
       ->setUser($viewer)
       ->setNoDataString(pht('You have no linked accounts.'));
 
-    $login_accounts = 0;
-    foreach ($accounts as $account) {
-      if ($account->isUsableForLogin()) {
-        $login_accounts++;
-      }
-    }
-
     foreach ($accounts as $account) {
       $item = new PHUIObjectItemView();
 
@@ -71,8 +64,6 @@ final class PhabricatorExternalAccountsSettingsPanel
             'Disabled (an administrator has disabled login for this '.
             'account provider).'));
       }
-
-      $can_unlink = $can_unlink && (!$can_login || ($login_accounts > 1));
 
       $can_refresh = $provider && $provider->shouldAllowAccountRefresh();
       if ($can_refresh) {
