8810cd2f4d1572e536f7aab7e76969bea80fda65
epriestley <git@epriestley.com>
2019-02-13 17:50:10 -0800

    Add a standalone view for the Maniphest task graph
    Summary:
    See PHI1073. Improve the UX here:
    
      - When there are a small number of connected tasks, no changes.
      - When there are too many total connected tasks, but not too many directly connected tasks, show hint text with a "View Standalone Graph" button to view more of the graph.
      - When there are too many directly connected tasks, show better hint text with a "View Standalone Graph" button.
      - Always show a "View Standalone Graph" option in the dropdown menu.
      - Add a standalone view which works the same way but has a limit of 2,000.
        - This view doesn't have "View Standalone Graph" links, since they'd just link back to the same page, but is basically the same otherwise.
      - Increase the main page task limit from 100 to 200.
    
    Test Plan:
    Mobile View:
    
    {F6210326}
    
    Way too much stuff:
    
    {F6210327}
    
    New persistent link to the standalone page:
    
    {F6210328}
    
    Kind of too much stuff:
    
    {F6210329}
    
    Standalone view:
    
    {F6210330}
    
    Reviewers: amckinley
    
    Reviewed By: amckinley
    
    Subscribers: 20after4
    
    Differential Revision: https://secure.phabricator.com/D20164

--- src/applications/maniphest/controller/ManiphestController.php
+++ src/applications/maniphest/controller/ManiphestController.php
@@ -61,4 +61,102 @@ abstract class ManiphestController extends PhabricatorController {
     return $view;
   }
 
+  final protected function newTaskGraphDropdownMenu(
+    ManiphestTask $task,
+    $has_parents,
+    $has_subtasks,
+    $include_standalone) {
+    $viewer = $this->getViewer();
+
+    $parents_uri = urisprintf(
+      '/?subtaskIDs=%d#R',
+      $task->getID());
+    $parents_uri = $this->getApplicationURI($parents_uri);
+
+    $subtasks_uri = urisprintf(
+      '/?parentIDs=%d#R',
+      $task->getID());
+    $subtasks_uri = $this->getApplicationURI($subtasks_uri);
+
+    $dropdown_menu = id(new PhabricatorActionListView())
+      ->setViewer($viewer)
+      ->addAction(
+        id(new PhabricatorActionView())
+          ->setHref($parents_uri)
+          ->setName(pht('Search Parent Tasks'))
+          ->setDisabled(!$has_parents)
+          ->setIcon('fa-chevron-circle-up'))
+      ->addAction(
+        id(new PhabricatorActionView())
+          ->setHref($subtasks_uri)
+          ->setName(pht('Search Subtasks'))
+          ->setDisabled(!$has_subtasks)
+          ->setIcon('fa-chevron-circle-down'));
+
+    if ($include_standalone) {
+      $standalone_uri = urisprintf('/graph/%d/', $task->getID());
+      $standalone_uri = $this->getApplicationURI($standalone_uri);
+
+      $dropdown_menu->addAction(
+        id(new PhabricatorActionView())
+          ->setHref($standalone_uri)
+          ->setName(pht('View Standalone Graph'))
+          ->setIcon('fa-code-fork'));
+    }
+
+    $graph_menu = id(new PHUIButtonView())
+      ->setTag('a')
+      ->setIcon('fa-search')
+      ->setText(pht('Search...'))
+      ->setDropdownMenu($dropdown_menu);
+
+    return $graph_menu;
+  }
+
+  final protected function newTaskGraphOverflowView(
+    ManiphestTask $task,
+    $overflow_message,
+    $include_standalone) {
+
+    $id = $task->getID();
+
+    if ($include_standalone) {
+      $standalone_uri = $this->getApplicationURI("graph/{$id}/");
+
+      $standalone_link = id(new PHUIButtonView())
+        ->setTag('a')
+        ->setHref($standalone_uri)
+        ->setColor(PHUIButtonView::GREY)
+        ->setIcon('fa-code-fork')
+        ->setText(pht('View Standalone Graph'));
+    } else {
+      $standalone_link = null;
+    }
+
+    $standalone_icon = id(new PHUIIconView())
+      ->setIcon('fa-exclamation-triangle', 'yellow')
+      ->addClass('object-graph-header-icon');
+
+    $standalone_view = phutil_tag(
+      'div',
+      array(
+        'class' => 'object-graph-header',
+      ),
+      array(
+        $standalone_link,
+        $standalone_icon,
+        phutil_tag(
+          'div',
+          array(
+            'class' => 'object-graph-header-message',
+          ),
+          array(
+            $overflow_message,
+          )),
+      ));
+
+    return $standalone_view;
+  }
+
+
 }
