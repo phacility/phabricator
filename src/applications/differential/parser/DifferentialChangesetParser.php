befbea2f00e6510a4b72a52c4d82c9d14cdecea0
epriestley <git@epriestley.com>
2020-04-22 20:20:00 -0700

    Don't pass "No newline at end of file." annotations to DocumentEngines as literal diff text
    Summary: See PHI1707, which has a Jupyter notebook which fails to diff nicely when modified. The root cause seems to be that the document does not end in a newline.
    
    Test Plan: Applied patch, diffed the file, got a Jupyter diff out of it.
    
    Differential Revision: https://secure.phabricator.com/D21159

--- src/applications/differential/parser/DifferentialChangesetParser.php
+++ src/applications/differential/parser/DifferentialChangesetParser.php
@@ -1714,10 +1714,7 @@ final class DifferentialChangesetParser extends Phobject {
       if ($old_file) {
         $old_ref->setFile($old_file);
       } else {
-        $old_data = $this->old;
-        $old_data = ipull($old_data, 'text');
-        $old_data = implode('', $old_data);
-
+        $old_data = $this->getRawDocumentEngineData($this->old);
         $old_ref->setData($old_data);
       }
     }
@@ -1730,10 +1727,7 @@ final class DifferentialChangesetParser extends Phobject {
       if ($new_file) {
         $new_ref->setFile($new_file);
       } else {
-        $new_data = $this->new;
-        $new_data = ipull($new_data, 'text');
-        $new_data = implode('', $new_data);
-
+        $new_data = $this->getRawDocumentEngineData($this->new);
         $new_ref->setData($new_data);
       }
     }
@@ -1901,6 +1895,22 @@ final class DifferentialChangesetParser extends Phobject {
     return id(new PhabricatorChangesetResponse())
       ->setRenderedChangeset($rendered_changeset)
       ->setChangesetState($state);
+  }
+
+  private function getRawDocumentEngineData(array $lines) {
+    $text = array();
+
+    foreach ($lines as $line) {
+      // If this is a "No newline at end of file." annotation, don't hand it
+      // off to the DocumentEngine.
+      if ($line['type'] == '\\') {
+        continue;
+      }
+
+      $text[] = $line['text'];
+    }
+
+    return implode('', $text);
   }
 
 }
