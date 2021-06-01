bdda7eed07345b86bff018830e0dd3963717ef39
epriestley <git@epriestley.com>
2021-06-01 06:08:07 -0700

    Improve display behavior for write locks held by omnipotent users
    Summary:
    Ref T13614. When an omnipotent user calls "synchronizeWorkingCopyBeforeWrite()", we record a WorkingCopyVersion record with a null "userPHID". The UI then renders this as "Unknown Object (????)".
    
    Improve this behavior:
    
      - When no PHID is available, just render nothing in the UI (this doesn't seem meaningfully different from no version existing at all).
      - Allow callers to provide an acting user PHID, similar to Editor.
    
    There's currently no way to perform this kind of write legitimately in the upstream, but T13614 is providing one.
    
    Test Plan:
      - Wrote a script that calls "synchronizeWorkingCopyBeforeWrite()" as the omnipotent user.
      - Ran script, saw "Unknown Object (????)" in the UI.
      - Applied UI fix, saw empty UI.
      - Applied "acting as" fix, modified script to act as the Diffusion application, ran script, saw "Diffusion" attribution in UI.
    
    {F8814806}
    
    Maniphest Tasks: T13614
    
    Differential Revision: https://secure.phabricator.com/D21669

--- src/applications/diffusion/protocol/DiffusionRepositoryClusterEngine.php
+++ src/applications/diffusion/protocol/DiffusionRepositoryClusterEngine.php
@@ -11,6 +11,7 @@ final class DiffusionRepositoryClusterEngine extends Phobject {
 
   private $repository;
   private $viewer;
+  private $actingAsPHID;
   private $logger;
 
   private $clusterWriteLock;
@@ -42,6 +43,23 @@ final class DiffusionRepositoryClusterEngine extends Phobject {
   public function setLog(DiffusionRepositoryClusterEngineLogInterface $log) {
     $this->logger = $log;
     return $this;
+  }
+
+  public function setActingAsPHID($acting_as_phid) {
+    $this->actingAsPHID = $acting_as_phid;
+    return $this;
+  }
+
+  public function getActingAsPHID() {
+    return $this->actingAsPHID;
+  }
+
+  private function getEffectiveActingAsPHID() {
+    if ($this->actingAsPHID) {
+      return $this->actingAsPHID;
+    }
+
+    return $this->getViewer()->getPHID();
   }
 
 
@@ -402,7 +420,7 @@ final class DiffusionRepositoryClusterEngine extends Phobject {
       $repository_phid,
       $device_phid,
       array(
-        'userPHID' => $viewer->getPHID(),
+        'userPHID' => $this->getEffectiveActingAsPHID(),
         'epoch' => PhabricatorTime::getNow(),
         'devicePHID' => $device_phid,
       ),
@@ -432,8 +450,6 @@ final class DiffusionRepositoryClusterEngine extends Phobject {
     if ($repository->isHosted()) {
       return;
     }
-
-    $viewer = $this->getViewer();
 
     $device = AlmanacKeys::getLiveDevice();
     $device_phid = $device->getPHID();
