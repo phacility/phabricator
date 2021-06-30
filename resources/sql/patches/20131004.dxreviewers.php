5521f76fe406338e4f42404fc98436817ae138cb
epriestley <git@epriestley.com>
2021-06-30 15:19:38 -0700

    Remove some "Phacility" and "epriestley" references
    Summary:
    Ref T13658. This just scrubs some of the simple references from the codebase.
    
    Most of what's left is in documentation which won't be relevant for a fork and/or which I need to separately revise (or more-or-less delete) at some point anyway.
    
    I removed the "install RHEL" and "install Ubuntu" scripts outright since I don't have any reasonable way to test them and don't plan to maintain them.
    
    Test Plan: Grepped for "phacility", "epriestley"; ran unit tests.
    
    Reviewers: cspeckmim
    
    Reviewed By: cspeckmim
    
    Subscribers: PHID-OPKG-gm6ozazyms6q6i22gyam
    
    Maniphest Tasks: T13658
    
    Differential Revision: https://secure.phabricator.com/D21678

--- resources/sql/patches/20131004.dxreviewers.php
+++ resources/sql/patches/20131004.dxreviewers.php
@@ -29,8 +29,7 @@ foreach (new LiskMigrationIterator($table) as $revision) {
   foreach ($reviewer_phids as $dst) {
     if (phid_get_type($dst) == PhabricatorPHIDConstants::PHID_TYPE_UNKNOWN) {
       // At least one old install ran into some issues here. Skip the row if we
-      // can't figure out what the destination PHID is. See here:
-      // https://github.com/phacility/phabricator/pull/507
+      // can't figure out what the destination PHID is.
       continue;
     }
 
