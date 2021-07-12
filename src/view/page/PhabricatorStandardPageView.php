2893c9db01c9b7bbdfb7989308027f6954a46f85
David Lawrence <dkl@mozilla.com>
2021-07-12 15:14:10 -0400

    Bug 1475019 - MOTD is displayed under the navigation bar if side menu is displayed

--- src/view/page/PhabricatorStandardPageView.php
+++ src/view/page/PhabricatorStandardPageView.php
@@ -233,6 +233,7 @@ final class PhabricatorStandardPageView extends PhabricatorBarePageView
     require_celerity_resource('phabricator-standard-page-view');
     require_celerity_resource('conpherence-durable-column-view');
     require_celerity_resource('font-lato');
+    require_celerity_resource('mozilla-motd-css');
 
     Javelin::initBehavior('workflow', array());
 
