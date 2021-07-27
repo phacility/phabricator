dbe2fb466fedd15e0aadb5e35dcc2c78bf2c4af6
epriestley <git@epriestley.com>
2021-07-27 14:02:48 -0700

    Add an "eval" rule to Remarkup
    Summary:
    Ref T13658. This adds a simple expression evaluator to Remarkup and supports platform name expressions. The syntax is:
    
    ```
    ${{{strings.platform.server.name}}}
    ```
    
    Note that this won't work inside code blocks (or literal blocks, or other block-level literal elements) right now, although it could be made to selectively (the ".path" expressions might be useful in documentation codeblocks).
    
    Test Plan: {F9391006}
    
    Reviewers: cspeckmim
    
    Reviewed By: cspeckmim
    
    Maniphest Tasks: T13658
    
    Differential Revision: https://secure.phabricator.com/D21713

---
+++ src/infrastructure/markup/markuprule/PhutilRemarkupEvalRule.php
@@ -0,0 +1,100 @@
+<?php
+
+final class PhutilRemarkupEvalRule extends PhutilRemarkupRule {
+
+  const KEY_EVAL = 'eval';
+
+  public function getPriority() {
+    return 50;
+  }
+
+  public function apply($text) {
+    return preg_replace_callback(
+      '/\${{{(.+?)}}}/',
+      array($this, 'newExpressionToken'),
+      $text);
+  }
+
+  public function newExpressionToken(array $matches) {
+    $expression = $matches[1];
+
+    if (!$this->isFlatText($expression)) {
+      return $matches[0];
+    }
+
+    $engine = $this->getEngine();
+    $token = $engine->storeText($expression);
+
+    $list_key = self::KEY_EVAL;
+    $expression_list = $engine->getTextMetadata($list_key, array());
+
+    $expression_list[] = array(
+      'token' => $token,
+      'expression' => $expression,
+      'original' => $matches[0],
+    );
+
+    $engine->setTextMetadata($list_key, $expression_list);
+
+    return $token;
+  }
+
+  public function didMarkupText() {
+    $engine = $this->getEngine();
+
+    $list_key = self::KEY_EVAL;
+    $expression_list = $engine->getTextMetadata($list_key, array());
+
+    foreach ($expression_list as $expression_item) {
+      $token = $expression_item['token'];
+      $expression = $expression_item['expression'];
+
+      $result = $this->evaluateExpression($expression);
+
+      if ($result === null) {
+        $result = $expression_item['original'];
+      }
+
+      $engine->overwriteStoredText($token, $result);
+    }
+  }
+
+  private function evaluateExpression($expression) {
+    static $string_map;
+
+    if ($string_map === null) {
+      $string_map = array(
+        'strings' => array(
+          'platform' => array(
+            'server' => array(
+              'name' => pht('Phabricator'),
+              'path' => pht('phabricator/'),
+            ),
+            'client' => array(
+              'name' => pht('Arcanist'),
+              'path' => pht('arcanist/'),
+            ),
+          ),
+        ),
+      );
+    }
+
+    $parts = explode('.', $expression);
+
+    $cursor = $string_map;
+    foreach ($parts as $part) {
+      if (isset($cursor[$part])) {
+        $cursor = $cursor[$part];
+      } else {
+        break;
+      }
+    }
+
+    if (is_string($cursor)) {
+      return $cursor;
+    }
+
+    return null;
+  }
+
+}
