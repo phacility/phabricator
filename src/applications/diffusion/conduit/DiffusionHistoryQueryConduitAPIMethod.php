phacility/phabricator

commit a989dd181dd1f1115d728595c2adb482784c4ce0

Fix Mercurial commit history ordering
Summary:
See <https://discourse.phabricator-community.org/t/diffusion-observed-mercurial-repository-history-broken/825>.

In D18769, I rewrote this from using the `--branch` flag (which is unsafe and does not function on branches named `--config=x.y` and such).

However, this rewrite accidentally changed the result order, which impacted Mercurial commit hisotry lists and graphs. Swap the order of the constraints so we get newest-to-oldest again, as expected.

Test Plan: Viewed a Mercurial repository's history graph, saw sensible chronology after the patch.

Reviewers: amckinley

Reviewed By: amckinley

Differential Revision: https://secure.phabricator.com/D18817

src/applications/diffusion/conduit/DiffusionHistoryQueryConduitAPIMethod.php
@@ -130,7 +130,7 @@ final class DiffusionHistoryQueryConduitAPIMethod
     } else {
       $path_arg = '';
       $revset_arg = hgsprintf(
-        'branch(%s) and reverse(ancestors(%s))',
+        'reverse(ancestors(%s)) and branch(%s)',
         $drequest->getBranch(),
         $commit_hash);
     }

commit b9ba0418ed13aba78ea95ebee14cdbbb43cc810f

Correct a parameter order swap in "diffusion.historyquery" for Mercurial
Summary: Ref T13666. See T13666#256253. The order of these parameters is incorrect (introduced in D18817, which was trying to turn the revset "A or B" into "B or A"), but when "commit" is the single head of "branch" (which is common), the revset is functionally equivalent.

Test Plan:
An easy reproduction case is to make a "diffusion.historyquery" call manually against a Mercurial repository, with a valid "branch" name and some non-head "commit".

  - Before patch, queried "head^" (by commit hash, not by symbol) of "default" and got "head" too (wrong).
  - Applied patch to `secure001.phacility.net` (my local `hg` web environment is iffy right now).
  - Re-ran the same query, saw "head^" as the first result rather than "head" (all hashes rather than symbols, as above), which is desired.

Reviewers: cspeckmim

Reviewed By: cspeckmim

Maniphest Tasks: T13666

Differential Revision: https://secure.phabricator.com/D21722

src/applications/diffusion/conduit/DiffusionHistoryQueryConduitAPIMethod.php
@@ -145,8 +145,8 @@ final class DiffusionHistoryQueryConduitAPIMethod
     } else {
       $revset_arg = hgsprintf(
         'reverse(ancestors(%s)) and branch(%s)',
-        $drequest->getBranch(),
-        $commit_hash);
+        $commit_hash,
+        $drequest->getBranch());
     }
 
     $hg_analyzer = PhutilBinaryAnalyzer::getForBinary('hg');