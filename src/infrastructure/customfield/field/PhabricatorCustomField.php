fc3ce9d6992d0b439dc53321e76e16b91f45b1c7
marcelo.fabri <marcelo.fabri@robinhood.com>
2024-03-03 18:22:07 -0800

    [custom fields] Call setObject before shouldEnableForRole

--- src/infrastructure/customfield/field/PhabricatorCustomField.php
+++ src/infrastructure/customfield/field/PhabricatorCustomField.php
@@ -79,6 +79,10 @@ abstract class PhabricatorCustomField extends Phobject {
         $role,
         $fields);
 
+      foreach ($fields as $field) {
+        $field->setObject($object);
+      }
+
       foreach ($fields as $key => $field) {
         // NOTE: We perform this filtering in "buildFieldList()", but may need
         // to filter again after subtype adjustment.
@@ -91,10 +95,6 @@ abstract class PhabricatorCustomField extends Phobject {
           unset($fields[$key]);
           continue;
         }
-      }
-
-      foreach ($fields as $field) {
-        $field->setObject($object);
       }
 
       $field_list = new PhabricatorCustomFieldList($fields);
